33a31d5bfd8310ee2592a91f3e3d269b5fd64a72
Sebastian Heimann <tofu@sec24c123.gfz-potsdam.de>
2010-02-15 09:49:29 +0100

    two test modules were still missing

---
+++ test/test_io.py
@@ -0,0 +1,60 @@
+from pyrocko import mseed, trace
+import unittest
+import numpy as num
+import time
+import tempfile
+import random
+from random import choice as rc
+from os.path import join as pjoin
+import os
+
+abc = 'abcdefghijklmnopqrstuvwxyz' 
+    
+def rn(n):
+    return ''.join( [ random.choice(abc) for i in xrange(n) ] )
+
+class IOTestCase( unittest.TestCase ):
+
+    def testWriteRead(self):
+        now = time.time()
+        n = 10
+        deltat = 0.1
+        
+        networks = [ rn(2) for i in range(5) ]
+        
+        traces1 = [ trace.Trace(rc(networks), rn(4), rn(2), rn(3), tmin=now+i*deltat*n*2, deltat=deltat, ydata=num.arange(n), mtime=now)
+            for i in range(100) ]
+            
+        tempdir = tempfile.mkdtemp()
+        fns = mseed.save(traces1, pjoin(tempdir, '%(network)s'))
+        traces2 = []
+        for fn in fns:
+            traces2.extend(mseed.load(fn))
+            
+        for tr in traces1:
+            assert tr in traces2
+            
+        for fn in fns:
+            os.remove(fn)
+        
+    def testReadNonexistant(self):
+        try:
+            trs = mseed.load('/tmp/thisfileshouldnotexist')
+        except OSError, e:
+            pass
+        assert isinstance(e, OSError)
+        
+    def testReadEmpty(self):
+        tempfn = tempfile.mkstemp()[1]
+        try:
+            trs = mseed.load(tempfn)
+        except mseed.MSeedError, e:
+            pass
+            
+        assert str(e).find('No SEED data detected') != -1
+        os.remove(tempfn)
+    
+
+unittest.main()
+
+    
---
+++ test/test_pile.py
@@ -0,0 +1,56 @@
+from pyrocko import trace, pile, io, config, util
+
+import unittest
+import numpy as num
+import tempfile, random, os
+from random import choice as rc
+from os.path import join as pjoin
+    
+def makeManyFiles( nfiles=200, nsamples=100000):
+    
+
+    abc = 'abcdefghijklmnopqrstuvwxyz' 
+    
+    def rn(n):
+        return ''.join( [ random.choice(abc) for i in xrange(n) ] )
+    
+    stations = [ rn(4) for i in xrange(10) ]
+    components = [ rn(3) for i in xrange(3) ]
+    networks = [ 'xx' ]
+    
+    datadir = tempfile.mkdtemp()
+    traces = []
+    for i in xrange(nfiles):
+        tmin = 1234567890+i*60*60*24*10 # random.randint(1,int(time.time()))
+        deltat = 1.0
+        data = num.ones(nsamples)
+        traces.append(trace.Trace(rc(networks), rc(stations),'',rc(components), tmin, None, deltat, data))
+    
+    fnt = pjoin( datadir, '%(network)s-%(station)s-%(location)s-%(channel)s-%(tmin)s.mseed')
+    io.save(traces, fnt, format='mseed')
+    
+    return datadir
+
+class PileTestCase( unittest.TestCase ):
+            
+    def testPileTraversal(self):
+        import tempfile, shutil
+        config.show_progress = False
+        nfiles = 200
+        nsamples = 100000
+        datadir = makeManyFiles(nfiles=nfiles, nsamples=nsamples)
+        filenames = util.select_files([datadir])
+        cachefilename = pjoin(datadir,'_cache_')
+        p = pile.Pile(filenames, cachefilename)
+        s = 0
+        for traces in p.chopper(tmin=None, tmax=None, tinc=1234.): #tpad=10.):
+            for trace in traces:
+                s += num.sum(trace.ydata)
+                
+        os.unlink(cachefilename)
+        shutil.rmtree(datadir)
+        assert s == nfiles*nsamples
+    
+
+unittest.main()
+
