e6e3991486c6290639ccbe131fb510d8b92580c9
Sebastian Heimann <sebastian.heimann@zmaw.de>
2010-03-17 12:01:27 +0100

    added a function to load kiwi kps ascii catalog event data

--- pyrocko/model.py
+++ pyrocko/model.py
@@ -1,4 +1,4 @@
-import orthodrome, config
+import orthodrome, config, util
 
 # simple flat datatypes until I have a better idea
 
@@ -11,6 +11,8 @@ class Event:
         self.depth = depth
         self.magnitude = magnitude
         self.region = region
+        
+
         
 class Station:
     def __init__(self, network, station, location, lat, lon, elevation, name='', components=None):
@@ -39,3 +41,23 @@ class Station:
         
     def __str__(self):
         return '%s.%s.%s  %f %f %f  %f %f %f  %s' % (self.network, self.station, self.location, self.lat, self.lon, self.elevation, self.dist_m, self.dist_deg, self.azimuth, self.name)
+
+
+
+def load_kps_event_list(filename):
+    elist =[]
+    f = open(filename, 'r')
+    for line in f:
+        toks = line.split()
+        if len(toks) < 7: continue
+        
+        tim = util.ctimegm(toks[0]+' '+toks[1])
+        lat, lon, depth, magnitude = [ float(x) for x in toks[2:6] ]
+        region = toks[-1]
+        name = util.gmctime_fn(tim)
+        e = Event(lat, lon, tim, name, depth, magnitude)
+        
+        elist.append(e)
+        
+    f.close()
+    return elist
