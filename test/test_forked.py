c521d19dfc02cd644e03b3736cbd1c6083022099
Sebastian Heimann <sebastian.heimann@zmaw.de>
2010-04-19 23:44:25 +0200

    snuffler can now be used as a widget

---
+++ test/test_forked.py
@@ -0,0 +1,27 @@
+import unittest
+from pyrocko import forked, util
+
+class ForkedTestCase(unittest.TestCase):
+    
+    def testForkedNormal(self):
+        try:
+            f = forked.Forked(flipped=False)
+            f.call('hallo1')
+            f.call('hallo2')
+            f.close()
+        except SystemExit:
+            pass
+                
+    def testForkedFlipped(self):
+        try:
+            f = forked.Forked(flipped=True)
+            f.call('f hallo1')
+            f.call('f hallo2')
+            f.close()
+        except SystemExit:
+            pass
+                
+if __name__ == "__main__":
+    util.setup_logging('test_forked', 'warning')
+    unittest.main()
+
