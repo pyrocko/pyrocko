2758762eca5cea7aeefd2aeb97563980a4340151
Sebastian Heimann <sebastian.heimann@zmaw.de>
2010-04-28 07:14:16 +0200

    slinksnuff

--- pyrocko/slink.py
+++ pyrocko/slink.py
@@ -1,20 +1,71 @@
-import subprocess, re, calendar, time
+import subprocess, re, calendar, time, os, signal, sys, logging
 import trace
 import numpy as num
 
+logger = logging.getLogger('pyrocko.slink')
+
 class SlowSlink:
-    def __init__(self, streams, host, port=18000):
-        self.streams = streams
+    def __init__(self, host='geofon.gfz-potsdam.de', port=18000):
         self.host = host
         self.port = port
+        self.running = False
+        self.stream_selectors = []
         
+    def query_streams(self):
+        cmd = [ 'slinktool',  '-Q', self.host+':'+str(self.port) ]
+        logger.debug('Running %s' % ' '.join(cmd))
+        slink = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+        (a,b) = slink.communicate()
+        streams = []
+        for line in a.splitlines():
+            toks = line.split()
+            if len(toks) == 9:
+                net, sta, loc, cha = toks[0], toks[1], '', toks[2]
+            else:
+                net, sta, loc, cha = toks[0], toks[1], toks[2], toks[3]
+            streams.append((net,sta,loc,cha))
+        return streams
+    
+    def add_stream(self, network, station, location, channel):
+        self.stream_selectors.append( '%s_%s:%s.D' % (network, station, channel) )
+    
+    def add_raw_stream_selector(self, stream_selector):
+        self.stream_selectors.append(stream_selector)
+    
     def start(self):
-        cmd = [ 'slinktool', '-u', '-S', self.streams, self.host+':'+str(self.port) ]
+        
+        assert not self.running
+        
+        streams_sel = ','.join(self.stream_selectors)
+        cmd = [ 'slinktool', '-u', '-S', streams_sel, self.host+':'+str(self.port) ]        
+        
+        logger.debug('Starting %s' % ' '.join(cmd))
         self.slink = subprocess.Popen(cmd,stdout=subprocess.PIPE)
         self.header = None
         self.vals = []
+        self.running = True
         self.run()
         
+    def stop(self):
+        if not self.running:
+            return
+        os.kill( self.slink.pid, signal.SIGTERM)
+        logger.debug("Waiting for slinktool to terminate...")
+        it = 0
+        while self.slink.poll() == -1:
+            time.sleep(0.01)
+            if it == 200:
+                logger.debug("Waiting for slinktool to terminate... trying harder...")
+                os.kill( self.slink.pid, signal.SIGKILL)
+            
+            it += 1
+        
+        self.running = False
+        logger.debug("Done, slinktool has terminated")
+
+    def sun_is_shining(self):
+        return self.running
+
     def run(self):
         while self.process():
             pass
@@ -52,8 +103,5 @@ class SlowSlink:
         return True
                         
     def got_trace(self, tr):
-        print 'add', tr
+        logger.info('Got trace from slinktool: %s' % tr)
         
-if __name__ == '__main__':
-    sl = SlowSlink(streams='IU_KONO:BH?.D', host='geofon-cluster.gfz-potsdam.de')
-    sl.start()
