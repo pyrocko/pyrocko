55f737197b8325d80e258849e1ede9d1c95b0d83
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-04-28 00:11:25 +0200

    worked on new experimental file format

--- pyrocko/sick.py
+++ pyrocko/sick.py
@@ -1,7 +1,16 @@
 '''File IO module for SICK traces format.'''
 
-import os
+import os, sys
 from struct import unpack, pack
+from cStringIO import StringIO
+import numpy as num
+try:
+    from hashlib import sha1
+except:
+    from sha import sha as shamod
+    sha1 = shamod.new
+
+from pyrocko import util
 
 size_record_header = 64
 no_hash = '\0' * 20
@@ -9,23 +18,23 @@ sick_version = '0000'
 
 record_types = ('trace_header', 'trace_data')
 
-type2store = {
-        num.int16: '>i2',
-        num.int32: '>i4',
-        num.int64: '>i8',
-        num.uint16: '>u2',
-        num.uint32: '>u4',
-        num.uint64: '>u8',
-        num.float32: '>f4',
-        num.float64: '>f8',
+numtypes = {
+        '@i2': (num.int16, '>i2'),
+        '@i4': (num.int32, '>i4'),
+        '@i8': (num.int64, '>i8'),
+        '@u2': (num.uint16, '>u2'),
+        '@u4': (num.uint32, '>u4'),
+        '@u8': (num.uint64, '>u8'),
+        '@f4': (num.float32, '>f4'),
+        '@f8': (num.float64, '>f8'),
     }
 
-store2type = dict( [ (v,k) for (k,v) in type2store.iteritems() ] )
+numtype2type = dict( [ (v[0],k) for (k,v) in numtypes.iteritems() ] )
 
 def serialize(a):
     s = StringIO()
     for x in a:
-        s.write(pack('>u', len(x)))
+        s.write(pack('>Q', len(x)))
         s.write(x)
 
     return s.getvalue()
@@ -34,24 +43,27 @@ def deserialize(s):
     ipos = 0
     a = []
     while ipos < len(s):
-        l = unpack('>u', s[ipos])
-        a.append(s[ipos+1:ipos+1+l])
-        ipos += 1+l
+        l = unpack('>Q', s[ipos:ipos+8])
+        a.append(s[ipos+8:ipos+8+l])
+        ipos += 8+l
 
     return a
 
 def packer(fmt):
     return ((lambda x: pack('>'+fmt, x)), (lambda x: unpack('>'+fmt, x)[0]))
 
-def unpack_array(format, data):
-    return num.fromstring(data, dtype=format).astype(store2type[format])
+def unpack_array(fmt, data):
+    return num.fromstring(data, dtype=numtypes[fmt][1]).astype(numtypes[fmt][0])
 
-def pack_array(format, data):
-    return data.astype(format).tostring()
+def pack_array(fmt, data):
+    return data.astype(numtypes[fmt][1]).tostring()
 
 
 def array_packer(fmt):
-    return ((lambda x pack_array(fmt, x)), (lambda x: unpack_array(fmt, x))) 
+    return ((lambda x: pack_array(fmt, x)), (lambda x: unpack_array(fmt, x))) 
+
+def encoding_packer(enc):
+    return ((lambda x: x.encode(enc)), (lambda x: x.decode(enc)))
 
 def noop(x):
     return x
@@ -67,27 +79,27 @@ castings = {
         'f8': packer('d'),
         'string': (noop, noop),
         'time_string': (util.time_to_str, util.str_to_time),
-        '@i2': array_packer('>i2'),
-        '@i4': array_packer('>i4'),
-        '@i8': array_packer('>i8'),
-        '@u2': array_packer('>u2'),
-        '@u4': array_packer('>u4'),
-        '@u8': array_packer('>u8'),
-        '@f4': array_packer('>f4'),
-        '@f8': array_packer('>f8'),
+        '@i2': array_packer('@i2'),
+        '@i4': array_packer('@i4'),
+        '@i8': array_packer('@i8'),
+        '@u2': array_packer('@u2'),
+        '@u4': array_packer('@u4'),
+        '@u8': array_packer('@u8'),
+        '@f4': array_packer('@f4'),
+        '@f8': array_packer('@f8'),
     }
 
 def pack_value(type, value):
     try:
         return castings[type][0](value)
-    except:
-        raise SickError('Packing value failed.')
+    except Exception, e:
+        raise SickError('Packing value failed (type=%s, value=%s, error=%s).' % (type,str(value)[:500], e))
     
 def unpack_value(type, value):
     try:
         return castings[type][1](value)
     except:
-        raise SickError('Unpacking value failed.')
+        raise SickError('Unpacking value failed (type=%s, error=%s).' % (type))
 
 class SickError(Exception):
     pass
@@ -143,8 +155,8 @@ def read_record(f, check_hash=False, max_size_record=None, type_wanted=None):
         if len(data) != size_payload:
             raise SickError('Read returned less data than expected.')
         
-        if check_hash and r.hash != no_hash:
-            if sha1.new(data).digest() != r.hash:
+        if check_hash and hash != no_hash:
+            if sha1(data).digest() != hash:
                 raise SickError('Hash computed from record data does not match value given in header.')
 
     finally:
@@ -165,7 +177,7 @@ def write_record(f, type, data, min_size_record=None, make_hash=False):
         size_record = max(min_size_record, size_record)
 
     if make_hash:
-        hash = sha1.new(data).digest()
+        hash = sha1(data).digest()
     else:
         hash = no_hash 
 
@@ -181,7 +193,7 @@ def unpack_record(data, format):
     d = {}
     for i in xrange(0,len(vals),3):
         key, type, value = vals[i:i+3]
-        if key in format and format[key] != type:
+        if format is not None and format[key] != type:
             SickError('Record value in unexpected format.')
 
         d[key] = unpack_value(type, value)
@@ -202,74 +214,31 @@ def pack_record(d, format):
     for key, value in d.iteritems():
         if key in format:
             a.append(key)
-            a.append(type)
+            a.append(format[key])
             a.append(pack_value(format[key], value))
 
     return serialize(a)
 
-def check_trace_header(d):
-    c = {}
-    for k in ('network', 'station', 'location', 'channel'):
-        c[k] = d.pop(k, '')
-
-    for k in ('tmin', 'tmax'):
-        if k in d:
-            c[k] = str_to_date(d.pop(k))
-    
-    if 'tmin' not in c:
-        c['tmin'] = 0.0
-    
-    if 'tmax' not in c:
-        c['tmax'] = None
-
-    c['deltat'] = float(d.pop('deltat'))
-    format = d.pop('format', None)
-    assert format in store2type 
-
-    return c, format
-
-def unpack_trace_header(data):
-    toks = data.split('\0')
-    return = check_trace_header(dict( [ (k,v) for (k,v) in zip(toks[0::2], toks[1::2]) ] ))
-    
-def pack_trace_header(tr):
-    
-    if tr.ydata is not None:
-        assert tr.ydata.dtype in type2store
-        format = type2store
-    else:
-        format = None 
-
-    a = [ 'network', tr.network, 'station', tr.station, 'location', tr.location, 'channel', tr.channel,
-            'tmin', date_to_str(tr.tmin), 
-            'deltat', '%f' % tr.deltat ]
-
-    if tr.ydata is not None:
-        assert tr.ydata.dtype in type2store
-        a.extend('format', type2store)
-    return '\0'.join(a)
-
-
-def unpack_trace_data(data, format):
-    return num.fromstring(data, dtype=format).astype(store2type[format])
-
-def pack_trace_data(data, format):
-    return data.astype(format).tostring()
-
+trace_header_record_format = {
+            'network': 'string',
+            'station': 'string',
+            'location': 'string',
+            'channel': 'string',
+            'tmin': 'time_string',
+            'tmax': 'time_string',
+            'deltat': 'f8',
+    }
 
 def load(fn, load_data=True):
-   
+    f = open(fn, 'r') 
     traces = []
     while True: 
         try:
-            d, format = unpack_trace_header(read_record(f))
-        
-            if format is not None: 
-                if load_data:
-                    d['ydata'] = unpack_trace_data(read_record(f), format)
-                else:
-                    skip_record(f)
-                    d['ydata'] = None
+            d = unpack_record(read_record(f, 'trace_header'), trace_header_record_format)
+            if load_data:
+                d.update( unpack_trace_data_record(read_record(f)) )
+            else:
+                skip_record(f)
             
             tr = trace.Trace(**d)
             traces.append(tr)
@@ -279,8 +248,31 @@ def load(fn, load_data=True):
 
     return traces
 
-def save(traces, fn):
+def extract(tr, format):
+    d = {}
+    for k in format.keys():
+        d[k] = getattr(tr,k)
+    return d
 
+def pack_trace_header_record(tr):
+    format = trace_header_record_format
+    return pack_record(extract(tr,format), format)
+
+def pack_trace_data_record(tr):
+
+    format = {}
+    type = numtype2type[tr.ydata.dtype.type]
+    format['ydata'] = type
+
+    return pack_record(extract(tr, format), format)
+
+def save(traces, fn):
+    f = open(fn, 'w')
     for tr in traces:
-        write_record( f, pack_trace_header(tr)
+        write_record(f, 'trace_header', pack_trace_header_record(tr), make_hash=True)
+        write_record(f, 'trace_data', pack_trace_data_record(tr), make_hash=True)
+
+    f.close()
+
+
 
