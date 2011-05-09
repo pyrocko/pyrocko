pyrocko/pyrocko

commit 57ffd6f53fcef82146e8d252b7a205d14710b9a9

can pass anti-aliasing filter end state into later invocation of decimate

pyrocko/trace.py
@@ -705,7 +705,7 @@ class Trace(object):
         
     def chop(self, tmin, tmax, inplace=True, include_last=False, snap=(round,round), want_incomplete=True):
         if want_incomplete:
-            if tmax <= self.tmin or self.tmax < tmin: 
+            if tmax <= self.tmin-self.deltat or self.tmax+self.deltat < tmin: 
                 raise NoData()
         else:
             if tmin < self.tmin or self.tmax < tmax: 
@@ -727,7 +727,7 @@ class Trace(object):
         
         return obj
     
-    def downsample(self, ndecimate, snap=False):
+    def downsample(self, ndecimate, snap=False, initials=None, demean=True):
         newdeltat = self.deltat*ndecimate
         if snap:
             ilag = (math.ceil(self.tmin / newdeltat) * newdeltat - self.tmin)/self.deltat
@@ -737,14 +737,23 @@ class Trace(object):
             self.tmin += ilag*self.deltat
         else:
             data = self.ydata.astype(num.float64)
+        
+        if demean:
+            data -= num.mean(data)
+        
+        result = util.decimate(data, ndecimate, ftype='fir', zi=initials)
+        if initials is None:
+            self.ydata, finals = result, None
+        else:
+            self.ydata, finals = result
             
-        data -= num.mean(data)
-        self.ydata = util.decimate(data, ndecimate, ftype='fir')
         self.deltat = reuse(self.deltat*ndecimate)
         self.tmax = self.tmin+(len(self.ydata)-1)*self.deltat
         self.update_ids()
         
-    def downsample_to(self, deltat, snap=False, allow_upsample_max=1):
+        return finals
+        
+    def downsample_to(self, deltat, snap=False, allow_upsample_max=1, initials=None, demean=True):
         ratio = deltat/self.deltat
         rratio = round(ratio)
         if abs(rratio - ratio)/ratio > 0.0001:
@@ -770,7 +779,7 @@ class Trace(object):
         deci_seq = util.decitab(int(rratio))
         for ndecimate in deci_seq:
              if ndecimate != 1:
-                self.downsample(ndecimate, snap=snap)
+                return self.downsample(ndecimate, snap=snap, initials=initials, demean=demean)
             
     def nyquist_check(self, frequency, intro='Corner frequency', warn=True, raise_exception=False):
         if frequency >= 0.5/self.deltat:
@@ -831,6 +840,10 @@ class Trace(object):
         nshort = tshort/self.deltat
         nlong = tlong/self.deltat
     
+        assert nshort < nlong
+        if nlong > len(self.ydata):
+            raise TraceTooShort('Samples in trace: %s, samples needed: %s' % (len(self.ydata), nlong))
+         
         if quad:
             sqrdata = self.ydata**2
         else:

pyrocko/util.py
@@ -4,6 +4,7 @@ import time, logging, os, sys, re, calendar, math
 from scipy import signal
 from os.path import join as pjoin
 import config
+import numpy as num
 
 logger = logging.getLogger('pyrocko.util')
 
@@ -85,7 +86,7 @@ class GlobalVars:
     decimate_iir_coeffs = {}
     re_frac = None
 
-def decimate(x, q, n=None, ftype='iir'):
+def decimate(x, q, n=None, ftype='iir', zi=None):
     """Downsample the signal x by an integer factor q, using an order n filter
     
     By default, an order 8 Chebyshev type I filter is used or a 30 point FIR 
@@ -110,23 +111,41 @@ def decimate(x, q, n=None, ftype='iir'):
         else:
             n = 8
             
+            
     if ftype == 'fir':
         coeffs = GlobalVars.decimate_fir_coeffs
         if (n, 1./q) not in coeffs:
             coeffs[n,1./q] = signal.firwin(n+1, 1./q, window='hamming')
         
         b = coeffs[n,1./q]
-        y = signal.lfilter(b, 1., x)
+        
+        if zi is None or zi is True:
+            zi_ = num.zeros(len(b)-1, dtype=num.float)
+        else:
+            zi_ = zi
+        
+        y, zf = signal.lfilter(b, 1., x, zi=zi_)
+            
+            
     else:
         coeffs = GlobalVars.decimate_iir_coeffs
         if (n,0.05,0.8/q) not in coeffs:
             coeffs[n,0.05,0.8/q] = signal.cheby1(n, 0.05, 0.8/q)
            
         b, a = coeffs[n,0.05,0.8/q]
-        y = signal.lfilter(b, a, x)
+        
+        if zi is None is True:
+            zi_ = num.zeros(max(len(a),len(b))-1, dtype=num.float)
+        else:
+            zi_ = zi
+        y, zf = signal.lfilter(b, a, x, zi=zi_)
 
-    return y[n/2::q].copy()
 
+    if zi is not None:
+        return y[n/2::q].copy(), zf
+    else:
+        return y[n/2::q].copy()
+    
 class UnavailableDecimation(Exception):
     '''Exception raised for unavailable decimation factors.'''
 

commit 6a3b030cca211ac8929f6810b8f01dafaef10c26

downsample_to was broken since commit 57ffd6f53fcef82146e8d252b7a205d14710b9a9

pyrocko/trace.py
@@ -789,10 +789,17 @@ class Trace(object):
                     rratio = round(ratio)
             
         deci_seq = util.decitab(int(rratio))
-        for ndecimate in deci_seq:
+        finals = []
+        for i, ndecimate in enumerate(deci_seq):
              if ndecimate != 1:
-                return self.downsample(ndecimate, snap=snap, initials=initials, demean=demean)
-            
+                 xinitials = None
+                 if initials is not None:
+                     xinitials = initials[i]
+                 finals.append(self.downsample(ndecimate, snap=snap, initials=xinitials, demean=demean))
+
+        if initials is not None:
+            return finals
+
     def nyquist_check(self, frequency, intro='Corner frequency', warn=True, raise_exception=False):
         if frequency >= 0.5/self.deltat:
             message = '%s (%g Hz) is equal to or higher than nyquist frequency (%g Hz). (Trace %s)' \