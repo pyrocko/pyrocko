9df09b083652e4338a9ba54f5b17981ef7245371
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-08-10 20:42:28 +0200

    WIP

--- pyrocko/iris_ws.py
+++ pyrocko/iris_ws.py
@@ -1,18 +1,46 @@
 import urllib, urllib2
 from xml.dom import minidom
+import util
 
 base_url = 'http://www.iris.edu/ws'
 
-def ws_get(url, **kwargs):
+def tdate(s):
+    return util.str_to_time(s, '%Y-%m-%d')
+
+def sdate(t):
+    return util.time_to_str(t, '%Y-%m-%d')
+
+def tdatetime(s):
+    return util.str_to_time(s, format='%Y-%m-%dT%H:%M:%S')
+
+def sdatetime(t):
+    return util.time_to_str(t, format='%Y-%m-%dT%H:%M:%S')
+
+def ws_request(url, post=False, **kwargs):
     url_values = urllib.urlencode(kwargs)
-    fullurl = url + '?' + url_values
-    print fullurl
-    return urllib2.urlopen(fullurl).read()
+    if post:
+        return urllib2.urlopen(url,data=post).read()
+    else:
+        return urllib2.urlopen(url+'?'+url_values).read()
 
 def station( **kwargs ):
-    return minidom.parseString(ws_get(base_url + '/station/query', **kwargs))
+    return minidom.parseString(ws_request(base_url + '/station/query', **kwargs))
 
+def bulkdataselect( selection, quality=None, minimumlength=None, longestonly=False ):
 
+    l = []
+    if quality is not None:
+        l.append('quality %s' % quality)
+    if minimumlength is not None:
+        l.append('minimumlength %s' % minimumlength)
+    if longestonly:
+        l.append('longestonly')
 
-
+    for (network, station, location, channel, tmin, tmax) in selection:
+        if location == '':
+            location = '--'
+        
+        l.append(' '.join((network, station, location, channel, sdatetime(tmin), sdatetime(tmax))))
+    
+    return ws_request(base_url + '/bulkdataselect/query', post='\n'.join(l))
 
