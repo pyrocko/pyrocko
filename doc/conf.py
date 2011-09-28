06fa60beafd51a32376734e2779950aab336a155
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-09-28 11:07:04 +0200

    changed documentation html title

--- doc/conf.py
+++ doc/conf.py
@@ -115,10 +115,10 @@ html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = u"%s v%s Manual" % (project, release)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = u"%s Manual" % project
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
