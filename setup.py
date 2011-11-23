f6403b4eba8570c77bcc6881cbe86ff58761bac9
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-11-23 17:17:45 +0100

    a module pyrocko.info containing version information is now created when setup.py is run.

--- setup.py
+++ setup.py
@@ -1,11 +1,58 @@
 import numpy
 from distutils.core import setup, Extension
+import os
+
+def git_infos():
+    '''Query git about sha1 of last commit and check if there are local modifications.'''
+
+    from subprocess import Popen, PIPE
+    import re
+    def q(c):
+        return Popen(c, stdout=PIPE).communicate()[0]
+    
+    sha1 = q(['git', 'log', '--pretty=oneline', '-n1']).split()[0]
+    sha1 = re.sub('[^0-9a-f]', '', sha1)
+    sstatus = q(['git', 'status'])
+    local_modifications = bool(re.search(r'^#\s+modified:', sstatus, flags=re.M))
+    return sha1, local_modifications
+
+def make_info_module(packname, version):
+    '''Put version and revision information into file pyrocko/info.py.'''
+
+    sha1, local_modifications = None, None
+    combi = '%s-%s' % (packname, version)
+    try:
+        sha1 , local_modifications = git_infos()
+        combi += '-%s' % sha1
+        if local_modifications:
+            combi += '-modified'
+
+    except OSError:
+        pass
+    
+    s = '''# This module is automatically created from setup.py
+git_sha1 = %s
+local_modifications = %s
+version = %s
+long_version = %s
+''' % tuple( [ repr(x) for x in (sha1, local_modifications, version, combi) ] )
+   
+    try:
+        f = open(os.path.join('pyrocko', 'info.py'), 'w')
+        f.write(s)
+        f.close()
+    except:
+        pass
 
 packname = 'pyrocko' 
+version = '0.2'
+
 subpacknames = [ 'pyrocko.snufflings' ]
 
+make_info_module(packname, version)
+
 setup( name = packname,
-    version = '0.2',
+    version = version,
     description = 'Seismological Processing Unit',
     packages = [ packname ] + subpacknames,
     ext_modules = [ 
