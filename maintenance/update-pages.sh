4aca2719cc01406809833669f4157308985f8b70
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-12-19 16:19:53 +0100

    got pages update script to work with older git versions

--- maintenance/update-pages.sh
+++ maintenance/update-pages.sh
@@ -5,17 +5,21 @@ if [ ! -f maintenance/update-pages.sh ] ; then
 fi
 
 if [ ! -d pages ] ; then
-    git clone -b gh-pages git@github.com:emolch/pyrocko.git pages || exit 1
+    git clone -n git@github.com:emolch/pyrocko.git pages || exit 1
+    cd pages || exit 1
+    git checkout -b gh-pages origin/gh-pages || exit 1
+    cd ..
 fi
 cd pages || exit 1
 git pull origin gh-pages || exit 1
 cd ..
 cd doc || exit 1
+make clean || exit 1
 make html || exit 1
 cd ..
 cp -R doc/_build/html/* pages/ || exit 1
 cd pages || exit 1
-git add -A || exit 1
+git add * || exit 1
 git commit || exit 1
 git push origin gh-pages
 
