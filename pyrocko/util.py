08a23525173ada20baee7e426748a74c6240f480
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2012-07-27 15:14:37 +0200

    util.gform() function added

--- pyrocko/util.py
+++ pyrocko/util.py
@@ -880,5 +880,30 @@ class TableReader:
             
         return row
 
+def gform( number, significant_digits=3 ):
+    '''Pretty print floating point numbers.
+    
+Align floating point numbers at the decimal dot.
+       
+      |  -d.dde+xxx|
+      |  -d.dde+xx |
+      |-ddd.       |
+      | -dd.d      |
+      |  -d.dd     |
+      |  -0.ddd    |
+      |  -0.0ddd   |
+      |  -0.00ddd  |
+      |  -d.dde-xx |
+      |  -d.dde-xxx|'''
+    
+    no_exp_range = (pow(10.,-1), 
+                    pow(10.,significant_digits))
+    width = significant_digits+significant_digits-1+1+1+5
+    
+    if (no_exp_range[0] <= abs(number) < no_exp_range[1]) or number == 0.:
+        s = ('%#.*g' % (significant_digits, number)).rstrip('0')
+    else:
+        s = '%.*E' % (significant_digits-1, number)
+    s = (' '*(-s.find('.')+(significant_digits+1))+s).ljust(width)
+    return s
 
-            
