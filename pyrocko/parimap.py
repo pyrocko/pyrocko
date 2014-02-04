6abbe9422a08293a2378721742cbf351281aa954
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2014-02-04 10:25:20 +0100

    parimap: flake8 ok

--- pyrocko/parimap.py
+++ pyrocko/parimap.py
@@ -1,6 +1,8 @@
 import Queue
-import itertools, multiprocessing
+import itertools
+import multiprocessing
 import traceback
+
 
 def worker(q_in, q_out, function, eprintignore):
     while True:
@@ -14,12 +16,12 @@ def worker(q_in, q_out, function, eprintignore):
         except Exception, e:
             if eprintignore is not None and not isinstance(e, eprintignore):
                 traceback.print_exc()
-            pass 
 
-        q_out.put((i,r,e))
+        q_out.put((i, r, e))
+
 
 def parimap(function, *iterables, **kwargs):
-    assert all( k in ('nprocs', 'eprintignore') for k in kwargs.keys() )
+    assert all(k in ('nprocs', 'eprintignore') for k in kwargs.keys())
 
     nprocs = kwargs.get('nprocs', None)
     eprintignore = kwargs.get('eprintignore', 'all')
@@ -36,8 +38,8 @@ def parimap(function, *iterables, **kwargs):
     if nprocs is None:
         nprocs = multiprocessing.cpu_count()
 
-    q_in   = multiprocessing.Queue(1)
-    q_out  = multiprocessing.Queue()
+    q_in = multiprocessing.Queue(1)
+    q_out = multiprocessing.Queue()
 
     procs = []
 
@@ -50,20 +52,22 @@ def parimap(function, *iterables, **kwargs):
     iterables = map(iter, iterables)
     while True:
         if nrun < nprocs and not all_written and not error_ahead:
-            args = tuple( it.next() for it in iterables )
+            args = tuple(it.next() for it in iterables)
             if len(args) == len(iterables):
                 if len(procs) < nrun + 1:
-                    p = multiprocessing.Process(target=worker, args=(q_in, q_out, function, eprintignore))
+                    p = multiprocessing.Process(
+                        target=worker,
+                        args=(q_in, q_out, function, eprintignore))
                     p.daemon = True
                     p.start()
                     procs.append(p)
 
-                q_in.put((nwritten,args))
+                q_in.put((nwritten, args))
                 nwritten += 1
                 nrun += 1
             else:
                 all_written = True
-                [q_in.put((None,None)) for p in procs] 
+                [q_in.put((None, None)) for p in procs]
 
         try:
             while nrun > 0:
@@ -80,16 +84,16 @@ def parimap(function, *iterables, **kwargs):
         if results:
             results.sort()
             # check for error ahead to prevent further enqueuing
-            if any( e for (_,_,e) in results ):
+            if any(e for (_, _, e) in results):
                 error_ahead = True
 
             while results:
-                (i,r,e) = results[0]
+                (i, r, e) = results[0]
                 if i == iout:
                     results.pop(0)
-                    if e: 
+                    if e:
                         if not all_written:
-                            [q_in.put((None,None)) for p in procs if p]
+                            [q_in.put((None, None)) for p in procs]
                         raise e
                     else:
                         yield r
@@ -101,4 +105,4 @@ def parimap(function, *iterables, **kwargs):
         if all_written and nrun == 0:
             break
 
-    [p.join() for p in procs if p]
+    [p.join() for p in procs]
