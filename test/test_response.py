82eae9e3cb6c831139c80fedb49323d51e2e4af8
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2014-03-28 08:38:56 +0100

    added some tests

--- test/test_response.py
+++ test/test_response.py
@@ -1,6 +1,7 @@
 import unittest, os
 import numpy as num
 from pyrocko import util, evalresp, pz, trace
+import guts
 
 def numeq(a,b, eps):
     return num.all(num.abs(num.array(a) - num.array(b)) < eps)
@@ -39,7 +40,58 @@ class ResponseTestCase(unittest.TestCase):
             lab.gca().loglog() 
             lab.show()
 
-        assert num.max(num.abs(transfer - transfer2)) < 1e-4
+        assert numeq(transfer, transfer2, 1e-4)
+
+    def test_dump_load(self):
+        
+        r = trace.FrequencyResponse()
+
+        r = trace.PoleZeroResponse([0j, 0j], [1j, 2j, 1+3j, 1-3j], 1.0)
+        r.regularize()
+        r2 = guts.load_string(r.dump())
+        assert cnumeq(r.poles, r2.poles, 1e-6)
+        assert cnumeq(r.zeros, r2.zeros, 1e-6)
+        assert numeq(r.constant, r2.constant)
+
+        r = trace.SampledResponse(
+                [0., 1., 5., 10.],
+                [0., 1., 1., 0.])
+
+        r.regularize()
+        r2 = guts.load_string(r.dump())
+        assert numeq(r.frequencies, r2.frequencies, 1e-6)
+        assert cnumeq(r.values, r2.values, 1e-6)
+
+        r = trace.IntegrationResponse(2, 5.0)
+        r2 = guts.load_string(r.dump())
+        assert numeq(r.n, r2.n)
+        assert numeq(r.gain, r2.gain, 1e-6)
+
+        r = trace.DifferentiationResponse(2, 5.0)
+        r2 = guts.load_string(r.dump())
+        assert numeq(r.n, r2.n)
+        assert numeq(r.gain, r2.gain, 1e-6)
+
+        r = trace.AnalogFilterResponse(
+            a=[1.0, 2.0, 3.0],
+            b=[2.0, 3.0])
+        r2 = guts.load_string(r.dump())
+        assert numeq(r.a, r2.a, 1e-6)
+        assert numeq(r.b, r2.b, 1e-6)
+
+
+def numeq(a, b, eps=0):
+    a = num.asarray(a)
+    b = num.asarray(b)
+    return num.max(num.abs(a - b)) <= eps
+
+
+def cnumeq(a, b, eps=0):
+    a = num.asarray(a)
+    b = num.asarray(b)
+    return num.max(num.abs(a.real - b.real)) <= eps and \
+           num.max(num.abs(a.imag - b.imag)) <= eps
+
 
 if __name__ == "__main__":
     util.setup_logging('test_response', 'warning')
