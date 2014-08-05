5648b48e70a90c74304e4e3705ab437ee3ed9b02
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2014-08-05 15:13:34 +0200

    changed default location of pyrocko trace metadata cache
    Trace metadata is now cached in the users home directory rather than
    under /tmp.

--- pyrocko/config.py
+++ pyrocko/config.py
@@ -1,4 +1,5 @@
 import os
 
 earthradius = 6371.*1000.
-cache_dir = '/tmp/pyrocko_0.3_cache_%s' % os.environ['USER']
+#cache_dir = '/tmp/pyrocko_0.3_cache_%s' % os.environ['USER']
+cache_dir = os.path.join(os.environ['HOME'], '.pyrocko_0.3_cache')
