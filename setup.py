1050415c0d19d44ea38f359c3b70fbcf8c84e0d0
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2014-08-06 10:10:05 +0200

    setup.py: cleanup and fixes for operation under easy_install

--- setup.py
+++ setup.py
@@ -1,33 +1,45 @@
 import sys
 
-if sys.version_info < (2,6) or (3,0) <= sys.version_info:
+if sys.version_info < (2, 6) or (3, 0) <= sys.version_info:
     sys.exit('This version of Pyrocko requires Python version >=2.6 and <3.0')
 
 import numpy
-from distutils.core import setup, Extension
-import os, time
+
+import os
+import time
 from os.path import join as pjoin
+
+from distutils.core import setup, Extension
+from distutils.cmd import Command
+from distutils.command.build_py import build_py
+from distutils.command.build_ext import build_ext
+from distutils.command.install import install
 
 
 class NotInAGitRepos(Exception):
     pass
 
+
 def git_infos():
-    '''Query git about sha1 of last commit and check if there are local modifications.'''
+    '''Query git about sha1 of last commit and check if there are local \
+       modifications.'''
 
     from subprocess import Popen, PIPE
     import re
+
     def q(c):
         return Popen(c, stdout=PIPE).communicate()[0]
-    
+
     if not os.path.exists('.git'):
         raise NotInAGitRepos()
 
     sha1 = q(['git', 'log', '--pretty=oneline', '-n1']).split()[0]
     sha1 = re.sub('[^0-9a-f]', '', sha1)
     sstatus = q(['git', 'status'])
-    local_modifications = bool(re.search(r'^#\s+modified:', sstatus, flags=re.M))
+    local_modifications = bool(re.search(r'^#\s+modified:', sstatus,
+                                         flags=re.M))
     return sha1, local_modifications
+
 
 def make_info_module(packname, version):
     '''Put version and revision information into file pyrocko/info.py.'''
@@ -35,14 +47,14 @@ def make_info_module(packname, version):
     sha1, local_modifications = None, None
     combi = '%s-%s' % (packname, version)
     try:
-        sha1 , local_modifications = git_infos()
+        sha1, local_modifications = git_infos()
         combi += '-%s' % sha1
         if local_modifications:
             combi += '-modified'
 
     except (OSError, NotInAGitRepos):
         pass
-   
+
     datestr = time.strftime('%Y-%m-%d_%H:%M:%S')
     combi += '-%s' % datestr
 
@@ -52,8 +64,9 @@ local_modifications = %s
 version = %s
 long_version = %s
 installed_date = %s
-''' % tuple( [ repr(x) for x in (sha1, local_modifications, version, combi, datestr) ] )
-   
+''' % tuple([repr(x) for x in (
+        sha1, local_modifications, version, combi, datestr)])
+
     try:
         f = open(pjoin('pyrocko', 'info.py'), 'w')
         f.write(s)
@@ -61,12 +74,14 @@ installed_date = %s
     except:
         pass
 
+
 def make_prerequisites():
     from subprocess import check_call
     try:
         check_call(['sh', 'prerequisites.sh'])
     except:
-        sys.exit('error: failed to build the included prerequisites with "sh prerequisites.sh"')
+        sys.exit('error: failed to build the included prerequisites with '
+                 '"sh prerequisites.sh"')
 
 
 def double_install_check():
@@ -88,7 +103,8 @@ def double_install_check():
 
         try:
             import pyrocko
-            x = (pyrocko.installed_date, p, pyrocko.__file__, pyrocko.long_version)
+            x = (pyrocko.installed_date, p, pyrocko.__file__,
+                 pyrocko.long_version)
             found.append(x)
             del sys.modules['pyrocko']
             del sys.modules['pyrocko.info']
@@ -103,7 +119,7 @@ def double_install_check():
     i = 1
 
     dates = sorted([x[0] for x in found])
-    
+
     if len(found) > 1:
         print >>e, 'sys.path configuration is: \n  %s' % '\n  '.join(sys.path)
         print >>e
@@ -116,11 +132,11 @@ def double_install_check():
 
                 if installed_date == dates[-1]:
                     oldnew = ' (newest)'
-            
+
             if fpath.endswith(initpyc):
                 fpath = fpath[:-len(initpyc)]
 
-            print >>e, 'Pyrocko installation #%i:' % i 
+            print >>e, 'Pyrocko installation #%i:' % i
             print >>e, '  date installed: %s%s' % (installed_date, oldnew)
             print >>e, '  path: %s' % fpath
             print >>e, '  version: %s' % long_version
@@ -128,20 +144,28 @@ def double_install_check():
             i += 1
 
     if len(found) > 1:
-        print >>e, 'Installation #1 is used with default sys.path configuration.'
+        print >>e, \
+            'Installation #1 is used with default sys.path configuration.'
         print >>e
-        print >>e, 'WARNING: Multiple installations of Pyrocko are present on this system.'
+        print >>e, 'WARNING: Multiple installations of Pyrocko are present '\
+            'on this system.'
         if found[0][0] != dates[-1]:
             print >>e, 'WARNING: Not using newest installed version.'
+        print >>e
 
 
-packname = 'pyrocko' 
+packname = 'pyrocko'
 version = '0.3'
 
-subpacknames = [ 'pyrocko.snufflings', 'pyrocko.gf', 'pyrocko.fomosto', 'pyrocko.fdsn' ]
+subpacknames = [
+    'pyrocko.snufflings',
+    'pyrocko.gf',
+    'pyrocko.fomosto',
+    'pyrocko.fdsn'
+]
 
-from distutils.cmd import Command
-class HeadlessCommand(Command):
+
+class double_install_check_cls(Command):
     user_options = []
 
     def initialize_options(self):
@@ -150,75 +174,81 @@ class HeadlessCommand(Command):
     def finalize_options(self):
         pass
 
-class make_prerequisites_cls(HeadlessCommand):
-    def run(self):
-        make_prerequisites()
-
-class make_info_module_cls(HeadlessCommand):
-    def run(self):
-        make_info_module(packname, version)
-
-class double_install_check_cls(HeadlessCommand):
     def run(self):
         double_install_check()
 
-from distutils.command import install, build
-build.build.sub_commands[0:0] = [
-    ('make_info_module', None),
-    ('make_prerequisites', None),
-]
+install.sub_commands.append(['double_install_check', None])
 
-install.install.sub_commands.extend([
-    ('double_install_check', None),
-])
+
+class custom_build_py(build_py):
+    def run(self):
+        make_info_module(packname, version)
+        build_py.run(self)
+
+
+class custom_build_ext(build_ext):
+    def run(self):
+        make_prerequisites()
+        build_ext.run(self)
 
 setup(
-    cmdclass = {
-        'make_info_module': make_info_module_cls,
-        'make_prerequisites': make_prerequisites_cls,
-        'double_install_check': double_install_check_cls},
+    cmdclass={
+        'build_py': custom_build_py,
+        'build_ext': custom_build_ext,
+        'double_install_check': double_install_check_cls,
+    },
 
-    name = packname,
-    version = version,
-    description = 'Seismological Processing Unit',
-    author = 'Sebastian Heimann',
-    author_email = 'sebastian.heimann@zmaw.de',
-    url = 'http://emolch.github.com/pyrocko/',
-    packages = [ packname ] + subpacknames,
-    ext_package = packname,
-    ext_modules = [ 
-        Extension( 'util_ext',
-            sources = [ pjoin(packname, 'util_ext.c') ]),
-        
-        Extension( 'mseed_ext',
-            include_dirs = [ numpy.get_include(), 'libmseed' ],
-            library_dirs = ['libmseed'],
-            libraries = ['mseed'],
-            sources = [ pjoin(packname, 'mseed_ext.c') ]),
-                
-        Extension( 'evalresp_ext',
-            include_dirs = [ numpy.get_include(), 'evalresp-3.3.0/include' ],
-            library_dirs = [ 'evalresp-3.3.0/lib' ],
-            libraries = ['evresp'],
-            sources = [ pjoin(packname, 'evalresp_ext.c') ]),
-            
-        Extension( 'gse2_ext',
-            include_dirs = [ numpy.get_include() ],
-            sources = [ pjoin(packname, 'gse2_ext.c') ]),
+    name=packname,
+    version=version,
+    description='Seismological Processing Unit',
+    author='Sebastian Heimann',
+    author_email='sebastian.heimann@zmaw.de',
+    url='http://emolch.github.com/pyrocko/',
+    packages=[packname] + subpacknames,
+    ext_package=packname,
+    ext_modules=[
+        Extension(
+            'util_ext',
+            sources=[pjoin(packname, 'util_ext.c')]),
 
-        Extension( 'autopick_ext',
-            include_dirs = [ numpy.get_include() ],
-            sources = [ pjoin(packname, 'autopick_ext.c') ]),
+        Extension(
+            'mseed_ext',
+            include_dirs=[numpy.get_include(), 'libmseed'],
+            library_dirs=['libmseed'],
+            libraries=['mseed'],
+            sources=[pjoin(packname, 'mseed_ext.c')]),
 
-        Extension( 'gf.store_ext',
-            include_dirs = [ numpy.get_include() ],
-            extra_compile_args = [ '-D_FILE_OFFSET_BITS=64', '-Wextra' ],
-            sources = [ pjoin(packname, 'gf', 'store_ext.c') ]),
+        Extension(
+            'evalresp_ext',
+            include_dirs=[numpy.get_include(), 'evalresp-3.3.0/include'],
+            library_dirs=['evalresp-3.3.0/lib'],
+            libraries=['evresp'],
+            sources=[pjoin(packname, 'evalresp_ext.c')]),
+
+        Extension(
+            'gse2_ext',
+            include_dirs=[numpy.get_include()],
+            sources=[pjoin(packname, 'gse2_ext.c')]),
+
+        Extension(
+            'autopick_ext',
+            include_dirs=[numpy.get_include()],
+            sources=[pjoin(packname, 'autopick_ext.c')]),
+
+        Extension(
+            'gf.store_ext',
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=['-D_FILE_OFFSET_BITS=64', '-Wextra'],
+            sources=[pjoin(packname, 'gf', 'store_ext.c')]),
     ],
-                
-    scripts = [ 'apps/snuffler', 'apps/hamster', 'apps/cake', 'apps/fomosto', 
-                'apps/jackseis' ],
 
-    package_data = { packname: ['data/*.png', 'data/*.html', 'data/earthmodels/*.nd'] },
+    scripts=[
+        'apps/snuffler',
+        'apps/hamster',
+        'apps/cake',
+        'apps/fomosto',
+        'apps/jackseis'],
+
+    package_data={
+        packname: ['data/*.png', 'data/*.html', 'data/earthmodels/*.nd']},
 )
-
