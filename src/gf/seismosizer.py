6fe84356e5067018307d725cf0c80fd8c480a320
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2014-11-27 09:54:30 +0100

    reformatted docstring

--- src/gf/seismosizer.py
+++ src/gf/seismosizer.py
@@ -1832,11 +1832,16 @@ class LocalEngine(Engine):
         return self._effective_default_store_id
 
     def get_store(self, store_id=None):
-        '''Get a store from the engine. If no *store_id* is provided the store
+        '''
+        Get a store from the engine.
+
+        :param store_id: identifier of the store (optional)
+        :returns: :py:class:`pyrocko.gf.store.Store` object
+
+        If no *store_id* is provided the store
         associated with the *default_store_id* is returned.
         Raises :py:exec`NoDefaultStoreSet` if *default_store_id* is undefined.
-
-        :param store_id: identifier of the store (optional)'''
+        '''
 
         if store_id is None:
             store_id = self.effective_default_store_id()
