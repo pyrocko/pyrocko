61fa1441bfc95762e900ee362a043874d05ad7af
marius <marius.kriegerowski@zmaw.de>
2014-12-03 19:03:03 +0100

    added a missing dependency for opensuse>=13

--- prerequisites/prerequisites_opensuse.sh
+++ prerequisites/prerequisites_opensuse.sh
@@ -1,4 +1,4 @@
 zypper install make git python-devel python-setuptools \
 python-numpy python-numpy-devel python-scipy python-matplotlib python-qt4 \
-python-PyYAML python-progressbar
+python-PyYAML python-progressbar python-matplotlib-tk
 easy_install pyavl
