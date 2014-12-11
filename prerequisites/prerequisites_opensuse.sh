3c4d8072b70af2345d4a1d8471cbce583ab3c079
HerrMuellerluedenscheid <marius.kriegerowski@zmaw.de>
2014-12-11 18:04:41 +0100

    added gcc to opensuse prerqes

--- prerequisites/prerequisites_opensuse.sh
+++ prerequisites/prerequisites_opensuse.sh
@@ -1,4 +1,4 @@
-zypper -n install make git python-devel python-setuptools \
+zypper -n install make git gcc python-devel python-setuptools \
 python-numpy python-numpy-devel python-scipy python-matplotlib python-qt4 \
 python-PyYAML python-progressbar python-matplotlib-tk
 easy_install pyavl
