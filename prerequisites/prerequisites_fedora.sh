c1e041cca0e37dd12f620e88218a703087609084
Marius Kriegerowski <marius.kriegerowski@zmaw.de>
2015-02-17 22:11:41 +0100

    added missing prerequisite

--- prerequisites/prerequisites_fedora.sh
+++ prerequisites/prerequisites_fedora.sh
@@ -1,3 +1,3 @@
-yum install -y make git python python-yaml numpy scipy PyQt4 
+yum install -y make git python python-yaml numpy scipy PyQt4 python-matplotlib
 easy_install progressbar
 easy_install pyavl
