34ee10b8f549fc7c1f080ad89980d28369966170
Marius Kriegerowski <marius.kriegerowski@zmaw.de>
2015-03-12 18:42:41 +0100

    marker_editor: Set selection from table in viewer and vice versa.

--- src/marker_editor.py
+++ src/marker_editor.py
@@ -17,7 +17,7 @@ class MarkerModel(QAbstractTableModel):
     def __init__(self, *args, **kwargs):
         QAbstractTableModel.__init__(self, *args, **kwargs)
         self.viewer = None
-        self.headerdata = ['Type', 'Time']
+        self.headerdata = ['Type', 'Time', 'Mag']
 
     def set_viewer(self, viewer):
         self.viewer = viewer
@@ -29,7 +29,7 @@ class MarkerModel(QAbstractTableModel):
         return len(self.viewer.markers)
 
     def columnCount(self, parent):
-        return 2
+        return 3
 
     def markers_changed(self, istart, istop):
         self.beginInsertRows(QModelIndex(), istart, istop-1)
@@ -59,9 +59,29 @@ class MarkerModel(QAbstractTableModel):
             if index.column() == 1:
                 s = util.time_to_str(marker.tmin)
 
+
+            if index.column() == 2:
+                if isinstance(marker, EventMarker):
+                    s = str(marker.get_event().magnitude)
+                else:
+                    s = ''
+
             return QVariant(QString(s))
 
         return QVariant()
+
+    def setData(self, index, value, role):
+        if role == Qt.EditRole:
+            imarker = index.row()
+            marker = self.viewer.markers[imarker]
+            if index.column() == 3:
+                marker.get_event().magnitude = value
+
+    def flags(self, index):
+        if index.column() == 2:
+            return Qt.ItemFlags(35)
+        return Qt.ItemFlags(33)
+
 
 
 class MarkerEditor(QFrame):
@@ -71,9 +91,37 @@ class MarkerEditor(QFrame):
         layout = QGridLayout()
         self.setLayout(layout)
         self.marker_table = MarkerTable()
+
         self.marker_model = MarkerModel()
         self.marker_table.setModel(self.marker_model)
+        self.selection_model = QItemSelectionModel(self.marker_model)
+        self.marker_table.setSelectionModel(self.selection_model)
+        self.connect(
+            self.selection_model,
+            SIGNAL("selectionChanged(QItemSelection, QItemSelection)"),
+            self.set_selected_markers)
+
         layout.addWidget(self.marker_table, 0, 0)
+
+        self.viewer = None
 
     def set_viewer(self, viewer):
         self.marker_model.set_viewer(viewer)
+        self.viewer = viewer
+        self.connect(self.viewer, SIGNAL('changed_marker_selection'), self.update_selection_model)
+
+    def set_selected_markers(self, selected, deselected):
+        ''' set markers selected in viewer at selection in table.'''
+        selected_markers = [self.viewer.markers[i.row()] for i in self.selection_model.selectedRows()]
+        self.viewer.deselect_all()
+        self.viewer.set_selected_markers(selected_markers)
+
+    def update_selection_model(self, indices):
+        ''' :param indices: list of indices of selected markers.'''
+        self.selection_model.clearSelection()
+        for i in indices:
+            left = self.marker_model.index(i, 0)
+            right = self.marker_model.index(i, 2)
+            row_selection = QItemSelection(left, right)
+            row_selection.select(left, right)
+            self.selection_model.select(row_selection, QItemSelectionModel.SelectionFlags(2))
