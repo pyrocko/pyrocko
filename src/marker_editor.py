704146b98157071aee744aa892a44983c98f336d
Marius Kriegerowski <marius.kriegerowski@zmaw.de>
2015-07-29 18:00:04 +0200

    marker_menu: data mapping and cleanup

--- src/marker_editor.py
+++ src/marker_editor.py
@@ -11,13 +11,13 @@ _column_mapping = dict(zip(_header_data, range(len(_header_data))))
 _attr_mapping = dict(zip([0,1,2], ['__class__', 'tmin', 'magnitude']))
 
 class MarkerItemDelegate(QStyledItemDelegate):
+    """Takes are of the table's style."""
     def __init__(self, *args, **kwargs):
         QStyledItemDelegate.__init__(self, *args, **kwargs)
 
     def initStyleOption(self, option, index):
         if not index.isValid():
             return
-
         QStyledItemDelegate.initStyleOption(self, option, index)
         if index.row()%2==0:
             option.backgroundBrush = QBrush(QColor(50,10,10,30))
@@ -26,39 +26,13 @@ class MarkerSortFilterProxyModel(QSortFilterProxyModel):
     def __init__(self):
         QSortFilterProxyModel.__init__(self)
 
-    #def mapFromSource(self, i):
-    #    return i
-
-    #def sort(self, col, order):
-    #    self.emit(SIGNAL("layoutAboutToBeChanged()"))
-    #    if _attr_mapping[col]=='magnitude':
-    #        markers = self.viewer.markers
-    #        event_markers = []
-    #        other = []
-    #        for im, m in enumerate(markers):
-    #            if isinstance(m, EventMarker):
-    #                event_markers.append(m)
-    #            else:
-    #                other.append(m)
-    #        #event_markers = [markers.pop(markers.index(m)) for m in markers if isinstance(m, EventMarker)]
-    #        event_markers = sorted(event_markers,
-    #                               key=operator.attrgetter('_event.magnitude'), 
-    #                               reverse=order==Qt.DescendingOrder)
-
-    #        event_markers.extend(other)
-    #        self.viewer.markers = event_markers
-    #    else:
-    #        self.viewer.markers = sorted(self.viewer.markers, key=operator.attrgetter(_attr_mapping[col]))
-    #        if order == Qt.DescendingOrder:
-    #            self.viewer.markers.reverse()
-    #    self.emit(SIGNAL("layoutChanged()"))
-
 class MarkerTableView(QTableView):
     def __init__(self, *args, **kwargs):
         QTableView.__init__(self, *args, **kwargs)
 
         self.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.setSortingEnabled(True)
+        self.setAlternatingRowColors(True)
 
         self.setShowGrid(False)
         self.verticalHeader().hide()
@@ -88,7 +62,7 @@ class MarkerTableModel(QAbstractTableModel):
         self.viewer = viewer
         self.connect(self.viewer, SIGNAL('markers_added(int,int)'), self.markers_added)
         self.connect(self.viewer, SIGNAL('markers_removed(QList<int>)'), self.markers_removed)
-
+        
     def rowCount(self, parent):
         if not self.viewer:
             return 0
@@ -99,8 +73,10 @@ class MarkerTableModel(QAbstractTableModel):
 
     def markers_added(self, istart, istop):
         """Insert rows representing a :py:class:`Marker` in the :py:class:`MarkerTableModel`."""
-        self.beginInsertRows(QModelIndex(), istart+1, istop)
+        self.emit(SIGNAL('dataChanged'))
+        self.beginInsertRows(QModelIndex(), istart, istop)
         self.endInsertRows()
+        #self.emit(SIGNAL('dataChanged()'))
 
     def make_chunks(self, items):
         """Split a list of integers into sublists of consecutive elements."""
@@ -152,6 +128,8 @@ class MarkerTableModel(QAbstractTableModel):
         return QVariant()
 
     def setData(self, index, value, role):
+        """
+        """
         if role == Qt.EditRole:
             imarker = index.row()
             marker = self.viewer.markers[imarker]
@@ -173,20 +151,18 @@ class MarkerEditor(QTableWidget):
         layout = QGridLayout()
         self.setLayout(layout)
         self.marker_table = MarkerTableView()
-        self.marker_table.setItemDelegate(MarkerItemDelegate(self.marker_table))
+        #self.marker_table.setItemDelegate(MarkerItemDelegate(self.marker_table))
 
         self.marker_model = MarkerTableModel()
-        proxyModel = MarkerSortFilterProxyModel()
-        proxyModel.setDynamicSortFilter(True)
-        proxyModel.setSourceModel(self.marker_model)
-
-        delegate = MarkerItemDelegate()
+        self.proxy_filter = MarkerSortFilterProxyModel()
+        self.proxy_filter.setDynamicSortFilter(True)
+        self.proxy_filter.setSourceModel(self.marker_model)
 
         header = self.marker_table.horizontalHeader()
         header.setModel(self.marker_model)
-        self.marker_table.setModel(proxyModel)
+        self.marker_table.setModel(self.proxy_filter)
 
-        self.selection_model = QItemSelectionModel(proxyModel)
+        self.selection_model = QItemSelectionModel(self.proxy_filter)
         self.marker_table.setSelectionModel(self.selection_model)
         self.connect(
             self.selection_model,
@@ -200,11 +176,13 @@ class MarkerEditor(QTableWidget):
         self.marker_model.set_viewer(viewer)
         self.viewer = viewer
         self.connect(self.viewer, SIGNAL('changed_marker_selection'), self.update_selection_model)
+        #self.connect(self.marker_table, SIGNAL("cellDoubleClicked(int, int)"), self.show_details)
+
         self.marker_table.set_viewer(self.viewer)
 
     def set_selected_markers(self, selected, deselected):
         ''' set markers selected in viewer at selection in table.'''
-        selected_markers = [self.viewer.markers[i.row()] for i in self.selection_model.selectedRows()]
+        selected_markers = [self.viewer.markers[self.proxy_filter.mapToSource(i).row()] for i in self.selection_model.selectedRows()]
         self.viewer.set_selected_markers(selected_markers)
 
     def get_marker_model(self):
@@ -218,10 +196,14 @@ class MarkerEditor(QTableWidget):
         flag = QItemSelectionModel.SelectionFlags(2)
         selections = QItemSelection()
         for i in indices:
-            left = self.marker_model.index(i, 0)
-            right = self.marker_model.index(i, num_columns-1)
+            left = self.proxy_filter.mapFromSource(self.marker_model.index(i, 0))
+            right = self.proxy_filter.mapFromSource(self.marker_model.index(i, num_columns-1))
             row_selection = QItemSelection(left, right)
             row_selection.select(left, right)
             selections.merge(row_selection, flag)
         self.selection_model.select(selections, flag)
+        if len(indices)!=0:
+            self.marker_table.scrollTo(self.proxy_filter.mapFromSource(self.marker_model.index(indices[0],0)))
 
+    def show_details(self):
+        print 'show details'
