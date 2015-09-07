19854b769f24b992497944f44f2fac7870da85c3
Marius Kriegerowski <marius@gfz-potsdam.de>
2015-09-07 09:49:26 +0200

    pile_viewer: cast action name to string when sorting

--- src/pile_viewer.py
+++ src/pile_viewer.py
@@ -554,8 +554,7 @@ def sort_actions(menu):
     actions = menu.actions()
     for action in actions:
         menu.removeAction(action)
-
-    actions.sort(key=lambda x: x.text())
+    actions.sort(key=lambda x: str(x.text()))
 
     help_action = filter(lambda a : a.text()=="Snuffler Controls", actions)
     if help_action:
