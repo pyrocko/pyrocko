41d82af5071c9df9fcc8f15f8a4e5185d591e6ea
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-03-19 11:36:27 +0100

    evalresp: flake8

--- src/evalresp.py
+++ src/evalresp.py
@@ -1,29 +1,29 @@
 import time
 
+
 def evalresp(
-        sta_list = '*',
-        cha_list = '*',
-        net_code = '*',
-        locid    = '*',
-        instant  = None,
-        units    = 'VEL',         # VEL, DIS, ACC, DEF
-        file     = '',
-        freqs    = None,
-        rtype    = 'AP',          # CS, AP
-        verbose  = '',
-        start_stage = -1,
-        stop_stage = 0,
-        stdio_flag = 0,
-        listinterp_out_flag = 0,
-        listinterp_in_flag = 0,
-        listinterp_tension = 1000.0):
-  
+        sta_list='*',
+        cha_list='*',
+        net_code='*',
+        locid='*',
+        instant=None,
+        units='VEL',         # VEL, DIS, ACC, DEF
+        file='',
+        freqs=None,
+        rtype='AP',          # CS, AP
+        verbose='',
+        start_stage=-1,
+        stop_stage=0,
+        stdio_flag=0,
+        listinterp_out_flag=0,
+        listinterp_in_flag=0,
+        listinterp_tension=1000.0):
+
     from pyrocko import evalresp_ext as ext
     datime = time.strftime('%Y,%j,%H:%M:%S', time.gmtime(instant))
-    
+
     return ext.evalresp(sta_list, cha_list, net_code, locid, datime,
                         units, file, freqs, rtype, verbose,
                         start_stage, stop_stage, stdio_flag,
                         listinterp_out_flag, listinterp_in_flag,
                         listinterp_tension)
-
