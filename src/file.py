0d8f298e27bd1536acb86f7e9718f50864bdc041
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-04-02 10:37:39 +0200

    file: flake8

--- src/file.py
+++ src/file.py
@@ -1,12 +1,13 @@
 
 # The container format:
-# A file consists of records. 
-# A record consists of a record header a record payload, and possibly padding.
-# A record header consists of a label, a version, the record size, the payload size, a hash, and a record type.
-# A record payload consists of a sequence of record entries.
-# A record entry consists of a key, a type, and a value.
+# * A file consists of records.
+# * A record consists of a record header a record payload, and possibly
+#   padding.
+# * A record header consists of a label, a version, the record size, the
+#   payload size, a hash, and a record type.
+# * A record payload consists of a sequence of record entries.
+# * A record entry consists of a key, a type, and a value.
 
-import os, sys
 from struct import unpack, pack
 from cStringIO import StringIO
 import numpy as num
@@ -26,86 +27,108 @@ size_record_header = 64
 no_hash = '\0' * 20
 
 numtypes = {
-        '@i2': (num.int16, '>i2'),
-        '@i4': (num.int32, '>i4'),
-        '@i8': (num.int64, '>i8'),
-        '@u2': (num.uint16, '>u2'),
-        '@u4': (num.uint32, '>u4'),
-        '@u8': (num.uint64, '>u8'),
-        '@f4': (num.float32, '>f4'),
-        '@f8': (num.float64, '>f8'),
-    }
+    '@i2': (num.int16, '>i2'),
+    '@i4': (num.int32, '>i4'),
+    '@i8': (num.int64, '>i8'),
+    '@u2': (num.uint16, '>u2'),
+    '@u4': (num.uint32, '>u4'),
+    '@u8': (num.uint64, '>u8'),
+    '@f4': (num.float32, '>f4'),
+    '@f8': (num.float64, '>f8'),
+}
 
-numtype2type = dict( [ (v[0],k) for (k,v) in numtypes.iteritems() ] )
+numtype2type = dict([(v[0], k) for (k, v) in numtypes.iteritems()])
+
 
 def packer(fmt):
     return ((lambda x: pack('>'+fmt, x)), (lambda x: unpack('>'+fmt, x)[0]))
 
+
 def unpack_array(fmt, data):
-    return num.fromstring(data, dtype=numtypes[fmt][1]).astype(numtypes[fmt][0])
+    return num.fromstring(
+        data, dtype=numtypes[fmt][1]).astype(numtypes[fmt][0])
+
 
 def pack_array(fmt, data):
     return data.astype(numtypes[fmt][1]).tostring()
 
 
 def array_packer(fmt):
-    return ((lambda x: pack_array(fmt, x)), (lambda x: unpack_array(fmt, x))) 
+    return ((lambda x: pack_array(fmt, x)), (lambda x: unpack_array(fmt, x)))
+
 
 def encoding_packer(enc):
     return ((lambda x: x.encode(enc)), (lambda x: x.decode(enc)))
 
+
 def noop(x):
     return x
 
+
 def time_to_str_ns(x):
     return util.time_to_str(x, format=9)
-    
+
 
 castings = {
-        'i2': packer('h'),
-        'i4': packer('i'),
-        'i8': packer('q'),
-        'u2': packer('H'),
-        'u4': packer('I'),
-        'u8': packer('Q'),
-        'f4': packer('f'),
-        'f8': packer('d'),
-        'string': (noop, noop),
-        'time_string': (time_to_str_ns, util.str_to_time),
-        '@i2': array_packer('@i2'),
-        '@i4': array_packer('@i4'),
-        '@i8': array_packer('@i8'),
-        '@u2': array_packer('@u2'),
-        '@u4': array_packer('@u4'),
-        '@u8': array_packer('@u8'),
-        '@f4': array_packer('@f4'),
-        '@f8': array_packer('@f8'),
-    }
+    'i2': packer('h'),
+    'i4': packer('i'),
+    'i8': packer('q'),
+    'u2': packer('H'),
+    'u4': packer('I'),
+    'u8': packer('Q'),
+    'f4': packer('f'),
+    'f8': packer('d'),
+    'string': (noop, noop),
+    'time_string': (time_to_str_ns, util.str_to_time),
+    '@i2': array_packer('@i2'),
+    '@i4': array_packer('@i4'),
+    '@i8': array_packer('@i8'),
+    '@u2': array_packer('@u2'),
+    '@u4': array_packer('@u4'),
+    '@u8': array_packer('@u8'),
+    '@f4': array_packer('@f4'),
+    '@f8': array_packer('@f8'),
+}
+
 
 def pack_value(type, value):
     try:
         return castings[type][0](value)
     except Exception, e:
-        raise FileError('Packing value failed (type=%s, value=%s, error=%s).' % (type,str(value)[:500], e))
-    
+        raise FileError(
+            'Packing value failed (type=%s, value=%s, error=%s).' %
+            (type, str(value)[:500], e))
+
+
 def unpack_value(type, value):
     try:
         return castings[type][1](value)
     except Exception, e:
-        raise FileError('Unpacking value failed (type=%s, error=%s).' % (type, e))
+        raise FileError(
+            'Unpacking value failed (type=%s, error=%s).' % (type, e))
+
 
 class FileError(Exception):
     pass
 
+
 class NoDataAvailable(Exception):
     pass
+
 
 class WrongRecordType(Exception):
     pass
 
+
+class MissingRecordValue(Exception):
+    pass
+
+
 class Record:
-    
-    def __init__(self, parent, mode, size_record, size_payload, hash, type, format, do_hash):
+    def __init__(
+            self, parent, mode, size_record, size_payload, hash, type, format,
+            do_hash):
+
         self.mode = mode
         self.size_record = size_record
         self.size_payload = size_payload
@@ -117,8 +140,9 @@ class Record:
             self._out = StringIO()
         else:
             self.size_remaining = self.size_record - size_record_header
-            self.size_padding = self.size_record - size_record_header - self.size_payload
-        
+            self.size_padding = self.size_record - size_record_header - \
+                self.size_payload
+
         self._f = parent._f
         self._parent = parent
         self._hasher = None
@@ -126,126 +150,133 @@ class Record:
         if do_hash and (self.mode == 'w' or self.hash):
             self._hasher = sha1()
         self._closed = False
-        
+
     def read(self, n=None):
-        
+
         assert not self._closed
         assert self.mode == 'r'
-        
-        if n == None:
+
+        if n is None:
             n = self.size_payload
-        
+
         n = min(n, self.size_remaining - self.size_padding)
         data = self._f.read(n)
         self.size_remaining -= len(data)
-        
+
         if len(data) != n:
             raise FileError('Read returned less data than expected.')
-        
+
         if self._hasher:
             self._hasher.update(data)
-            
+
         return data
-        
+
     def write(self, data):
         assert not self._closed
         assert self.mode == 'w'
-        
+
         self._out.write(data)
         if self._hasher:
             self._hasher.update(data)
-            
+
         self.size_payload += len(data)
-        
+
     def seek(self, n, whence):
         assert not self._closed
         assert self.mode == 'r'
         assert whence == SEEK_CUR
         assert n >= 0
-        
+
         n = min(n, self.size_remaining - self.size_padding)
         self._f.seek(n, whence)
         self._hasher = None
         self.size_remaining -= n
-        
+
     def skip(self, n):
         self.seek(n, SEEK_CUR)
-        
+
     def close(self):
         if self._closed:
             return
-        
+
         if self.mode == 'r':
             if self._hasher and self._hasher.digest() != self.hash:
                 self.read(self.size_remaining)
-                raise FileError('Hash computed from record data does not match value given in header.')
+                raise FileError(
+                    'Hash computed from record data does not match value '
+                    'given in header.')
             else:
                 self.seek(self.size_remaining, SEEK_CUR)
-        
+
             if self.size_padding:
                 self._f.seek(self.size_padding, SEEK_CUR)
         else:
-            if self.size_record is not None and self.size_payload > self.size_record - size_record_header:
-                raise FileError('Too much data to fit into size-limited record.')
-            
+            if self.size_record is not None and \
+                    self.size_payload > self.size_record - size_record_header:
+
+                raise FileError(
+                    'Too much data to fit into size-limited record.')
+
             if self.size_record is None:
                 self.size_record = self.size_payload + size_record_header
-            
-            self.size_padding = self.size_record - self.size_payload - size_record_header
-               
+
+            self.size_padding = self.size_record - self.size_payload - \
+                size_record_header
+
             if self._hasher is not None:
                 self.hash = self._hasher.digest()
-            
-            
-            self._parent.write_record_header(self.size_record, self.size_payload, self.hash, self.type)
-            self._f.write( self._out.getvalue() )
+
+            self._parent.write_record_header(
+                self.size_record, self.size_payload, self.hash, self.type)
+
+            self._f.write(self._out.getvalue())
             self._out.close()
-            self._f.write( '\0' * self.size_padding )
-        
+            self._f.write('\0' * self.size_padding)
+
         self._closed = True
         self._parent = None
         self._f = None
-        
+
     def entries(self):
-        
+
         sizes = []
         sum = 0
         while sum < self.size_payload:
             size = unpack('>Q', self.read(8))[0]
             sum += size + 8
             sizes.append(size)
-        
+
         n = len(sizes)/3
         keys = []
-        keys = [ self.read(sizes[j]) for j in xrange(n) ]
-        types = [ self.read(sizes[j]) for j in xrange(n,2*n) ]
+        keys = [self.read(sizes[j]) for j in xrange(n)]
+        types = [self.read(sizes[j]) for j in xrange(n, 2*n)]
         for key, type, j in zip(keys, types, xrange(2*n, 3*n)):
             yield key, type, sizes[j]
-       
+
     def unpack(self, exclude=None):
-        
+
         d = {}
         for key, type, size in self.entries():
             if self.format[key] != type:
                 FileError('Record value in unexpected format.')
-            
+
             if not exclude or key not in exclude:
                 d[key] = unpack_value(type, self.read(size))
             else:
                 self.skip(size)
                 d[key] = None
-    
+
         for key in self.format:
             if key not in d:
                 raise FileError('Missing record entry: %s.' % key)
-            
+
         return d
-        
+
     def pack(self, d):
         for key in self.format:
             if key not in d:
                 raise MissingRecordValue()
-            
+
         keys = []
         types = []
         values = []
@@ -254,24 +285,29 @@ class Record:
                 type = self.format[key]
                 if isinstance(type, tuple):
                     type = self._parent.get_type(key, d[key])
-                
+
                 keys.append(key)
                 types.append(type)
                 values.append(pack_value(type, d[key]))
-        
-        sizes = [ len(x) for x in keys+types+values ]
-        
+
+        sizes = [len(x) for x in keys+types+values]
+
         self.write(pack('>%iQ' % len(sizes), *sizes))
         for x in keys+types+values:
-            self.write(x)        
-        
+            self.write(x)
+
+
 class File:
-    
-    def __init__(self, f, type_label='TEST', version='0000', record_formats={}):
-        
+
+    def __init__(
+            self, f,
+            type_label='TEST',
+            version='0000',
+            record_formats={}):
+
         assert len(type_label) == 4
         assert len(version) == 4
-        
+
         self._file_type_label = type_label
         self._file_version = version
         self._record_formats = record_formats
@@ -280,54 +316,64 @@ class File:
 
     def read_record_header(self):
         data = self._f.read(size_record_header)
-        
+
         if len(data) == 0:
             raise NoDataAvailable()
-        
+
         if len(data) != size_record_header:
             raise FileError('Read returned less data than expected.')
-        
-        label, version, size_record, size_payload, hash, type = unpack('>4s4sQQ20s20s', data)
-    
+
+        label, version, size_record, size_payload, hash, type = unpack(
+            '>4s4sQQ20s20s', data)
+
         if label != self._file_type_label:
             raise FileError('record file type label missing.')
-        
+
         if version != self._file_version:
             raise FileError('file version %s not supported.' % version)
-    
+
         type = type.rstrip()
-        
+
         if hash == no_hash:
             hash = None
-    
+
         return size_record, size_payload, hash, type
-    
+
     def write_record_header(self, size_record, size_payload, hash, type):
         if hash is None:
             hash = no_hash
-        data = pack('>4s4sQQ20s20s', self._file_type_label,
-             self._file_version, size_record, size_payload, hash, type.ljust(20)[:20])
+        data = pack(
+            '>4s4sQQ20s20s',
+            self._file_type_label,
+            self._file_version,
+            size_record,
+            size_payload,
+            hash,
+            type.ljust(20)[:20])
+
         self._f.write(data)
-    
+
     def next_record(self, check_hash=False):
         if self._current_record:
             self._current_record.close()
-            
+
         size_record, size_payload, hash, type = self.read_record_header()
         format = self._record_formats[type]
-        self._current_record = Record(self, 'r', size_record, size_payload, hash, type, format, check_hash)
+        self._current_record = Record(
+            self, 'r', size_record, size_payload, hash, type, format,
+            check_hash)
+
         return self._current_record
-    
+
     def add_record(self, type, size_record=None, make_hash=False):
         if self._current_record:
             self._current_record.close()
-        
+
         format = self._record_formats[type]
-        self._current_record = Record(self, 'w', size_record, 0, None, type, format, make_hash)
+        self._current_record = Record(
+            self, 'w', size_record, 0, None, type, format, make_hash)
         return self._current_record
-        
+
     def close(self):
         if self._current_record:
             self._current_record.close()
-            
-            
