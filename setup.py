61b1b88e05b8b3da50fd95e65aaf1730bc4fec7b
miili <isken@gfz-potsdam.de>
2016-07-26 14:59:20 +0200

    Update to bash_completion, snuffler now completes

--- setup.py
+++ setup.py
@@ -402,7 +402,7 @@ setup(
         'apps/gmtpy-epstopdf',
         'apps/automap'],
 
-    data_files=[('/etc/bash_completion.d', ['extras/fomosto'])],
+    data_files=[('/etc/bash_completion.d', ['extras/pyrocko'])],
 
     package_data={
         packname: ['data/*.png', 'data/*.html', 'data/earthmodels/*.nd',
