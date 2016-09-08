e798ff9884ac278ae3a8d3c44609e68be589388c
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-09-08 10:40:30 +0200

    mseed: flake8

--- src/mseed.py
+++ src/mseed.py
@@ -1,5 +1,6 @@
 from struct import unpack
-import os, re
+import os
+import re
 import logging
 
 from pyrocko import trace
@@ -8,8 +9,10 @@ from pyrocko.io_common import FileLoadError, FileSaveError
 
 logger = logging.getLogger('pyrocko.mseed')
 
+
 class CodeTooLong(FileSaveError):
     pass
+
 
 def iload(filename, load_data=True):
     from pyrocko import mseed_ext
@@ -17,7 +20,7 @@ def iload(filename, load_data=True):
     have_zero_rate_traces = False
     try:
         traces = []
-        for tr in mseed_ext.get_traces( filename, load_data ):
+        for tr in mseed_ext.get_traces(filename, load_data):
             network, station, location, channel = tr[1:5]
             tmin = float(tr[5])/float(mseed_ext.HPTMODULUS)
             tmax = float(tr[6])/float(mseed_ext.HPTMODULUS)
@@ -28,31 +31,35 @@ def iload(filename, load_data=True):
                 continue
 
             ydata = tr[8]
-            
-            traces.append(trace.Trace(network, station, location, channel, tmin, tmax, deltat, ydata))
-        
+
+            traces.append(trace.Trace(
+                network, station, location, channel, tmin, tmax,
+                deltat, ydata))
+
         for tr in traces:
             yield tr
-    
+
     except (OSError, mseed_ext.MSeedError), e:
         raise FileLoadError(str(e))
 
     if have_zero_rate_traces:
-        logger.warn('Ignoring traces with sampling rate of zero in file %s (maybe LOG traces)' % filename)
-    
+        logger.warn(
+            'Ignoring traces with sampling rate of zero in file %s '
+            '(maybe LOG traces)' % filename)
+
+
 def as_tuple(tr):
     from pyrocko import mseed_ext
     itmin = int(round(tr.tmin*mseed_ext.HPTMODULUS))
     itmax = int(round(tr.tmax*mseed_ext.HPTMODULUS))
     srate = 1.0/tr.deltat
-    return (tr.network, tr.station, tr.location, tr.channel, 
+    return (tr.network, tr.station, tr.location, tr.channel,
             itmin, itmax, srate, tr.get_ydata())
-
 
 
 def save(traces, filename_template, additional={}, overwrite=True):
     from pyrocko import mseed_ext
-            
+
     fn_tr = {}
     for tr in traces:
         for code, maxlen, val in zip(
@@ -62,8 +69,8 @@ def save(traces, filename_template, additional={}, overwrite=True):
 
             if len(val) > maxlen:
                 raise CodeTooLong(
-                        '%s code too long to be stored in MSeed file: %s' % 
-                        (code, val))
+                    '%s code too long to be stored in MSeed file: %s' %
+                    (code, val))
 
         fn = tr.fill_template(filename_template, **additional)
         if not overwrite and os.path.exists(fn):
@@ -71,31 +78,34 @@ def save(traces, filename_template, additional={}, overwrite=True):
 
         if fn not in fn_tr:
             fn_tr[fn] = []
-        
+
         fn_tr[fn].append(tr)
-        
+
     for fn, traces_thisfile in fn_tr.items():
         trtups = []
-        traces_thisfile.sort(lambda a,b: cmp(a.full_id, b.full_id))
+        traces_thisfile.sort(lambda a, b: cmp(a.full_id, b.full_id))
         for tr in traces_thisfile:
             trtups.append(as_tuple(tr))
-        
+
         ensuredirs(fn)
         try:
             mseed_ext.store_traces(trtups, fn)
         except mseed_ext.MSeedError, e:
-            raise FileSaveError( str(e) + ' (while storing traces to file \'%s\')' % fn)
-            
+            raise FileSaveError(
+                str(e) + ' (while storing traces to file \'%s\')' % fn)
+
     return fn_tr.keys()
 
 tcs = {}
+
+
 def detect(first512):
 
     if len(first512) < 256:
         return False
 
     rec = first512
-    
+
     try:
         sequence_number = int(rec[:6])
     except:
@@ -121,22 +131,15 @@ def detect(first512):
         if all(bads):
             return False
 
-        #nblockettes = vals[-4]
-        #offset_next_blockette = vals[-1]
-        #if offset_next_blockette + 8 < len(rec):
-        #    fmt_dr_head = '>2H4b'
-        #    vals_dr_head = unpack(fmt_dr_head, rec[offset_next_blockette:offset_next_blockette+8])
-        #    blockette_type, offset_next_blockette, encoding, word_order, data_record_length_exponent, reserved = vals_dr_head
-            
     else:
-        if not type_code in 'VAST':
+        if type_code not in 'VAST':
             return False
 
         continuation_code = rec[7]
         if continuation_code != ' ':
             return False
 
-        blockette_type  = rec[8:8+3]
+        blockette_type = rec[8:8+3]
         if not re.match(r'^\d\d\d$', blockette_type):
             return False
 
