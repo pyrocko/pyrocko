964d61bf2b567cba7798cad0f2550cbb8d31e935
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-09-14 14:53:07 +0200

    config: pyrocko and snuffler have separate config files. New VisibleLengthSetting.

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from os.path import expanduser, expandvars
 from copy import deepcopy
 
 from pyrocko import util
-from pyrocko.guts import Object, Float, String, load, dump, List, Dict
+from pyrocko.guts import Object, Float, String, load, dump, List, Dict, TBase
 
 guts_prefix = 'pf'
 
@@ -12,7 +12,8 @@ pyrocko_dir_tmpl = os.environ.get(
     'PYROCKO_DIR',
     os.path.join('~', '.pyrocko'))
 
-conf_path_tmpl = os.path.join(pyrocko_dir_tmpl, 'config.pf')
+conf_path_tmpl = os.path.join(pyrocko_dir_tmpl, 'config{module}.pf')
+
 default_phase_key_mapping = {
     'F1': 'P', 'F2': 'S', 'F3': 'R', 'F4': 'Q', 'F5': '?'}
 
@@ -26,15 +27,43 @@ class PathWithPlaceholders(String):
     pass
 
 
+class VisibleLengthSetting(Object):
+    class __T(TBase):
+        def regularize_extra(self, val):
+            if isinstance(val, list):
+                return self.cls(key=val[0], value=val[1])
+
+            return val
+
+        def to_save(self, val):
+            return (val.key, val.value)
+
+        def to_save_xml(self, val):
+            raise NotImplementedError()
+
+    key = String.T()
+    value = Float.T()
+
+
+class SnufflerConfig(Object):
+    visible_length_setting = List.T(
+        VisibleLengthSetting.T(),
+        default=[VisibleLengthSetting(key='Short', value=6000.),
+                 VisibleLengthSetting(key='Medium', value=20000.),
+                 VisibleLengthSetting(key='Long', value=60000.)])
+    phase_key_mapping = Dict.T(
+        String.T(), String.T(), default=default_phase_key_mapping)
+
+    def get_phase_name(self, key):
+        return self.phase_key_mapping.get('F%s' % key, 'Undefined')
+
+
 class PyrockoConfig(Object):
     cache_dir = PathWithPlaceholders.T(
         default=os.path.join(pyrocko_dir_tmpl, 'cache'))
     earthradius = Float.T(default=6371.*1000.)
     gf_store_dirs = List.T(PathWithPlaceholders.T())
     gf_store_superdirs = List.T(PathWithPlaceholders.T())
-    phase_key_mapping = Dict.T(
-        String.T(), String.T(), default=default_phase_key_mapping)
-    visible_length_default = String.T(default='Short')
     topo_dir = PathWithPlaceholders.T(
         default=os.path.join(pyrocko_dir_tmpl, 'topo'))
     geonames_dir = PathWithPlaceholders.T(
@@ -44,12 +73,11 @@ class PyrockoConfig(Object):
     leapseconds_url = String.T(
         default='http://www.ietf.org/timezones/data/leap-seconds.list')
 
-    @property
-    def visible_length_options(self):
-        return {'Short': 6000, 'Medium': 20000, 'Long': 60000}
 
-    def get_phase_name(self, key):
-        return self.phase_key_mapping.get('F%s' % key, 'Undefined')
+def fill_template(tmpl, config_type):
+    tmpl = tmpl .format(
+        module=('.' + config_type) if config_type != 'pyrocko' else '')
+    return tmpl
 
 
 def expand(x):
@@ -90,34 +118,40 @@ def mtime(p):
 g_conf_mtime = None
 g_conf = None
 
+configs = {'pyrocko': PyrockoConfig,
+           'snuffler': SnufflerConfig}
 
-def raw_config():
+
+def raw_config(config_type):
     global g_conf
     global g_conf_mtime
 
     conf_path = expand(conf_path_tmpl)
+    conf_path = fill_template(conf_path, config_type)
 
     if not os.path.exists(conf_path):
-        g_conf = PyrockoConfig()
-        write_config(g_conf)
+        g_conf = configs[config_type]()
+        write_config(g_conf, config_type)
 
     conf_mtime_now = mtime(conf_path)
     if conf_mtime_now != g_conf_mtime:
         g_conf = load(filename=conf_path)
-        if not isinstance(g_conf, PyrockoConfig):
+        if not isinstance(g_conf, configs[config_type]):
             raise BadConfig('config file does not contain a '
-                            'valid "pf.PyrockoConfig" section.')
+                            'valid {config_cls} section.'
+                            .format(configs[config_type].__class__.__name__))
 
         g_conf_mtime = conf_mtime_now
 
     return g_conf
 
 
-def config():
-    return processed(raw_config())
+def config(config_type='pyrocko'):
+    return processed(raw_config(config_type))
 
 
-def write_config(conf):
+def write_config(conf, config_type):
     conf_path = expand(conf_path_tmpl)
+    conf_path = fill_template(conf_path, config_type)
     util.ensuredirs(conf_path)
     dump(conf, filename=conf_path)
