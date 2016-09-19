b0df904a883333500351123c9a50d6014af629b8
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-09-19 11:43:12 +0200

    snufflings: flake8

--- src/snufflings/__init__.py
+++ src/snufflings/__init__.py
@@ -1,15 +1,15 @@
-from pyrocko.snufflings import minmax, rms, stalta, geofon, ampspec, catalogs, download
+from pyrocko.snufflings import minmax, rms, stalta, geofon, ampspec, \
+    catalogs, download
 modules = [minmax, rms, download, stalta, geofon, ampspec, catalogs]
+
 
 def __snufflings__():
     snufflings = []
     for mod in modules:
         snufflings.extend(mod.__snufflings__())
-        
+
     for snuffling in snufflings:
         snuffling.setup()
-        snuffling.set_name( snuffling.get_name() + ' (builtin)' )
+        snuffling.set_name(snuffling.get_name() + ' (builtin)')
 
     return snufflings
-    
-    
