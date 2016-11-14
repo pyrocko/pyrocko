1b11a3d5ad3df3f92bd3857cfdea1725cdaca409
miili <isken@gfz-potsdam.de>
2016-11-14 16:17:25 +0100

    Pimped orthodrome testing for _ext modules / added benchmarking decorator

--- test/common.py
+++ test/common.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pyrocko import util
 
 
@@ -9,3 +10,13 @@ def test_data_file(fn):
         util.download_file(url, fpath)
 
     return fpath
+
+
+def benchmark(func):
+    def stopwatch(*args):
+        t0 = time.time()
+        result = func(*args)
+        elapsed = time.time() - t0
+        util.logger.info('%s executed in %.8f' % (func.__name__, elapsed))
+        return result
+    return stopwatch
