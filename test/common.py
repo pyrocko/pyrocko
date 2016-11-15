ea4a26c58719abab08b598209d438ae6bc0aac9d
miili <isken@gfz-potsdam.de>
2016-11-15 10:32:18 +0100

    Pimped benchmarking!

--- test/common.py
+++ test/common.py
@@ -2,6 +2,8 @@ import os
 import time
 from pyrocko import util
 
+benchmark_results = []
+
 
 def test_data_file_no_download(fn):
     return os.path.join(os.path.split(__file__)[0], 'data', fn)
@@ -16,11 +18,26 @@ def test_data_file(fn):
     return fpath
 
 
-def benchmark(func):
-    def stopwatch(*args):
-        t0 = time.time()
-        result = func(*args)
-        elapsed = time.time() - t0
-        util.logger.info('%s executed in %.8f' % (func.__name__, elapsed))
-        return result
-    return stopwatch
+class Benchmark(object):
+    def __init__(self, prefix=''):
+        self.prefix = prefix
+        self.results = []
+
+    def __call__(self, func):
+        def stopwatch(*args):
+            t0 = time.time()
+            name = self.prefix + func.__name__
+            result = func(*args)
+            elapsed = time.time() - t0
+            self.results.append((name, elapsed))
+            return result
+        return stopwatch
+
+    def __str__(self):
+        rstr = ['Benchmark results']
+        indent = max([len(name) for name, _ in self.results])
+        rstr.append('=' * (indent + 17))
+        for res in self.results:
+            rstr.append(
+                '{0:<{indent}}{1:.8f} s'.format(*res, indent=indent+5))
+        return '\n'.join(rstr)
