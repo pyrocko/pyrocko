e1b28a598381f9ba9f404deee298ab091caae63d
Henriette Sudhaus <hsudhaus@posteo.de>
2016-11-18 14:10:13 +0100

    more docs

--- src/gf/meta.py
+++ src/gf/meta.py
@@ -478,6 +478,8 @@ class Location(Object):
     def distance_to(self, other):
         '''
         Compute surface distance [m] to other location object.
+
+
         '''
 
         if self.same_origin(other):
@@ -500,6 +502,13 @@ class Location(Object):
     def distance_3d_to(self, other):
         '''
         Compute 3D distance [m] to other location object.
+
+        All coordinates are transformed to cartesian coordinates if necessary
+        then distance is:
+
+        .. math::
+
+        |Delta = \\sqrt{\Delta \\bf{x}^2 + \Delta \\bf{y}^2 + \Delta \\bf{z}^2}
         '''
 
         if self.same_origin(other):
