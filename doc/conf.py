a035593f70b6bd3434fa672898e686938902a9b2
miili <isken@gfz-potsdam.de>
2016-11-21 17:08:38 +0100

    using `sphinx.imgmath` instead of deprecated `sphinx.pngmath`

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ from pyrocko import version as pyrocko_version
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
-'sphinx.ext.pngmath', # 'sphinx.ext.jsmath', 
+'sphinx.ext.imgmath', # 'sphinx.ext.jsmath', 
 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
