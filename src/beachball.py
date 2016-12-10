707daa88a2f2d6c85a636b5d9cf35a7d9e00e7fe
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-12-10 17:22:34 +0100

    beachball: fix winding determination

--- src/beachball.py
+++ src/beachball.py
@@ -56,7 +56,8 @@ def circulation(points, axis):
         (points2[1:, 0] - points2[:-1, 0]) *
         (points2[1:, 1] + points2[:-1, 1]))
 
-    result -= (points2[0, 0] - points[-1, 0]) * points[0, 1] + points[-1, 1]
+    result -= (points2[0, 0] - points2[-1, 0]) \
+        * (points2[0, 1] + points2[-1, 1])
     return result
 
 
