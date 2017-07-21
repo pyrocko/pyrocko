d0540618daed42ccf96874a4518a08a730a5d805
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2017-07-21 12:00:51 +0200

    test_rdseed

---
+++ test/test_rdseed.py
@@ -0,0 +1,19 @@
+from pyrocko import rdseed
+from pyrocko import util
+import unittest
+import common
+
+
+class RDSeedTestCase(unittest.TestCase):
+
+    def test_read(self):
+        fpath = common.test_data_file('test_stations.dseed')
+        dseed = rdseed.SeedVolumeAccess(fpath)
+        pyrocko_stations = dseed._get_stations_from_file()
+        assert(len(pyrocko_stations) == 1)
+        assert(dseed.get_pile().is_empty())
+
+
+if __name__ == "__main__":
+    util.setup_logging('test_rdseed', 'warning')
+    unittest.main()
