1b7b18ed792cb77339b5490132db2354a0912187
miili <isken@gfz-potsdam.de>
2017-07-28 15:21:06 +0200

    tst travis deploy

--- maintenance/anaconda_deploy.sh
+++ maintenance/anaconda_deploy.sh
@@ -11,12 +11,13 @@ else
     MINICONDA_FILE=Miniconda3-latest-Linux-x86_64.sh
 fi
 
-# wget $MINICONDA_URL/$MINICONDA_FILE -O miniconda.sh
-# chmod +x miniconda.sh
-# ./miniconda.sh -b
+wget $MINICONDA_URL/$MINICONDA_FILE -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
 export PATH=$PATH:$CONDA_PREFIX/bin
 
 conda install -y conda-build anaconda-client
+anaconda-client login --username $ANACONDA_USER --password $ANACONDA_PASSWORD
 
 mkdir pyrocko
 cp anaconda-meta.yaml pyrocko/meta.yaml
