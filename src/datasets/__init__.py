df88915e71bd0d1ccfb6158377f64528f5053104
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2017-08-01 16:39:15 +0200

    relative import

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,4 +2,4 @@
 #
 # The Pyrocko Developers, 21st Century
 # ---|P------/S----------~Lg----------
-from tectonics import PeterBird2003, GSRM1  # noqa
+from .tectonics import PeterBird2003, GSRM1  # noqa
