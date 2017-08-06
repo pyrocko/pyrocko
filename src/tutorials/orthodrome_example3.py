6362d6cd334479c0fb986639a8139bba0e8e2894
Sebastian Heimann <Sebastian Heimann sebastian.heimann@gfz-potsdam.de>
2017-08-06 22:01:42 +0200

    tutorials: avoid using integers where floats are expected

--- src/tutorials/orthodrome_example3.py
+++ src/tutorials/orthodrome_example3.py
@@ -9,11 +9,11 @@ import numpy as num  # noqa
 
 ncoords = 1000
 # First set of coordinates
-lats_a = num.random.random_integers(-180, 180, ncoords)
-lons_a = num.random.random_integers(-90, 90, ncoords)
+lats_a = num.random.uniform(-180., 180., ncoords)
+lons_a = num.random.uniform(-90., 90., ncoords)
 
 # Second set of coordinates
-lats_b = num.random.random_integers(-180, 180, ncoords)
-lons_b = num.random.random_integers(-90, 90, ncoords)
+lats_b = num.random.uniform(-180., 180., ncoords)
+lons_b = num.random.uniform(-90., 90., ncoords)
 
 orthodrome.azibazi_numpy(lats_a, lons_a, lats_b, lons_b)
