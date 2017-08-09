415878f1f93452cb43994050331ac4ba2e62a23a
Sebastian Heimann <Sebastian Heimann sebastian.heimann@gfz-potsdam.de>
2017-08-09 22:54:40 +0200

    test runner: add option --coverage

--- test/run.py
+++ test/run.py
@@ -1,3 +1,5 @@
+import sys
+import argparse
 import unittest
 import pyrocko.util
 from os import path
@@ -20,6 +22,39 @@ all = get_test_suite()
 
 gf = get_test_suite(pattern='test_gf_*.py')
 
+trace = get_test_suite(pattern='test_trace.py')
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        description='Pyrocko test runner '
+                    '(for unittest standard options, see below)')
+
+    parser.add_argument(
+        '--coverage', action='store_true',
+        help='Measure coverage of tests')
+    parser.add_argument(
+        '-h', '--help', action='store_true',
+        help='Show help messages and exit')
+    options, args = parser.parse_known_args()
+
+    argv = [sys.argv[0]] + args
+    if options.help:
+        parser.print_help()
+        argv.append('-h')
+        print()
+
     pyrocko.util.setup_logging('test_all', 'warning')
-    unittest.main()
+
+    if options.coverage and not options.help:
+        import coverage
+        cov = coverage.Coverage(source=['pyrocko'])
+        cov.start()
+
+    unittest.main(argv=argv, exit=False)
+
+    if options.coverage and not options.help:
+        cov.stop()
+        cov.save()
+        cov.report()
+        cov.html_report()
