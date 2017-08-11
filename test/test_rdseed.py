96674dd5fb3bdc96c5920639b4af3b072b0928e2
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2017-08-11 16:01:03 +0200

    test_rdseed: more testing

--- test/test_rdseed.py
+++ test/test_rdseed.py
@@ -1,9 +1,12 @@
 from __future__ import division, print_function, absolute_import
+import os
+import tempfile
 import unittest
 from . import common
 
 from pyrocko import util, trace
 from pyrocko.io import rdseed
+from pyrocko.io import eventdata
 
 
 class RDSeedTestCase(unittest.TestCase):
@@ -22,6 +25,22 @@ class RDSeedTestCase(unittest.TestCase):
         assert(isinstance(dseed.get_pyrocko_response(tr, target='dis'),
                           trace.Evalresp))
 
+        st = dseed.get_pyrocko_station(tr)
+        assert(st.network == tr.network)
+        assert(st.network == tr.network)
+
+    def test_problems(self):
+        p = eventdata.Problems()
+        p.add('X', 'y')
+        fn = tempfile.mkstemp()[1]
+        p.dump(fn)
+
+        pl = eventdata.Problems()
+        pl.load(fn)
+
+        os.remove(fn)
+
+        assert p.__dict__ == pl.__dict__
 
 if __name__ == '__main__':
     util.setup_logging('test_rdseed', 'warning')
