41ca3b7a9bd1d9c782d34eb7b02059089fc7e7eb
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-10-25 16:59:59 +0200

    update deps install scripts

--- prerequisites/prerequisites_debian_python2.sh
+++ prerequisites/prerequisites_debian_python2.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sudo apt-get install make git python-dev python-setuptools
-sudo apt-get install python-numpy python-numpy-dev python-scipy python-matplotlib
-sudo apt-get install python-pyqt5 python-pyqt5.qtwebengine python-pyqt5.qtopengl python-pyqt5.qtsvg
-sudo apt-get install python-future python-requests
-sudo apt-get install python-yaml python-progressbar python-jinja2
+sudo apt-get update
+sudo apt-get install -y make git python-dev python-setuptools
+sudo apt-get install -y python-numpy python-numpy-dev python-scipy python-matplotlib
+sudo apt-get install -y python-pyqt5 python-pyqt5.qtwebengine python-pyqt5.qtopengl python-pyqt5.qtsvg
+sudo apt-get install -y python-future python-requests
+sudo apt-get install -y python-yaml python-progressbar python-jinja2
--- prerequisites/prerequisites_debian_python3.sh
+++ prerequisites/prerequisites_debian_python3.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install make git python3-dev python3-setuptools
-sudo apt-get install python3-numpy python3-numpy-dev python3-scipy python3-matplotlib
-sudo apt-get install python3-pyqt5 python3-pyqt5.qtwebengine python3-pyqt5.qtopengl python3-pyqt5.qtsvg
-sudo apt-get install python3-yaml python3-progressbar python3-jinja2
+sudo apt-get update
+sudo apt-get install -y make git python3-dev python3-setuptools
+sudo apt-get install -y python3-numpy python3-numpy-dev python3-scipy python3-matplotlib
+sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtwebengine python3-pyqt5.qtopengl python3-pyqt5.qtsvg
+sudo apt-get install -y python3-yaml python3-progressbar python3-jinja2
