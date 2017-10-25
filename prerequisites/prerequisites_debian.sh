f495f7eff7b656f89c0c47549519a7d1707f07de
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-10-25 16:44:42 +0200

    updated deps script

--- prerequisites/prerequisites_debian.sh
+++ prerequisites/prerequisites_debian.sh
@@ -1,5 +1,6 @@
-apt-get install -y make git python-dev python-setuptools \
-python-numpy python-numpy-dev python-scipy python-matplotlib \
-python-qt4 python-qt4-gl \
-python-yaml python-progressbar
-easy_install pyavl
+
+sudo apt-get install make git python-dev python-setuptools
+sudo apt-get install python-numpy python-numpy-dev python-scipy python-matplotlib
+sudo apt-get install python-pyqt5 python-pyqt5.qtwebengine python-pyqt5.qtopengl python-pyqt5.qtsvg
+sudo apt-get install python-future python-requests
+sudo apt-get install python-yaml python-progressbar python-jinja2
