34781b35cbb0c5a2fb26464c8e504ee5b586401a
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-10-30 11:32:25 +0100

    update vagrant testing scripts

--- maintenance/vagrant/centos-7/inside.sh
+++ maintenance/vagrant/centos-7/inside.sh
@@ -8,7 +8,9 @@ if [ -z "$branch" ]; then
 fi
 
 pyrockodir="pyrocko-$branch"
-outfile="/vagrant/test-$branch.out"
+outfile_py2="/vagrant/test-$branch.py2.out"
+
+rm -f "$outfile_py2"
 
 cd $HOME
 sudo yum -y install git python-setuptools python34-setuptools
@@ -19,7 +21,9 @@ fi
 git clone -b $branch https://github.com/pyrocko/pyrocko.git "$pyrockodir"
 cd "$pyrockodir"
 ln -s /vagrant/pyrocko-test-data test/data
-python setup.py install_prerequisites --force-yes
-sudo python setup.py install -f
-rm -f "$outfile"
-nosetests test > >(tee -a "$outfile") 2> >(tee -a "$outfile" >&2)
+
+python setup.py install_prerequisites --force-yes && \
+    sudo python setup.py install -f && \
+    python -m pyrocko.print_version >> "$outfile_py2" && \
+    nosetests test > >(tee -a "$outfile_py2") 2> >(tee -a "$outfile_py2" >&2) || \
+    /bin/true
--- maintenance/vagrant/debian-8/inside.sh
+++ maintenance/vagrant/debian-8/inside.sh
@@ -11,6 +11,9 @@ pyrockodir="pyrocko-$branch"
 outfile_py3="/vagrant/test-$branch.py3.out"
 outfile_py2="/vagrant/test-$branch.py2.out"
 
+rm -f "$outfile_py3"
+rm -f "$outfile_py2"
+
 cd $HOME
 sudo apt-get update -y
 sudo apt-get install -y git python-setuptools python3-setuptools
@@ -22,12 +25,14 @@ git clone -b $branch https://github.com/pyrocko/pyrocko.git "$pyrockodir"
 cd "$pyrockodir"
 ln -s /pyrocko-test-data test/data
 
-python3 setup.py install_prerequisites --force-yes
-sudo python3 setup.py install -f
-rm -f "$outfile_py3"
-nosetests3 test > >(tee -a "$outfile_py3") 2> >(tee -a "$outfile_py3" >&2) || /bin/true
+python3 setup.py install_prerequisites --force-yes && \
+    sudo python3 setup.py install -f && \
+    python3 -m pyrocko.print_version >> "$outfile_py3" && \
+    nosetests3 test > >(tee -a "$outfile_py3") 2> >(tee -a "$outfile_py3" >&2) || \
+    /bin/true
 
-python setup.py install_prerequisites --force-yes
-sudo python setup.py install -f
-rm -f "$outfile_py2"
-nosetests test > >(tee -a "$outfile_py2") 2> >(tee -a "$outfile_py2" >&2) || /bin/true
+python setup.py install_prerequisites --force-yes && \
+    sudo python setup.py install -f && \
+    python -m pyrocko.print_version >> "$outfile_py2" && \
+    nosetests test > >(tee -a "$outfile_py2") 2> >(tee -a "$outfile_py2" >&2) || \
+    /bin/true
