b6a599ebb73cbd156d91aa6ff49e970c0e618fb7
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2017-11-01 13:16:55 +0100

    wip: appstore installer

--- src/gui/app_store.py
+++ src/gui/app_store.py
@@ -56,7 +56,8 @@ prolog_html = '''
 
 class AppTile(qw.QWidget):
 
-    snuffling_update_required = qc.pyqtSignal()
+    want_install_snuffling = qc.pyqtSignal(str, dict)
+    want_remove_snuffling = qc.pyqtSignal(str)
     open_url_signal = qc.pyqtSignal(str)
 
     def __init__(self, data, label, installed=False):
@@ -71,7 +72,7 @@ class AppTile(qw.QWidget):
     def setup(self):
         self.layout().addWidget(qw.QLabel(self.label))
         self.button_install = qw.QPushButton('')
-        self.button_install.clicked.connect(self.un_install)
+        self.button_install.clicked.connect(self.install_remove)
         self.layout().addWidget(self.button_install)
 
         button_checkout = qw.QPushButton('Source Code')
@@ -90,55 +91,33 @@ class AppTile(qw.QWidget):
             button_label = 'Install'
 
         self.button_install.setText(button_label)
-        self.snuffling_update_required.emit()
 
-    def download(self, name, item):
-        logger.debug('Downloading snuffling %s' % item['name'])
-        response = urlopen(item['download_url'])
-        with open(pjoin(destination_path, name, item['name']), 'w') as f:
-            fd = response.read()
-            f.write(fd)
+    @qc.pyqtSlot()
+    def install_remove(self):
+        if not self.is_installed:
+            self.want_install_snuffling.emit(self.data['name'], self.data)
+        else:
+            self.want_remove_snuffling.emit(self.data['name'])
 
     @property
     def is_installed(self):
         installed_snufflings = os.listdir(destination_path)
         return self.data['name'] in installed_snufflings
 
-    def un_install(self):
-        if self.is_installed:
-            self.remove()
-        else:
-            if self.data['type'] == 'dir':
-                response = urlopen(
-                    base_url + 'contents/' + self.data['name'] + '?ref=%s'%git_branch)
-                logger.debug(response)
-                json_data = json.load(response.decode())
-
-                name = self.data['name']
-                os.mkdir(pjoin(destination_path, name))
-                for item in json_data:
-                    self.download(name, item)
-            else:
-                self.download('', self.data)
-        self.update_state()
-
-    def remove(self):
-        logger.debug('Delete snuffling %s' % self.data['name'])
-        fns = pjoin(destination_path, self.data['name'])
-        try:
-            shutil.rmtree(fns)
-        except OSError as e:
-            os.remove(fns)
-            print(e)
+    def connect_to_installer(self, install_service):
+        self.want_install_snuffling.connect(install_service.install)
+        self.want_remove_snuffling.connect(install_service.remove)
 
 
 class AppWidget(qw.QWidget):
-    def __init__(self, viewer=None, *args, **kwargs):
+    def __init__(self, viewer=None, install_service=None, *args, **kwargs):
         qw.QWidget.__init__(self, *args, **kwargs)
         self.url = base_url + 'contents' + '?ref=%s' % git_branch
         self.json_data = None
+        self.install_service = install_service
         self.viewer = viewer
         self.setLayout(qw.QVBoxLayout())
+        self.tiles = []
 
     def fail(self, message):
         box = qw.QMessageBox(self.viewer)
@@ -153,20 +132,13 @@ class AppWidget(qw.QWidget):
             nbytes_header = 100
             header_request = ('Range', 'bytes=0-%s' % nbytes_header)
             reader_re = re.compile('name\s*=(.*)', flags=re.IGNORECASE)
-            
             response = urlopen(self.url)
             reader = codecs.getreader("utf-8")
             self.json_data = json.load(reader(response))
 
             n_repos = len(self.json_data)
-            layout = self.layout()
-            progress_dialog = qw.QProgressDialog(parent=self)
-            progress_dialog.setModal(qc.Qt.WindowModal)
-            progress_dialog.setWindowTitle('Connecting')
-            progress_dialog.setLabelText('Connecting to AppStore')
+            progress_dialog = self.get_progressbar()
             progress_dialog.setMaximum(n_repos)
-            progress_dialog.setAutoClose(True)
-            progress_dialog.canceled.connect(self.cancel_refresh)
             progress_dialog.show()
             self.refresh_cancelled = False
 
@@ -179,62 +151,129 @@ class AppWidget(qw.QWidget):
                 if data['name'] in exclude:
                     continue
 
-                try:
-                    installed_snufflings = os.listdir(destination_path)
-                    is_installed = data['name'] in installed_snufflings
-                except OSError as e:
-                    if e.errno == 2:
-                        os.mkdir(destination_path)
-                        is_installed = False
-                    else:
-                        raise e
-
                 if data['type'] == 'dir':
-                    # TODO: to be handled
-                    continue
-
-                req = Request(data['download_url'])
+                    req = Request(
+                        base_url + 'contents/' + data['name'] +'/snuffling.py' + '?ref=%s'%git_branch)
+                else:
+                    req = Request(data['download_url'])
+                
                 req.add_header(*header_request)
                 response = urlopen(req)
 
                 app_header = reader(response).read()
 
                 m = re.search(reader_re, app_header)
+                print(app_header)
                 if m:
                     app_label = m.group(1).strip()
                 else:
                     app_label = data['name']
-                tile = AppTile(data, label=app_label, installed=is_installed)
+
+                tile = AppTile(
+                    data,
+                    label=app_label,
+                    installed=self.install_service.is_installed(data['name']))
+
+                tile.connect_to_installer(self.install_service)
                 tile.open_url_signal.connect(self.open_browser)
-                tile.snuffling_update_required.connect(self.setup_snufflings)
+
                 self.layout().addWidget(tile)
 
         except URLError as e:
             self.fail('No connection to internet')
+            print(e)
             self.setVisible(False)
         finally:
             progress_dialog.setValue(n_repos)
 
+    def get_progressbar(self):
+        progress_dialog = qw.QProgressDialog(parent=self)
+        progress_dialog.setModal(qc.Qt.WindowModal)
+        progress_dialog.setWindowTitle('Connecting')
+        progress_dialog.setLabelText('Connecting to AppStore')
+        progress_dialog.setAutoClose(True)
+        progress_dialog.canceled.connect(self.cancel_refresh)
+        return progress_dialog
+
     def cancel_refresh(self):
         self.refresh_cancelled = True
+
+    @qc.pyqtSlot(str)
+    def open_browser(self, url):
+        f = WebKitFrame(url)
+        if self.viewer:
+            self.viewer.panel_parent.add_tab('Browse the code', f)
+        else:
+            logger.warn('No viewer available')
+
+
+class Installer:
+    ''' 
+    Downloads, installs and removes snuffling modules from the *pile_viewer*
+    '''
+    def __init__(self, pile_viewer):
+        self.pile_viewer = pile_viewer
+        self.snufflings = {}
+
+    def download(self, data):
+        name = data['name']
+        logger.debug('Downloading snuffling %s' % name)
+        if data['type'] == 'dir':
+            response = urlopen(
+                base_url + 'contents/' + name + '?ref=%s'%git_branch)
+            logger.debug(response)
+            json_data = json.load(response.decode())
+
+            os.mkdir(pjoin(destination_path, name))
+            for item in json_data:
+                self.download(name, item)
+        else:
+            response = urlopen(data['download_url'])
+            with open(pjoin(destination_path, name), 'w') as f:
+                fd = response.read()
+                f.write(fd)
+
+    def is_installed(self, name):
+        for (directory, sname) in self.snufflings.keys():
+            if sname == name:
+                return True
+        return False
+
+    @qc.pyqtSlot(str, str)
+    def install(self, name, data):
+        self.download(data)
+        directory = pjoin(destination_path, name)
+        snuffling = pyrocko.gui.snuffling.SnufflingModule(
+            directory, name, self.pile_viewer)
+        
+        self.snuffling_modules[directory, name] = suffling
+        self.pile_viewer.add_snuffling(snuffling)
+
+    @qc.pyqtSlot(str)
+    def remove(self, name):
+        logger.debug('Delete snuffling %s' % name)
+        directory = pjoin(destination_path, name)
+        self.pile_viewer.remove_snuffling(self.snuffling_modulesp[directory, name])
+        try:
+            shutil.rmtree(directory)
+        except OSError as e:
+            os.remove(fns)
+            print(e)
 
     def setup_snufflings(self):
         if self.viewer:
             logger.debug('setup snufflings')
             self.viewer.setup_snufflings([destination_path])
 
-    @qc.pyqtSlot(str)
-    def open_browser(self, url):
-        f = WebKitFrame(url)
-        self.viewer.panel_parent.add_tab('Browse the code', f)
-
 
 class AppStore(qw.QFrame):
     def __init__(self, viewer=None, *args, **kwargs):
         qw.QFrame.__init__(self, *args, **kwargs)
         self.viewer = viewer
-        w = AppWidget(viewer=self.viewer)
+        install_service = Installer(pile_viewer=self.viewer)
+        w = AppWidget(viewer=self.viewer, install_service=install_service)
         w.refresh()
+
         self.setLayout(qw.QVBoxLayout())
         self.setAutoFillBackground(True)
         pal = self.palette()
