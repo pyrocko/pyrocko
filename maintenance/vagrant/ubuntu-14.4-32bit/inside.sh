c1012493f9ab148d81fc029f83b562018f70ea21
miili <isken@gfz-potsdam.de>
2017-11-06 14:24:54 +0100

    vagrant: fixed python version str

--- maintenance/vagrant/ubuntu-14.4-32bit/inside.sh
+++ maintenance/vagrant/ubuntu-14.4-32bit/inside.sh
@@ -33,7 +33,7 @@ ln -s "/pyrocko-test-data" "test/data"
 python3 setup.py install_prerequisites --force-yes && \
     sudo python3 setup.py install -f && \
     echo -n "Python Version: " >> "$outfile_py3" && \
-    python3 --version 2>> "$outfile_py3" && \
+    python3 --version >> "$outfile_py3" && \
     python3 -m pyrocko.print_version >> "$outfile_py3" && \
     nosetests3 "$thetest" > >(tee -a "$outfile_py3") 2> >(tee -a "$outfile_py3" >&2) || \
     /bin/true
