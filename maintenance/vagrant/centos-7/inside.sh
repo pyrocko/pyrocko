3a482044ea995abb02b945ec58a11512354d5d27
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-11-06 10:48:21 +0100

    vagrant testing with single tests

--- maintenance/vagrant/centos-7/inside.sh
+++ maintenance/vagrant/centos-7/inside.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-branch=$1
+branch="$1"
 if [ -z "$branch" ]; then
     branch=master
+fi
+
+thetest="$2"
+if [ -z "$thetest" ]; then
+    thetest="test"
 fi
 
 pyrockodir="pyrocko-$branch"
@@ -13,17 +18,17 @@ outfile_py2="/vagrant/test-$branch.py2.out"
 rm -f "$outfile_py2"
 
 cd $HOME
-sudo yum -y install git python-setuptools python34-setuptools
+sudo yum -y install git python-setuptools
 
 if [ -e "$pyrockodir" ] ; then
     sudo rm -rf "$pyrockodir"
 fi
-git clone -b $branch https://github.com/pyrocko/pyrocko.git "$pyrockodir"
+git clone -b "$branch" "/vagrant/pyrocko.git" "$pyrockodir"
 cd "$pyrockodir"
-ln -s /vagrant/pyrocko-test-data test/data
+ln -s "/vagrant/pyrocko-test-data" "test/data"
 
 python setup.py install_prerequisites --force-yes && \
     sudo python setup.py install -f && \
     python -m pyrocko.print_version >> "$outfile_py2" && \
-    nosetests test > >(tee -a "$outfile_py2") 2> >(tee -a "$outfile_py2" >&2) || \
+    nosetests "$thetest" > >(tee -a "$outfile_py2") 2> >(tee -a "$outfile_py2" >&2) || \
     /bin/true
--- maintenance/vagrant/centos-7/outside.sh
+++ maintenance/vagrant/centos-7/outside.sh
@@ -2,22 +2,34 @@
 
 set -e
 
-branch=$1
+branch="$1"
 
 if [ -z "$branch" ]; then
     branch=master
+fi
+
+thetest="$2"
+
+if [ -z "$thetest" ]; then
+    thetest="test"
 fi
 
 if [ ! -d "pyrocko-test-data" ]; then
     cp -R "../../../test/data" pyrocko-test-data
 fi
 
+if [ -d "pyrocko.git" ]; then
+    rm -rf "pyrocko.git"
+fi
+git clone --bare "../../.." "pyrocko.git"
+
 echo "testing branch $branch"
 rm -f log.out
 echo "testing branch $branch" >> log.out
 date -uIseconds >> log.out
 vagrant up
-vagrant ssh -- -X /vagrant/inside.sh $branch > >(tee -a "log.out") 2> >(tee -a "log.out" >&2) || /bin/true
+vagrant rsync
+vagrant ssh -- -X /vagrant/inside.sh $branch $thetest > >(tee -a "log.out") 2> >(tee -a "log.out" >&2) || /bin/true
 vagrant rsync-back || ( vagrant plugin install vagrant-rsync-back && vagrant rsync-back )
 vagrant halt
 date -uIseconds >> log.out
