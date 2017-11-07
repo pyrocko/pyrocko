dbb562d6d00bf722e5b9be3f9c311fe5a6e76749
miili <isken@gfz-potsdam.de>
2017-11-07 12:27:44 +0100

    Updated doc routines

--- maintenance/anaconda_deploy.sh
+++ maintenance/anaconda_deploy.sh
@@ -5,6 +5,8 @@ set -e
 MINICONDA_URL="http://repo.continuum.io/miniconda"
 CONDA_PREFIX=$HOME/miniconda3
 
+BUILD_DIR=`dirname $0`/pyrocko-anaconda-deploy
+
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     MINICONDA_FILE=Miniconda3-latest-MacOSX-x86_64.sh
 else
@@ -13,20 +15,23 @@ fi
 
 wget $MINICONDA_URL/$MINICONDA_FILE -O miniconda.sh
 chmod +x miniconda.sh
-./miniconda.sh -b
+./miniconda.sh -b -u
 export PATH=$PATH:$CONDA_PREFIX/bin
 
 conda install -y conda-build anaconda-client
-anaconda-client login --username $ANACONDA_USER --password $ANACONDA_PASSWORD
+anaconda logout
+anaconda login --username $ANACONDA_USER --password $ANACONDA_PASSWORD
 
-mkdir pyrocko
-cp anaconda-meta.yaml pyrocko/meta.yaml
-wget http://pyrocko.org/v0.3/_images/pyrocko_shadow.png -O pyrocko/snuffler.png
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cp anaconda-meta.yaml $BUILD_DIR/meta.yaml
+wget http://pyrocko.org/v0.3/_images/pyrocko_shadow.png -O $BUILD_DIR/snuffler.png
 
-cd pyrocko
+cd $BUILD_DIR
 conda config --set anaconda_upload yes
 conda-build .
-rm -rf pyrocko
+cd ..
+rm -rf $BUILD_DIR
 
 # Workaround for https://github.com/travis-ci/travis-ci/issues/6522
 # Turn off exit on failure.
