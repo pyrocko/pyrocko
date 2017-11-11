6432dbf3ee2ba3c2e5ffb10a323224df07fb0556
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-11-11 09:46:45 +0100

    vagrant: add opensuse box

---
+++ maintenance/vagrant/opensuse-42.1/inside.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+branch="$1"
+if [ -z "$branch" ]; then
+    branch=master
+fi
+
+thetest="$2"
+if [ -z "$thetest" ]; then
+    thetest="test"
+fi
+
+pyrockodir="pyrocko-$branch"
+outfile_py3="/vagrant/test-$branch.py3.out"
+outfile_py2="/vagrant/test-$branch.py2.out"
+
+rm -f "$outfile_py3"
+rm -f "$outfile_py2"
+
+cd $HOME
+sudo zypper -n refresh
+sudo zypper -n update
+sudo zypper -n install git python-setuptools python3-setuptools
+
+if [ -e "$pyrockodir" ] ; then
+    sudo rm -rf "$pyrockodir"
+fi
+git clone -b $branch "/vagrant/pyrocko.git" "$pyrockodir"
+cd "$pyrockodir"
+ln -s "/pyrocko-test-data" "test/data"
+
+python3 setup.py install_prerequisites --force-yes && \
+    sudo python3 setup.py install -f && \
+    echo -n "Python Version: " >> "$outfile_py3" && \
+    python3 --version >> "$outfile_py3" && \
+    python3 -m pyrocko.print_version >> "$outfile_py3" && \
+    nosetests3 "$thetest" > >(tee -a "$outfile_py3") 2> >(tee -a "$outfile_py3" >&2) || \
+    /bin/true
+
+python setup.py install_prerequisites --force-yes && \
+    sudo python setup.py install -f && \
+    echo -n "Python Version: " >> "$outfile_py2" && \
+    python --version 2>> "$outfile_py2" && \
+    python -m pyrocko.print_version >> "$outfile_py2" && \
+    nosetests "$thetest" > >(tee -a "$outfile_py2") 2> >(tee -a "$outfile_py2" >&2) || \
+    /bin/true
