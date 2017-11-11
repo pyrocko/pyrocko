83cbce6d6df6655c8117257d6a1877f92b7efab8
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2017-11-11 10:40:41 +0100

    deps: update opensuse deps

--- prerequisites/prerequisites_opensuse_python2.sh
+++ prerequisites/prerequisites_opensuse_python2.sh
@@ -2,8 +2,9 @@
 
 sudo zypper -n install make git gcc python-devel python-setuptools \
     python-numpy python-numpy-devel python-scipy python-matplotlib \
+    python-matplotlib-qt5 python-matplotlib-qt4 python-matplotlib-tk \
     python-qt5 python-qt4 \
-    python-PyYAML python-progressbar python-matplotlib-tk \
+    python-PyYAML python-progressbar python-Jinja2 \
     python-future python-requests \
     python-nose python-coverage
 
