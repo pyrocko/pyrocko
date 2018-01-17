7733c24b26fd03ed1519338e050fc2e9a9a6ed1f
miili <isken@gfz-potsdam.de>
2018-01-17 16:55:09 +0100

    introduced more methods

--- src/obspy_compat.py
+++ src/obspy_compat.py
@@ -2,7 +2,6 @@ def to_pyrocko_trace(obspy_trace):
     '''
     Convert ObsPy trace object to Pyrocko trace object.
     '''
-
     from pyrocko import trace
 
     return trace.Trace(
@@ -27,12 +26,14 @@ def to_pyrocko_events(obspy_catalog):
     '''
     Convert ObsPy catalog object to list of Pyrocko event objects.
     '''
+    if not obspy_catalog:
+        return None
+
+    from pyrocko import model
 
     events = []
     for obspy_event in obspy_catalog:
         for origin in obspy_event.origins:
-
-            
 
             events.append(model.Event(
                 name=obspy_event.resource_id + origin.resource_id,
@@ -42,28 +43,95 @@ def to_pyrocko_events(obspy_catalog):
                 depth=origin.depth,
                 region=origin.region))
 
-
-
     return events
 
 
-def snuffle(obspy_stream, obspy_inventory, obspy_catalog):
-    '''
-    '''
+def to_pyrocko_stations(obspy_inventory):
+    if not obspy_inventory:
+        return None
 
+    from pyrocko import model
+    stations = []
+    for net in obspy_inventory.networks:
+        for sta in net.stations:
+            stations.append(
+                model.Station(
+                    lat=sta.latitude,
+                    lon=sta.longitude,
+                    elevation=sta.elevation,
+                    network=net.code,
+                    station=sta.code,
+                    location='',
+                    channels=[
+                        model.station.Channel(
+                            name=cha.code,
+                            azimuth=cha.azimuth,
+                            dip=cha.dip) for cha in sta.channels]
+                    ))
+
+    return stations
+
+
+def to_obspy_stream(pyrocko_pile):
+    import obspy
+    stream = obspy.Stream()
+    stream.extend([to_obspy_trace(tr) for tr in pyrocko_pile.iter_all()])
+    return stream
+
+
+def to_obspy_trace(pyrocko_trace):
+    '''Convert Pyrocko trace to ObsPy Trace
+    '''
+    import obspy
+
+    obspy_trace = obspy.Trace(
+        data=pyrocko_trace.ydata,
+        header=obspy.core.trace.Stats(
+            dict(
+                network=pyrocko_trace.network,
+                station=pyrocko_trace.station,
+                location=pyrocko_trace.location,
+                channel=pyrocko_trace.channel,
+                delta=pyrocko_trace.deltat,
+                starttime=pyrocko_trace.tmin,
+                endtime=pyrocko_trace.tmax)
+            ))
+
+    return obspy_trace
+
+
+def snuffle(obspy_stream, obspy_inventory=None, obspy_catalog=None):
+    '''
+    '''
     from pyrocko import trace
-    events = to_pyrocko_events(catalog)
+    import obspy
+    events = to_pyrocko_events(obspy_catalog)
     stations = to_pyrocko_stations(obspy_inventory)
 
-    print trace.snuffle(
+    if isinstance(obspy_stream, obspy.Trace):
+        obspy_stream = obspy.Stream(traces=obspy_stream)
+
+    trace.snuffle(
         to_pyrocko_traces(obspy_stream),
         events=events,
         stations=stations,
         want_markers=True)
 
 
-def install_obspy_hooks():
+def plant():
     '''Add some Pyrocko methods to ObsPy classes.'''
 
     import obspy
+    obspy.Trace.to_pyrocko_trace = to_pyrocko_trace
+    obspy.Trace.snuffle = snuffle
+
     obspy.Stream.snuffle = snuffle
+    obspy.Stream.to_pyrocko_traces = to_pyrocko_traces
+
+    obspy.core.event.Catalog.to_pyrocko_events = to_pyrocko_events
+    obspy.core.inventory.inventory.Inventory.to_pyrocko_stations =\
+        to_pyrocko_stations
+
+    import pyrocko
+    pyrocko.trace.Trace.to_obspy_trace = to_obspy_trace
+    pyrocko.pile.Pile.to_obspy_stream = to_obspy_stream
