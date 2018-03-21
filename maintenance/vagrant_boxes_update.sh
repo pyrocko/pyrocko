8294fc0363f0e9c738f3a733c32afb2958d57f59
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2018-03-21 16:40:51 +0100

    vagrant testing: improved boxes update script

--- maintenance/vagrant_boxes_update.sh
+++ maintenance/vagrant_boxes_update.sh
@@ -6,6 +6,8 @@ rm -f vagrant/*/*.out
 
 for box in `ls vagrant` ; do 
     cd "vagrant/$box"
+    vagrant plugin repair
+    vagrant plugin expunge --reinstall --force
     vagrant box update
     cd ../..
 done
