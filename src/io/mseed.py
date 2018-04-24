f6ee43f3a54b8f1f139e7596a3fb2496dfb4f9c5
miili <marius.isken@gmail.com>
2018-04-24 16:59:04 +0200

    io.mseed: bugfix detection

--- src/io/mseed.py
+++ src/io/mseed.py
@@ -121,7 +121,7 @@ def detect(first512):
     if sequence_number < 0:
         return False
 
-    type_code = rec[6]
+    type_code = rec[6:7]
     if type_code in b'DRQM':
         bads = []
         for sex in '<>':
@@ -143,7 +143,7 @@ def detect(first512):
         if type_code not in b'VAST':
             return False
 
-        continuation_code = rec[7]
+        continuation_code = rec[7:8]
         if continuation_code != b' ':
             return False
 
