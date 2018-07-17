11a0edb5d1f749a72a1f7d658f0f20cb2f0171d4
hvasbath <Hannes.Bathke@gmx.net>
2018-07-17 13:09:42 +0200

    example STF for paper

--- src/gf/seismosizer.py
+++ src/gf/seismosizer.py
@@ -902,6 +902,42 @@ class SmoothRampSTF(STF):
                 self.duration, self.rise_ratio, self.anchor)
 
 
+class LinearRampSTF(STF):
+
+    duration = Float.T(
+        default=0.0,
+        help='baseline of the ramp')
+
+    anchor = Float.T(
+        default=0.0,
+        help='anchor point with respect to source.time: ('
+             '-1.0: left -> source duration [0, T] ~ hypocenter time, '
+             ' 0.0: center -> source duration [-T/2, T/2] ~ centroid time, '
+             '+1.0: right -> source duration [-T, 0] ~ rupture end time)')
+
+    def discretize_t(self, deltat, tref):
+
+        tmin_stf = tref - self.duration * (self.anchor + 1.) * 0.5
+        tmax_stf = tref + self.duration * (1. - self.anchor) * 0.5
+
+        tmin = round(tmin_stf / deltat) * deltat
+        tmax = round(tmax_stf / deltat) * deltat
+        nt = int(round((tmax - tmin) / deltat)) + 1
+
+        times = num.linspace(tmin, tmax, nt)
+
+        if nt > 1:
+            amplitudes = num.linspace(1., 0, nt)
+            amplitudes /= num.sum(amplitudes)
+        else:
+            amplitudes = num.ones(1)
+
+        return times, amplitudes
+
+    def base_key(self):
+        return (type(self).__name__, self.duration, self.anchor)
+
+
 class STFMode(StringChoice):
     choices = ['pre', 'post']
 
