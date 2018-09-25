191a4035c69e04e2f64b2d9e326ac2ee87d1ed71
miili <isken@gfz-potsdam.de>
2018-09-25 13:42:04 +0200

    fomosto: list available modelling backends

--- src/fomosto/__init__.py
+++ src/fomosto/__init__.py
@@ -2,3 +2,13 @@
 #
 # The Pyrocko Developers, 21st Century
 # ---|P------/S----------~Lg----------
+
+import os
+import re
+
+op = os.path
+
+
+modules = [m for m in os.listdir(op.dirname(__file__))
+           if re.search(r'[^_(dummy)].py', m)]
+AVAILABLE_BACKENDS = set([op.splitext(m)[0] for m in modules])
