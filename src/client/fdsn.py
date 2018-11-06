fbd659e092cdfaacc37870045fae63196b0acd3e
Andreas Steinberg <asteinbe@gfz-potsdam.de>
2018-11-06 12:27:05 +0100

    g_timeout as fdsn_timeout in config (#307)
    * g_timeout as fdsn_timeout in config
    
    * fdsn default timeout none
    
    * fdsn default timeout none
    
    * DoubleDC description
    
    * DoubleDC description
    
    * explicit import of config
    
    * Quantity velocity from numpy diff
    
    * typo fix retrived
    
    * update to master

--- src/client/fdsn.py
+++ src/client/fdsn.py
@@ -22,7 +22,7 @@ except ImportError:
     from urllib.error import HTTPError
 
 from pyrocko import util
-
+from pyrocko import config
 
 logger = logging.getLogger('pyrocko.client.fdsn')
 
@@ -53,7 +53,10 @@ g_site_abbr = {
 
 g_default_site = 'geofon'
 
-g_timeout = 20.
+if config.config().fdsn_timeout is None:
+    g_timeout = 20.
+else:
+    g_timeout = config.config().fdsn_timeout
 
 re_realm_from_auth_header = re.compile(r'(realm)\s*[:=]\s*"([^"]*)"?')
 
@@ -108,7 +111,6 @@ def _request(url, post=False, user=None, passwd=None,
     if url_values:
         url += '?' + url_values
     logger.debug('Accessing URL %s' % url)
-
     url_args = {
         'timeout': g_timeout
     }
