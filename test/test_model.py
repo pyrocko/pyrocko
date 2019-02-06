935e7822bd17c1948f264779f827966b0f4fd822
Marius <marius.kriegerowski@gmail.com>
2019-02-06 14:01:52 +0100

    test_model: add missing event depth

--- test/test_model.py
+++ test/test_model.py
@@ -52,7 +52,7 @@ class ModelTestCase(unittest.TestCase):
         tempdir = tempfile.mkdtemp(prefix='pyrocko-model')
         fn = pjoin(tempdir, 'event.txt')
         e1 = model.Event(
-            10., 20., 1234567890., 'bubu', region='taka tuka land',
+            10., 20., 1234567890., 'bubu', depth=10., region='taka tuka land',
             moment_tensor=moment_tensor.MomentTensor(strike=45., dip=90),
             magnitude=5.1, magnitude_type='Mw',
             tags=['cluster:-1', 'custom_magnitude:2.5'])
