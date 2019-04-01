4ebdb76fd2077d26f7502b08b0cc7a8096311e77
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2019-04-01 13:28:44 +0200

    event: optional event depth

--- src/model/event.py
+++ src/model/event.py
@@ -65,6 +65,7 @@ class Event(Location):
     '''
 
     time = Timestamp.T(default=util.str_to_time('1970-01-01 00:00:00'))
+    depth = Float.T(optional=True)
     name = String.T(default='', optional=True)
     magnitude = Float.T(optional=True)
     magnitude_type = String.T(optional=True)
