4c4b43b837bbd6446ace77fb88a3215bee1e410d
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2019-08-27 07:48:07 +0200

    station/channels: add ('1', '2', '3') as valid channel triple

--- src/model/station.py
+++ src/model/station.py
@@ -272,7 +272,7 @@ class Station(Location):
 
         out_groups = []
         for kind, components in cg.items():
-            for sys in ('ENZ', '12Z', 'XYZ', 'RTZ'):
+            for sys in ('ENZ', '12Z', 'XYZ', 'RTZ', '123'):
                 if allin(sys, components):
                     out_groups.append(tuple([kind+c for c in sys]))
 
