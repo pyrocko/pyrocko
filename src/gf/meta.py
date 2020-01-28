9bbc67f9f4830c9bef0f181cf2976fda2d81f0d5
braunfuss <andreas.steinberg@bgr.de>
2020-01-28 10:12:30 +0100

    undo removed from_pyrocko_trace func

--- src/gf/meta.py
+++ src/gf/meta.py
@@ -72,6 +72,16 @@ class SeismosizerTrace(Object):
                            deltat=self.deltat,
                            tmin=self.tmin)
 
+    @classmethod
+    def from_pyrocko_trace(cls, tr, **kwargs):
+        d = dict(
+            codes=tr.nslc_id,
+            tmin=tr.tmin,
+            deltat=tr.deltat,
+            data=num.asarray(tr.get_ydata(), dtype=num.float32))
+        d.update(kwargs)
+        return cls(**d)
+
 
 class SeismosizerResult(Object):
     n_records_stacked = Int.T(optional=True, default=1)
