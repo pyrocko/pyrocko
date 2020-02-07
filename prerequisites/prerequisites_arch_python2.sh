18e6fc2bb9234bf06a40804a59754431ebefa4ad
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-02-07 23:14:25 +0100

    installation: update python2 prereqs for arch

--- prerequisites/prerequisites_arch_python2.sh
+++ prerequisites/prerequisites_arch_python2.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syu --noconfirm --needed git make gcc python2 python2-setuptools \
     python2-numpy python2-scipy python2-matplotlib \
-    python2-pyqt5 qt5-webengine qt5-svg qt5-webkit python2-pyqt4 \
-    python2-cairo python2-opengl python2-progressbar \
+    python2-pyqt5 qt5-webengine qt5-svg qt5-webkit \
+    python2-cairo python2-opengl \
     python2-requests python2-yaml python2-jinja python2-future \
     python2-nose python2-coverage
