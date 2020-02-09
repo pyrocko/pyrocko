6377852889268f12107121e00314b76642952734
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-02-09 22:31:43 +0100

    installation: update python3 prereqs for arch

--- prerequisites/prerequisites_arch_python3.sh
+++ prerequisites/prerequisites_arch_python3.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syu --noconfirm --needed git make gcc python python-setuptools \
     python-numpy python-scipy python-matplotlib \
-    python-pyqt5 qt5-webengine qt5-svg qt5-webkit python-pyqt4 \
+    python-pyqt5 qt5-webengine qt5-svg qt5-webkit \
     python-cairo python-opengl python-progressbar \
     python-requests python-yaml python-jinja python-future \
     python-nose python-coverage
