a624fca1eac7156cf616373e0939ea5373374c20
hvasbath <Hannes.Bathke@gmx.net>
2020-02-12 23:58:56 +0100

    sparrow.elements Introduced CPTHandler
    - Geometry, Table and Source Elements use common CPT handling framework
    - added MPL colormaps to colormap choices
    - Geometry and Source use common VTK utils and model.Geometry

--- src/gui/sparrow/elements/base.py
+++ src/gui/sparrow/elements/base.py
@@ -10,7 +10,7 @@ import os
 import numpy as num
 
 from pyrocko import automap
-from pyrocko.guts import Object, String, Tuple
+from pyrocko.guts import Object, String, Tuple, StringChoice
 from pyrocko.plot import AutoScaler, AutoScaleMode
 from pyrocko.dataset import topo
 
@@ -59,8 +59,8 @@ class Element(object):
         self._state = None
 
 
-class CPTChoice(Object):
-    choices = ['slip_colors']
+class CPTChoice(StringChoice):
+    choices = ['slip_colors', 'seismic', 'jet', 'hot_r', 'gist_earth_r']
 
 
 class CPTState(ElementState):
@@ -69,58 +69,188 @@ class CPTState(ElementState):
     cpt_scale = Tuple.T(default=(None, None))
 
 
-def bind_state_cpt(state, update_function):
-    for state_attr in ['cpt_name', 'cpt_mode', 'cpt_scale']:
-        state.add_listener(update_function, state_attr)
+class CPTHandler(Element):
+
+    def __init__(self):
+
+        Element.__init__(self)
+        self._cpts = {}
+        self._autoscaler = None
+        self._lookuptable = None
+        self._cpt_combobox = None
+        self._values = None
+        self._state = None
+        self._cpt_scale_lineedit = None
+
+    def bind_state(self, cpt_state, update_function):
+        for state_attr in ['cpt_name', 'cpt_mode', 'cpt_scale']:
+            cpt_state.add_listener(update_function, state_attr)
+
+        self._state = cpt_state
+
+    def unbind_state(self):
+        self._cpts = {}
+        self._lookuptable = None
+        self._values = None
+        self._autoscaler = None
+
+    def open_cpt_load_dialog(self):
+        caption = 'Select one *.cpt file to open'
+
+        fns, _ = fnpatch(qw.QFileDialog.getOpenFileNames(
+            self._parent, caption, options=common.qfiledialog_options))
+
+        if fns:
+            self.load_cpt_file(self, fns[0])
+
+    def load_cpt_file(self, path):
+        cpt_name = 'USR' + os.path.basename(path).split('.')[0]
+        self._cpts.update([(cpt_name, automap.read_cpt(path))])
+
+        self._state.cpt_name = cpt_name
+
+        self._update_cpt_combobox()
+        self.update_cpt()
+
+    def _update_cpt_combobox(self):
+        from pyrocko import config
+        conf = config.config()
+
+        if self._cpt_combobox is None:
+            raise ValueError('CPT combobox needs init before updating!')
+
+        cb = self._cpt_combobox
+
+        if cb is not None:
+            cb.clear()
+
+            for s in CPTChoice.choices:
+                if s not in self._cpts:
+                    try:
+                        cpt = automap.read_cpt(topo.cpt(s))
+                    except Exception:
+                        from matplotlib import pyplot as plt
+                        cmap = plt.cm.get_cmap(s)
+                        cpt = automap.CPT.from_numpy(cmap(range(256))[:, :-1])
+
+                    self._cpts.update([(s, cpt)])
+
+            cpt_dir = conf.colortables_dir
+            if os.path.isdir(cpt_dir):
+                for f in [
+                        f for f in os.listdir(cpt_dir)
+                        if f.lower().endswith('.cpt')]:
+
+                    s = 'USR' + os.path.basename(f).split('.')[0]
+                    self._cpts.update(
+                        [(s, automap.read_cpt(os.path.join(cpt_dir, f)))])
+
+            for i, (s, cpt) in enumerate(self._cpts.items()):
+                cb.insertItem(i, s, qc.QVariant(self._cpts[s]))
+                cb.setItemData(i, qc.QVariant(s), qc.Qt.ToolTipRole)
+
+        cb.setCurrentIndex(cb.findText(self._state.cpt_name))
+
+    def _update_cptscale_lineedit(self):
+        le = self._cpt_scale_lineedit
+        if le is not None:
+            le.clear()
+
+            self._cptscale_to_lineedit(self._state, le)
 
 
-def open_cpt_load_dialog(self):
-    caption = 'Select one *.cpt file to open'
+    def _cptscale_to_lineedit(self, state, widget):
+        sel = widget.selectedText() == widget.text()
 
-    fns, _ = fnpatch(qw.QFileDialog.getOpenFileNames(
-        self._parent, caption, options=common.qfiledialog_options))
+        crange = (None, None)
+        if self._lookuptable is not None:
+            crange = self._lookuptable.GetRange()
 
-    if fns:
-        load_cpt_file(self, fns[0])
+        if all(state.cpt_scale):
+            crange = state.cpt_scale
+
+        fmt = ', '.join(['%s' if item is None else '%g' for item in crange])
+
+        widget.setText(fmt % crange)
+
+        if sel:
+            widget.selectAll()
+
+    def update_cpt(self):
+        state = self._state
+
+        if self._autoscaler is None:
+            self._autoscaler = AutoScaler()
+
+        if self._cpt_scale_lineedit:
+            if state.cpt_mode == 'off':
+                self._cpt_scale_lineedit.setEnabled(True)
+            else:
+                self._cpt_scale_lineedit.setEnabled(False)
+
+                if any(state.cpt_scale):
+                    state.cpt_scale = (None, None)
+
+        if state.cpt_name is not None and self._values is not None:
+            vscale = (num.nanmin(self._values), num.nanmax(self._values))
+
+            vmin, vmax = None, None
+            if state.cpt_scale != (None, None):
+                vmin, vmax = state.cpt_scale
+            else:
+                vmin, vmax, _ = self._autoscaler.make_scale(
+                    vscale, override_mode=state.cpt_mode)
+
+            self._cpts[state.cpt_name].scale(vmin, vmax)
+            cpt = self._cpts[state.cpt_name]
+
+            vtk_cpt = cpt_to_vtk_lookuptable(cpt)
+
+            self._lookuptable = vtk_cpt
+            self._update_cptscale_lineedit()
+
+        elif state.cpt_name and self._values is None:
+            raise ValueError('No values passed to colormapper!')
+
+    def cpt_controls(self, parent, state, layout):
+        self._parent = parent
+
+        iy = layout.rowCount() + 1
+
+        layout.addWidget(qw.QLabel('Color Map:'), iy, 0)
+
+        cb = common.CPTComboBox()
+        layout.addWidget(cb, iy, 1)
+        state_bind_combobox(
+            self, state, 'cpt_name', cb)
+
+        self._cpt_combobox = cb
+
+        pb = qw.QPushButton('Load CPT')
+        layout.addWidget(pb, iy, 2)
+        pb.clicked.connect(self.open_cpt_load_dialog)
+
+        iy += 1
+        layout.addWidget(qw.QLabel('Color Scaling:'), iy, 0)
+
+        cb = common.string_choices_to_combobox(AutoScaleMode)
+        layout.addWidget(cb, iy, 1)
+        state_bind_combobox(
+            self, state, 'cpt_mode', cb)
+
+        le = qw.QLineEdit()
+        le.setEnabled(False)
+        layout.addWidget(le, iy, 2)
+        state_bind(
+            self, state,
+            ['cpt_scale'], _lineedit_to_cptscale,
+            le, [le.editingFinished, le.returnPressed],
+            self._cptscale_to_lineedit)
+
+        self._cpt_scale_lineedit = le
 
 
-def load_cpt_file(owner, path):
-    cpt_name = 'USR' + os.path.basename(path).split('.')[0]
-    owner._cpts.update([(cpt_name, automap.read_cpt(path))])
-
-    owner._state.cpt_name = cpt_name
-
-    _update_cpt_combobox(owner)
-    update_cpt(owner)
-
-
-def _update_cptscale_lineedit(owner):
-    le = owner._cpt_scale_lineedit
-    if le is not None:
-        le.clear()
-
-        owner._cptscale_to_lineedit(owner._state, le)
-
-
-def _cptscale_to_lineedit(self, state, widget):
-    sel = widget.selectedText() == widget.text()
-
-    crange = (None, None)
-    if self._lookuptable is not None:
-        crange = self._lookuptable.GetRange()
-
-    if all(state.cpt_scale):
-        crange = state.cpt_scale
-
-    fmt = ', '.join(['%s' if item is None else '%g' for item in crange])
-
-    widget.setText(fmt % crange)
-
-    if sel:
-        widget.selectAll()
-
-
-def _lineedit_to_cptscale(widget, state):
+def _lineedit_to_cptscale(cpt_state, widget):
     s = str(widget.text())
     s = s.replace(',', ' ')
 
@@ -130,112 +260,7 @@ def _lineedit_to_cptscale(widget, state):
         crange[0]+0.01 if crange[0] >= crange[1] else crange[1]))
 
     try:
-        state.cpt_scale = crange
+        cpt_state.cpt_scale = crange
     except Exception:
         raise ValueError(
             'need two numerical values: <vmin>, <vmax>')
-
-
-def _update_cpt_combobox(owner):
-    from pyrocko import config
-    conf = config.config()
-
-    cb = owner._cpt_combobox
-
-    if cb is not None:
-        cb.clear()
-
-        for s in CPTChoice.choices:
-            if s not in owner._cpts:
-                owner._cpts.update([(s, automap.read_cpt(topo.cpt(s)))])
-
-        cpt_dir = conf.colortables_dir
-        if os.path.isdir(cpt_dir):
-            for f in [
-                    f for f in os.listdir(cpt_dir)
-                    if f.lower().endswith('.cpt')]:
-
-                s = 'USR' + os.path.basename(f).split('.')[0]
-                owner._cpts.update(
-                    [(s, automap.read_cpt(os.path.join(cpt_dir, f)))])
-
-        for i, (s, cpt) in enumerate(owner._cpts.items()):
-            cb.insertItem(i, s, qc.QVariant(owner._cpts[s]))
-            cb.setItemData(i, qc.QVariant(s), qc.Qt.ToolTipRole)
-
-    cb.setCurrentIndex(cb.findText(owner._state.cpt_name))
-
-
-def update_cpt(owner):
-    state = owner._state
-
-    if owner._autoscaler is None:
-        owner._autoscaler = AutoScaler()
-
-    if owner._cpt_scale_lineedit:
-        if state.cpt_mode == 'off':
-            owner._cpt_scale_lineedit.setEnabled(True)
-        else:
-            owner._cpt_scale_lineedit.setEnabled(False)
-
-            if any(state.cpt_scale):
-                state.cpt_scale = (None, None)
-
-    if state.cpt_name is not None and owner._values is not None:
-        vscale = (num.nanmin(owner._values), num.nanmax(owner._values))
-
-        vmin, vmax = None, None
-        if state.cpt_scale != (None, None):
-            vmin, vmax = state.cpt_scale
-        else:
-            vmin, vmax, _ = owner._autoscaler.make_scale(
-                vscale, override_mode=state.cpt_mode)
-
-        owner._cpts[state.cpt_name].scale(vmin, vmax)
-        cpt = owner._cpts[state.cpt_name]
-
-        vtk_cpt = cpt_to_vtk_lookuptable(cpt)
-
-        owner._lookuptable = vtk_cpt
-        _update_cptscale_lineedit(owner)
-
-
-def cpt_controls(owner, state, layout):
-    iy = layout.rowCount() + 1
-
-    layout.addWidget(qw.QLabel('Color Map:'), iy, 0)
-
-    cb = common.CPTComboBox()
-    layout.addWidget(cb, iy, 1)
-    state_bind_combobox(
-        owner, state, 'cpt_name', cb)
-
-    owner._cpt_combobox = cb
-    owner._update_cpt_combobox = _update_cpt_combobox.__get__(owner)
-
-    owner.open_cpt_load_dialog = open_cpt_load_dialog.__get__(owner)
-
-    pb = qw.QPushButton('Load CPT')
-    layout.addWidget(pb, iy, 2)
-    pb.clicked.connect(owner.open_cpt_load_dialog)
-
-    iy += 1
-    layout.addWidget(qw.QLabel('Color Scaling:'), iy, 0)
-
-    cb = common.string_choices_to_combobox(AutoScaleMode)
-    layout.addWidget(cb, iy, 1)
-    state_bind_combobox(
-        owner, state, 'cpt_mode', cb)
-
-    owner._cptscale_to_lineedit = _cptscale_to_lineedit.__get__(owner)
-
-    le = qw.QLineEdit()
-    le.setEnabled(False)
-    layout.addWidget(le, iy, 2)
-    state_bind(
-        owner, state,
-        ['cpt_scale'], _lineedit_to_cptscale,
-        le, [le.editingFinished, le.returnPressed],
-        owner._cptscale_to_lineedit)
-
-    owner._cpt_scale_lineedit = le
