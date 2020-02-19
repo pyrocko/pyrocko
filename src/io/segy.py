2b9811dce15074794c667a5830431cae656a86a3
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-02-19 09:52:44 +0100

    segy: correct reading of SEGY files containing IEEE floating-point values

--- src/io/segy.py
+++ src/io/segy.py
@@ -90,7 +90,7 @@ def iload(filename, load_data, endianness='>'):
             2: (endianness+'i4', 4, "4-byte, two's complement integer"),
             3: (endianness+'i4', 2, "2-byte, two's complement integer"),
             4: (None,  4, "4-byte fixed-point with gain (obolete)"),
-            5: ('f4',  4, "4-byte IEEE floating-point"),
+            5: (endianness+'f4',  4, "4-byte IEEE floating-point"),
             6: (None,  0, "not currently used"),
             7: (None,  0, "not currently used"),
             8: ('i1',  1, "1-byte, two's complement integer")}
