ba1fb3997bdde32a485849d0cb11218f251fe27c
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-02-28 00:01:01 +0100

    testing: add drone ci

--- maintenance/pip/test_wheels.sh
+++ maintenance/pip/test_wheels.sh
@@ -4,7 +4,6 @@ set -e
 
 cd /src
 
-pip install -r requirements.txt jinja2 pybtex
-pip install jinja2 pybtex
+pip install -r requirements-all.txt
 pip install -f /wheels --no-index pyrocko
 python -m nose test.test_util
