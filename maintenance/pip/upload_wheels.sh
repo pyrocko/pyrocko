15bb27181fb4b06a243ecfc1246bb816e6404e10
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-03-12 16:20:31 +0100

    drone: pypi uploading

--- maintenance/pip/upload_wheels.sh
+++ maintenance/pip/upload_wheels.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-twine upload --repository-url https://test.pypi.org/legacy/ wheels/* dist/*
+destination=$1
+
+if [ -z "$destination" ] ; then
+    echo "usage: upload_wheels.sh (testing|live)"
+fi
+
+if [ "$destination" == 'live' ] ; then
+    twine upload wheels/* dist/* \
+        --username="$PYPI_USERNAME" --password="$PYPI_PASSWORD" \
+        --skip-existing --disable-progress-bar --comment='*grunz-grunz*'
+else
+    twine upload --repository-url https://test.pypi.org/legacy/ wheels/* dist/* \
+        --username="$PYPI_USERNAME" --password="$PYPI_PASSWORD" \
+        --skip-existing --disable-progress-bar --comment='*grunz-grunz*'
+fi
