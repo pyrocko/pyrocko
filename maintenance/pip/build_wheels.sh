252d40ade7dff5097544ae917b5b19d9c162034f
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-08-17 14:47:43 +0200

    exclude py3.9 in pip wheel building

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir wheels_temp
 for x in /opt/python/* ; do
-    "$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (2, 7) or sys.version_info >= (3, 5, 0)))' || continue
+    "$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (2, 7) or (3, 5, 0) <= sys.version_info <= (3, 8, 0)))' || continue
     #"$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (3, 7)))' || continue
     "$x/bin/pip" install --upgrade pip
     "$x/bin/pip" install --no-cache-dir -r requirements.txt
