b919dc19086e1609802d348103060a2d4502e318
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-08-18 09:27:24 +0200

    pip-wheels: update

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+rm -rf wheels_temp wheels
+
 mkdir wheels_temp
 for x in /opt/python/* ; do
     "$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (2, 7) or (3, 5, 0) <= sys.version_info <= (3, 8, 0)))' || continue
@@ -12,7 +14,7 @@ for x in /opt/python/* ; do
 done
 
 mkdir wheels
-for wheel in wheels_temp/*.whl ; do
+for wheel in wheels_temp/pyrocko-*.whl ; do
     auditwheel repair "$wheel" --plat $PLAT -w wheels
     rm "$wheel"
 done
