d36322dc47653b748f80c0e68d25b0282f8b238e
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-08-18 10:49:56 +0200

    pip-wheels: reenable py38

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -6,7 +6,7 @@ rm -rf wheels_temp wheels
 
 mkdir wheels_temp
 for x in /opt/python/* ; do
-    "$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (2, 7) or (3, 5, 0) <= sys.version_info <= (3, 8, 0)))' || continue
+    "$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (2, 7) or (3, 5, 0) <= sys.version_info < (3, 9, 0)))' || continue
     #"$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (3, 7)))' || continue
     "$x/bin/pip" install --upgrade pip
     "$x/bin/pip" install --no-cache-dir -r requirements.txt
