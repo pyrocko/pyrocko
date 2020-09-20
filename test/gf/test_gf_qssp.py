516eeca5c75b6ddb786b1628b1037cfcadd25223
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-09-20 14:12:01 +0200

    fomosto qssp backend: support for QSSP 2020

--- test/gf/test_gf_qssp.py
+++ test/gf/test_gf_qssp.py
@@ -78,6 +78,39 @@ class QSSPTestCase(unittest.TestCase):
 
         engine.process(source, targets)
 
+    @unittest.skipUnless(
+        'qssp.2020' in qssp.have_backend(), 'backend qssp.2020 not available')
+    def test_qssp_build_2020_rotational(self):
+        qssp.init(self.tmpdir, '2020', config_params=dict(
+            stored_quantity='rotation',
+            source_depth_max=10e3,
+            distance_min=500e3,
+            distance_max=600e3))
+
+        store = gf.store.Store(self.tmpdir, 'r')
+        store.make_ttt()
+        qssp.build(self.tmpdir)
+
+        del store
+
+        engine = gf.LocalEngine(store_dirs=[self.tmpdir])
+
+        source = gf.DCSource(
+            lat=0.,
+            lon=0.,
+            depth=10e3,
+            magnitude=6.0)
+
+        targets = [gf.Target(
+            quantity='rotation',
+            codes=('', 'ROT', '', comp),
+            lat=0.,
+            lon=0.,
+            north_shift=500e3,
+            east_shift=100e3) for comp in 'NEZ']
+
+        engine.process(source, targets)
+
 
 if __name__ == '__main__':
     util.setup_logging('test_gf_qssp', 'warning')
