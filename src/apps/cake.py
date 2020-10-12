62e5b130face32fc5c065cd5d6a97cf0e67fbaf2
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2020-10-12 17:56:21 +0200

    cake: fix error with new less permissive numpy/python3.8

--- src/apps/cake.py
+++ src/apps/cake.py
@@ -389,7 +389,7 @@ as in --phases.''')
                         'format for distances is '
                         '"min_distance:max_distance:n_distances"')
 
-                distances = num.linspace(*map(float, ssn))
+                distances = num.linspace(float(ssn[0]), float(ssn[1]), int(ssn[2]))
             else:
                 distances = num.array(
                     list(map(
