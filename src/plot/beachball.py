pyrocko/pyrocko

commit 47d9df01942cd31ec84a58e901cc14895ef199e3

plot.beachball: adding axes transform
`size_units` now understand `axes` transformation

src/plot/beachball.py
@@ -461,7 +461,14 @@ def choose_transform(axes, size_units, position, size):
         if size is None:
             size = 1.0
 
-        size = size * 0.5
+        size = size
+
+    elif size_units == 'axes':
+        transform = axes.transAxes
+        if size is None:
+            size = 1.
+
+        size = size * .5
 
     else:
         raise BeachballError(

commit 77817d93eb246d75fc604cd8aba7941cd68c2507

beachball: fix broken sizing of symbols for case transform='data'
Broken since 47d9df01942cd31ec84a58e901cc14895ef199e3. Affects Grond
report plots on MT decomposition.

src/plot/beachball.py
@@ -461,7 +461,7 @@ def choose_transform(axes, size_units, position, size):
         if size is None:
             size = 1.0
 
-        size = size
+        size = size * 0.5
 
     elif size_units == 'axes':
         transform = axes.transAxes