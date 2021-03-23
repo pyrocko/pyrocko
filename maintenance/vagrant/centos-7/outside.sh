8e9c499fc1bbbcb854e34c41ccad95772a1841ef
Pyrocko Test Debugger <info@pyrocko.org>
2021-03-23 10:28:39 +0100

    vagrant tests: no x forwarding

--- maintenance/vagrant/centos-7/outside.sh
+++ maintenance/vagrant/centos-7/outside.sh
@@ -25,7 +25,7 @@ echo "testing branch $branch" >> log.out
 date -uIseconds >> log.out
 vagrant up
 vagrant rsync
-vagrant ssh -- -X /vagrant/inside.sh "$branch" "$thetest" > >(tee -a "log.out") 2> >(tee -a "log.out" >&2) || /bin/true
+vagrant ssh -- /vagrant/inside.sh "$branch" "$thetest" > >(tee -a "log.out") 2> >(tee -a "log.out" >&2) || /bin/true
 vagrant rsync-back || ( vagrant plugin install vagrant-rsync-back && vagrant rsync-back )
 vagrant halt
 date -uIseconds >> log.out
