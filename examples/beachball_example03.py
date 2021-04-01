dc481d7d4a2ffa19ce4812d7dd22a4d3758990f4
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2021-04-01 12:44:30 +0200

    examples: fix import error in beachball example

--- examples/beachball_example03.py
+++ examples/beachball_example03.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from pyrocko import moment_tensor as pmt
 from pyrocko import plot
+from pyrocko.plot import beachball
 
 
 fig = plt.figure(figsize=(4., 2.))
@@ -11,7 +12,7 @@ axes.set_ylim(0., 2.)
 axes.set_axis_off()
 
 for i, beachball_type in enumerate(['full', 'deviatoric', 'dc']):
-    plot.beachball.plot_beachball_mpl(
+    beachball.plot_beachball_mpl(
             pmt.as_mt((124654616., 370943136., -6965434.0,
                        553316224., -307467264., 84703760.0)),
             axes,
