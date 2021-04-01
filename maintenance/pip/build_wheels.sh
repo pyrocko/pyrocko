0ce1e0f466e9cf44d6801792824288c5a0cf614e
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2021-04-02 00:46:46 +0200

    pip: improve compatibility with older numpy versions

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -5,11 +5,13 @@ set -e
 rm -rf wheels_temp wheels
 
 mkdir wheels_temp
+mv pyproject.toml pyproject.toml.orig
+cp maintenance/pip/pyproject-build-pip.toml pyproject.toml
 for x in /opt/python/* ; do
     "$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (2, 7) or (3, 5, 0) <= sys.version_info < (3, 9, 0)))' || continue
     #"$x/bin/python" -c 'import sys ; sys.exit(not (sys.version_info[:2] == (3, 7)))' || continue
     "$x/bin/pip" install --upgrade pip
-    "$x/bin/pip" install --no-cache-dir -r requirements.txt
+    "$x/bin/pip" install --no-cache-dir -r maintenance/pip/requirements-build-pip.txt
     "$x/bin/pip" wheel -v . -w wheels_temp
 done
 
@@ -18,3 +20,5 @@ for wheel in wheels_temp/pyrocko-*.whl ; do
     auditwheel repair "$wheel" --plat $PLAT -w wheels
     rm "$wheel"
 done
+
+mv pyproject.toml.orig pyproject.toml
