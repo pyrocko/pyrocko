f5deb9122f19882621b73f4d22ee4f0881071dfb
Pyrocko Test Debugger <info@pyrocko.org>
2021-07-26 10:42:24 +0200

    vagrant testing: fix connection problem with win-10 vm

--- maintenance/vagrant/win-10/outside.sh
+++ maintenance/vagrant/win-10/outside.sh
@@ -23,7 +23,12 @@ echo "testing branch: $branch"
 echo "running test: $thetest"
 echo "testing branch $branch" >> log.out
 date -uIseconds >> log.out
-vagrant up
+vagrant up &
+pid=$!
+sleep 30
+vagrant ssh -- powershell -Command Set-NetConnectionProfile -NetworkCategory Private
+wait $pid
+
 vagrant ssh -- "C:\\vagrant\\inside.bat" "$branch" "$thetest" > >(tee -a "log.out") 2> >(tee -a "log.out" >&2) || /bin/true
 vagrant halt
 date -uIseconds >> log.out
