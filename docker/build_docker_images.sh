2918e8e6b078aacb829c01b866f72de8863a96e4
Pyrocko Test Debugger <info@pyrocko.org>
2021-09-24 12:23:52 +0200

    docker: update

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+set -e
+
+rm -rf pyrocko-test-data
+wget -r http://data.pyrocko.org/testing/pyrocko/ -nv --no-parent -nH --cut-dirs=2 -P pyrocko-test-data
+
 docker build nest -t pyrocko-nest
+
+rm -rf fat-nest/pyrocko-test-data
+cp -r pyrocko-test-data fat-nest
+docker build fat-nest -t pyrocko-fat-nest
+
 docker build docs -t pyrocko-docs
 docker build util -t pyrocko-util
 docker build pyrocko -t pyrocko
---
+++ docker/fat-nest/Dockerfile
@@ -0,0 +1,2 @@
+FROM pyrocko-nest
+COPY pyrocko-test-data /pyrocko-test-data
