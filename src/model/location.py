fe199fa15a22767c627e9a9402454ec8405c18b5
Marius Isken <marius.isken@gfz-potsdam.de>
2021-10-18 18:07:19 +0200

    orthodrome: adding cross- and alongtrack distance
    upd
    
    upd
    
    docs: fixing docstring
    
    orthodrome: fix docstrings
    
    upd
    
    fixes

--- src/model/location.py
+++ src/model/location.py
@@ -7,13 +7,11 @@
 Representation of a geographical location, base class for stations, events,
 etc.
 '''
-
-import numpy as num
 import math
 
+import numpy as num
 from pyrocko import orthodrome
-from pyrocko.guts import Object, Float
-
+from pyrocko.guts import Float, Object
 
 guts_prefix = 'pf'
 
@@ -119,7 +117,11 @@ class Location(Object):
         '''
         Compute surface distance [m] to other location object.
 
+        :param other: Other location.
+        :type other: :py:class:`~pyrocko.model.location.Location`
 
+        :return: Distance to another location in [m].
+        :rtype: float
         '''
 
         if self.same_origin(other):
@@ -147,6 +149,11 @@ class Location(Object):
             \\Delta = \\sqrt{\\Delta {\\bf x}^2 + \\Delta {\\bf y}^2 + \
                       \\Delta {\\bf z}^2}
 
+        :param other: Other location.
+        :type other: :py:class:`~pyrocko.model.location.Location`
+
+        :return: 3D distance to another location in [m].
+        :rtype: float
         '''
 
         if self.same_origin(other):
@@ -170,6 +177,48 @@ class Location(Object):
             latlondepth_to_cartesian(lat, lon, -self.elevation + self.depth),
             dtype=float)
 
+    def crosstrack_distance_to(self, path_begin, path_end):
+        '''
+        Compute distance to a great-circle arc.
+
+        :param path_begin: Location of the start of the arc.
+        :param path_end: Location of the end of the arc.
+        :type path_begin: :py:class:`~pyrocko.model.location.Location`
+        :type path_end: :py:class:`~pyrocko.model.location.Location`
+
+        :return: Distance to a great circle arc in [deg].
+        :rtype: float
+        '''
+        return orthodrome.crosstrack_distance(
+            path_begin.effective_latlon[0], path_begin.effective_latlon[1],
+            path_end.effective_latlon[0], path_end.effective_latlon[1],
+            self.effective_latlon[0], self.effective_latlon[1]
+        )
+
+    def alongtrack_distance_to(self, path_begin, path_end, meter=False):
+        '''
+        Compute distance along a great-circle arc.
+
+        :param path_begin: Location of the start of the arc.
+        :param path_end: Location of the end of the arc.
+        :param meter: Return [m] instead of [deg].
+        :type path_begin: :py:class:`~pyrocko.model.location.Location`
+        :type path_end: :py:class:`~pyrocko.model.location.Location`
+
+        :return: Distance from the start of the great circle arc
+            in [deg] or [m].
+        :rtype: float
+        '''
+        func = orthodrome.alongtrack_distance
+        if meter:
+            func = orthodrome.alongtrack_distance_m
+
+        return func(
+            path_begin.effective_latlon[0], path_begin.effective_latlon[1],
+            path_end.effective_latlon[0], path_end.effective_latlon[1],
+            self.effective_latlon[0], self.effective_latlon[1]
+        )
+
     def offset_to(self, other):
         if self.same_origin(other):
             other_north_shift, other_east_shift = get_offset(other)
@@ -185,6 +234,12 @@ class Location(Object):
     def azibazi_to(self, other):
         '''
         Compute azimuth and backazimuth to and from other location object.
+
+        :param other: Other location.
+        :type other: :py:class:`~pyrocko.model.location.Location`
+
+        :return: Azimuth and back-azimuth to the location in [deg].
+        :rtype: tuple[float, float]
         '''
 
         if self.same_origin(other):
@@ -215,6 +270,76 @@ class Location(Object):
     def coords5(self):
         return num.array([
             self.lat, self.lon, self.north_shift, self.east_shift, self.depth])
+
+
+def filter_azimuths(locations, center, azimuth, azimuth_width):
+    '''
+    Filter locations by azimuth swath from a center location.
+
+    :param locations: list[Location]): List of Locations.
+    :param center: Relative center location.
+    :param azimuth: Azimuth in [deg]. -180 to 180 or 0 to 360.
+    :param azimuth_width: Width of the swath.
+    :type locations: list
+    :type center: Location
+    :type azimuth: float
+    :type azimuth_width: float
+
+    :return: Filtered locations.
+    :rtype: list[Location]
+    '''
+    filt_locations = []
+    for loc in locations:
+        loc_azi, _ = center.azibazi_to(loc)
+        angle = orthodrome.angle_difference(loc_azi, azimuth)
+        if abs(angle) <= azimuth_width / 2.:
+            filt_locations.append(loc)
+    return filt_locations
+
+
+def filter_distance(locations, reference, distance_min, distance_max):
+    '''Filter location by distance to a reference point.
+
+    :param locations: Locations to filter.
+    :param reference: Reference location.
+    :param distance_min: Minimum distance in [m].
+    :param distance_max: Maximum distance in [m].
+    :type locations: list
+    :type reference: Location
+    :type distance_min: float
+    :type distance_max: float
+
+    :return: Filtered locations.
+    :rtype: list[Location]
+    '''
+    return [
+        loc
+        for loc in locations
+        if distance_min <= loc.distance_to(reference) <= distance_max
+    ]
+
+
+def filter_crosstrack_distance(locations, start_path, end_path, distance_max):
+    '''Filter location by distance to a great-circle path.
+
+    :param locations: Locations to filter.
+    :param start_path: Start of the great circle path.
+    :param end_path: End of the great circle path.
+    :param distance_max: Distance to the great-circle in [deg].
+    :type locations: list
+    :type start_path: Location
+    :type end_path: Location
+    :type distance_max: float
+
+    :return: Filtered locations.
+    :rtype: list[Location]
+    '''
+    return [
+        loc
+        for loc in locations
+        if abs(loc.crosstrack_distance_to(
+            start_path, end_path)) <= distance_max
+    ]
 
 
 def get_offset(obj):
