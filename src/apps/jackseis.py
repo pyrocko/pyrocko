dd7ab93072a7fbe5f3bab383a5e9e78035173d58
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2021-12-17 12:12:13 +0100

    jackseis: add option to scale seismograms

--- src/apps/jackseis.py
+++ src/apps/jackseis.py
@@ -299,6 +299,10 @@ def process(get_pile, options):
 
                 traces = out_traces
 
+            if options.scale is not None:
+                for tr in traces:
+                    tr.ydata = options.scale * tr.ydata
+
             if options.output_data_type != 'same':
                 for tr in traces:
                     tr.ydata = tr.ydata.astype(
@@ -438,6 +442,14 @@ def main(args=None):
         dest='downsample',
         metavar='RATE',
         help='downsample to RATE [Hz]')
+
+    parser.add_option(
+        '--scale',
+        dest='scale',
+        type=float,
+        metavar='FACTOR',
+        help='scale seismograms with given FACTOR. Changes data type to '
+             'float64. Use --output-data-type to change again after scaling.')
 
     parser.add_option(
         '--output',
