94d397dac09e5f9660de7b81054cb14cc6f85f40
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2021-12-19 23:42:23 +0100

    add file headers

--- src/squirrel/io/backends/tdms_idas.py
+++ src/squirrel/io/backends/tdms_idas.py
@@ -1,3 +1,8 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
 from __future__ import absolute_import, print_function
 
 from pyrocko.io.io_common import get_stats, touch  # noqa
