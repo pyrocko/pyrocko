4f680dcd861717f4f1dd21e63cb5437590479a31
Pyrocko Test Debugger <info@pyrocko.org>
2022-03-04 11:31:53 +0100

    vagrant testing: fix centos8 box, eol exceeded, use different mirrors

--- maintenance/vagrant/centos-8/inside.sh
+++ maintenance/vagrant/centos-8/inside.sh
@@ -18,7 +18,11 @@ outfile_py3="/vagrant/test-$branch.py3.out"
 rm -f "$outfile_py3"
 
 cd $HOME
-sudo yum -y install git make gcc mesa-libGL python3
+sudo sed -i 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo yum -y update
+
+sudo yum -y install git make gcc mesa-libGL python3 patch
 
 if [ -e "$pyrockodir" ] ; then
     sudo rm -rf "$pyrockodir"
