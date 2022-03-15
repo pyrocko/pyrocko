ee109240797241d8d8d985d662239b5614353625
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2022-03-15 09:16:41 +0100

    libmseed: silence compiler warnings
    - includes a fix for blockette 395, where byte swapping of timestamp seems to
      have been broken (Calibration Abort Blockette, end of calibration time)

--- prerequisites/prerequisites.bat
+++ prerequisites/prerequisites.bat
@@ -4,6 +4,8 @@
 IF NOT EXIST "libmseed" (
     tar -xzf libmseed-2.19.6.tar.gz --exclude=doc --exclude=test --exclude=example
     patch -s -p0 < libmseed-2.19.6-speedread.patch
+    patch -s -p0 < libmseed-2.19.6-selection-fallthru.patch
+    patch -s -p0 < libmseed-2.19.6-fix-blkt-395-bswap.patch
 ) ELSE (
     ECHO libmseed found
 )
--- prerequisites/prerequisites.sh
+++ prerequisites/prerequisites.sh
@@ -6,6 +6,8 @@ if [ ! -d libmseed ]; then
     rm -rf libmseed
     tar -xzf libmseed-2.19.6.tar.gz --exclude=doc --exclude=test --exclude=example
     patch -s -p0 < libmseed-2.19.6-speedread.patch
+    patch -s -p0 < libmseed-2.19.6-selection-fallthru.patch
+    patch -s -p0 < libmseed-2.19.6-fix-blkt-395-bswap.patch
 fi
 
 if [ ! -d evalresp-3.3.0/lib ]; then
