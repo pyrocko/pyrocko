7d79ba6cf74f1faf16d7bf2c0a8d3465372640ee
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2022-04-27 12:53:10 +0200

    update conda packaging

--- maintenance/conda/build_packages_windows.sh
+++ maintenance/conda/build_packages_windows.sh
@@ -35,7 +35,7 @@ else
     conda config --set anaconda_upload no
 fi
 
-conda-build --python 3.6 build
+# conda-build --python 3.6 build
 conda-build --python 3.7 build
 conda-build --python 3.8 build
 conda-build --python 3.9 build
