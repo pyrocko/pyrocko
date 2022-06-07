5fd5dd39edf1a9e1406c0faa686a668d21c3070a
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2022-06-07 09:44:12 +0200

    squirrel scan: improve description

--- src/squirrel/tool/commands/scan.py
+++ src/squirrel/tool/commands/scan.py
@@ -16,10 +16,10 @@ def make_subparser(subparsers):
 
 Read and cache meta-data of all files in formats understood by Squirrel under
 selected paths. Subdirectories are recursively traversed and file formats are
-auto-detected unless a specific format is forced with the --format option.
+auto-detected unless a specific format is forced with the ``--format`` option.
 Modification times of files already known to Squirrel are checked by default
 and re-indexed as needed. To speed up scanning, these checks can be disabled
-with the --optimistic option. With this option, only new files are indexed
+with the ``--optimistic`` option. With this option, only new files are indexed
 during scanning and modifications are handled "last minute" (i.e. just before
 the actual data (e.g. waveform samples) are requested by the application).
 
@@ -28,7 +28,7 @@ application through a runtime selection which is discarded again when the
 application quits. Getting the cached meta-data into the runtime selection can
 be a bottleneck for application startup with large datasets. To speed up
 startup of Squirrel-based applications, persistent selections created with the
---persistent option can be used.
+``--persistent`` option can be used.
 
 After scanning, information about the current data selection is printed.
 ''')
