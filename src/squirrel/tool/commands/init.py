bc80115f4913ce259e5e48e9281ec8d99df624e6
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2022-06-07 09:43:35 +0200

    squirrel init: improve description

--- src/squirrel/tool/commands/init.py
+++ src/squirrel/tool/commands/init.py
@@ -10,12 +10,29 @@ from pyrocko import squirrel as sq
 
 headline = 'Create local environment.'
 
+description = headline + '''
+
+Squirrel based applications can either use the user’s global Squirrel
+environment or a project specific local environment. Running this command in a
+project's top level directory creates a local environment. Any Squirrel
+application started thereafter in this directory or any of its subdirectories,
+will use the local enviroment instead of the global one.
+
+The local enviroment consists of a directory ```.squirrel``` which contains
+the Squirrel's database and a cache directory.
+
+A local environment can be removed by removing its ```.squirrel``` directory.
+
+The user's global enviroment database resides in Pyrocko’s global cache
+directory, by default in ```$HOME/.pyrocko/cache/squirrel```.
+'''
+
 
 def make_subparser(subparsers):
     return subparsers.add_parser(
         'init',
         help=headline,
-        description=headline)
+        description=description)
 
 
 def setup(parser):
