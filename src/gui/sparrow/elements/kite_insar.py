65030993b0ef604d233f4cafbf991bfccf44dda2
mmetz <mmetz@gfz-potsdam.de>
2022-11-28 13:41:18 +0100

    sparrow elements: appearance element controls

--- src/gui/sparrow/elements/kite_insar.py
+++ src/gui/sparrow/elements/kite_insar.py
@@ -166,6 +166,8 @@ class KiteElement(Element):
             layout.addWidget(cb, 1, 0)
             state_bind_checkbox(self, self._state, 'visible', cb)
 
+            layout.addWidget(qw.QFrame(), 3, 0, 1, 2)
+
             self._controls = frame
 
         return self._controls
