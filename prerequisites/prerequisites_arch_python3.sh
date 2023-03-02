d473ede66b6ab0d171fecdd4fff6f53971341109
Pyrocko Test Debugger <info@pyrocko.org>
2023-03-02 09:33:10 +0100

    prerequisites: update arch prerequisite list

--- prerequisites/prerequisites_arch_python3.sh
+++ prerequisites/prerequisites_arch_python3.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu --noconfirm --needed \
     git make gcc patch python python-setuptools python-pip python-wheel \
     python-numpy python-scipy python-matplotlib \
-    python-pyqt5 qt5-webengine qt5-svg qt5-webkit \
+    python-pyqt5 qt5-webengine qt5-svg \
     python-cairo \
     python-requests python-yaml python-jinja \
     python-pytest python-coverage
