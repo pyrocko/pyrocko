d4783d2d918e08362be0752ccb9922abc312640e
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2023-03-28 10:27:34 +0200

    pip wheels: enable build on py311

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -6,7 +6,7 @@ rm -rf wheels_temp wheels
 
 mkdir wheels_temp
 for x in `ls /opt/python` ; do
-    "/opt/python/$x/bin/python" -c 'import sys ; sys.exit(not ((3, 7, 0) <= sys.version_info <= (3, 11, 0)) or sys.implementation.name != "cpython")' || continue
+    "/opt/python/$x/bin/python" -c 'import sys ; sys.exit(not ((3, 7, 0) <= sys.version_info) or sys.implementation.name != "cpython")' || continue
     "/opt/python/$x/bin/pip" install --upgrade pip
     "/opt/python/$x/bin/pip" wheel -v . -w wheels_temp --only-binary=:all:
 done
