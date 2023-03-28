6745f3907740b16584fd749f8187264804375396
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2023-03-28 11:40:26 +0200

    pip: build macos wheels

--- maintenance/pip/upload_wheels.sh
+++ maintenance/pip/upload_wheels.sh
@@ -6,12 +6,16 @@ if [ -z "$destination" ] ; then
     echo "usage: upload_wheels.sh (testing|live)"
 fi
 
+if [ -z "$TWINE" ] ; then
+    TWINE=twine
+fi
+
 if [ "$destination" == 'live' ] ; then
-    twine upload dist/pyrocko-* \
+    $TWINE upload dist/pyrocko-* \
         --username="$PYPI_USERNAME" --password="$PYPI_PASSWORD" \
         --skip-existing --disable-progress-bar
 else
-    twine upload --repository-url https://test.pypi.org/legacy/ dist/pyrocko-* \
+    $TWINE upload --repository-url https://test.pypi.org/legacy/ dist/pyrocko-* \
         --username="$PYPI_USERNAME" --password="$PYPI_PASSWORD" \
         --skip-existing --disable-progress-bar
 fi
