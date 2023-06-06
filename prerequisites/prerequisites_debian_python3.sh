18d626a1ad16416bca2a455fce4c05c7e4158196
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2023-06-06 22:29:28 +0200

    prerequisites: update debian prereqs

--- prerequisites/prerequisites_debian_python3.sh
+++ prerequisites/prerequisites_debian_python3.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y make git python3-dev python3-setuptools python3-pip \
     python3-pyqt5.qtsvg python3-pyqt5.qtwebkit python3-yaml \
     python3-jinja2 python3-requests python3-coverage \
     python3-pytest
+sudo apt-get install -y python3-vtk9 \
+    || sudo apt-get install -y python3-vtk8 \
+    || sudo apt-get install -y python3-vtk7
