fa7643c5d170b20a2dfd745489befb96183e556e
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2023-06-15 11:45:20 +0200

    beachball example: add polarities

--- examples/beachball_example04.py
+++ examples/beachball_example04.py
@@ -11,7 +11,13 @@ mt = pmt.MomentTensor.random_dc()
 
 # receiver positions
 rdepth = 0.0
-rlatlons = [(50., 10.), (60., -50.), (-30., 60.)]
+rlatlons = [
+    (50., 10.),
+    (60., -50.),
+    (-30., 60.),
+    (0.8, 0.8),  # first arrival is P  -> takeoff angle < 90deg
+    (0.5, 0.5),  # first arrival is p  -> takeoff angle > 90deg
+]
 
 # earth model and phase for takeoff angle computations
 mod = cake.load_model('ak135-f-continental.m')
@@ -38,7 +44,7 @@ beachball.plot_beachball_mpl(
 for rlat, rlon in rlatlons:
     distance = orthodrome.distance_accurate50m(slat, slon, rlat, rlon)
     rays = mod.arrivals(
-        phases=cake.PhaseDef('P'),
+        phases=[cake.PhaseDef('P'), cake.PhaseDef('p')],
         zstart=sdepth, zstop=rdepth, distances=[distance*cake.m2d])
 
     if not rays:
@@ -46,16 +52,26 @@ for rlat, rlon in rlatlons:
 
     takeoff = rays[0].takeoff_angle()
     azi = orthodrome.azimuth(slat, slon, rlat, rlon)
+    polarity = num.sign(beachball.amplitudes(mt, [azi], [takeoff]))
 
     # to spherical coordinates, r, theta, phi in radians
-    rtp = num.array([[1., num.deg2rad(takeoff), num.deg2rad(90.-azi)]])
-
+    # flip direction when takeoff is upward
+    rtp = num.array([[
+        1.0 if takeoff <= 90. else -1.,
+        num.deg2rad(takeoff),
+        num.deg2rad(90.-azi)]])
     # to 3D coordinates (x, y, z)
     points = beachball.numpy_rtp2xyz(rtp)
 
     # project to 2D with same projection as used in beachball
     x, y = beachball.project(points, projection=projection).T
 
-    axes.plot(x, y, '+', ms=10., mew=2.0, mec='black', mfc='none')
+    axes.plot(
+        x, y,
+        '+' if polarity > 0.0 else 'x',
+        ms=10. if polarity > 0.0 else 10./num.sqrt(2.),
+        mew=2.0,
+        mec='black',
+        mfc='none')
 
 fig.savefig('beachball-example04.png')
