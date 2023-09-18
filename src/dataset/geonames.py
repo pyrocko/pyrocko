0948b874563b77ab350ee1dd72c1897c2c1e2fff
hvasbath <Hannes.Bathke@gmx.net>
2023-09-18 15:49:49 +0200

    dataset.geonames: add country related data

--- src/dataset/geonames.py
+++ src/dataset/geonames.py
@@ -25,6 +25,9 @@ from pyrocko import util, config
 from pyrocko import orthodrome as od
 from pyrocko.util import urlopen
 
+from itertools import chain
+
+
 logger = logging.getLogger('pyrocko.dataset.geonames')
 
 GeoName = namedtuple('GeoName', '''
@@ -57,16 +60,37 @@ population
 feature_code
 '''.split())
 
+
+Country = namedtuple('Country', '''
+ISO
+name
+capital
+population
+area
+feature_code
+lat
+lon
+'''.split())
+
+
 base_url = 'https://mirror.pyrocko.org/download.geonames.org/export/dump/'
+fallback_url = 'https://download.geonames.org/export/dump/'
 
 
 def download_file(fn, dirpath):
-    url = base_url + '/' + fn
+    url = base_url + fn
     fpath = op.join(dirpath, fn)
     logger.info('starting download of %s' % url)
 
     util.ensuredirs(fpath)
-    f = urlopen(url)
+    try:
+        f = urlopen(url)
+    except OSError:
+        logger.info('File not found in pyrocko mirror, '
+                    'falling back to original source.')
+        url = fallback_url + fn
+        f = urlopen(url)
+
     fpath_tmp = fpath + '.%i.temp' % os.getpid()
     g = open(fpath_tmp, 'wb')
     while True:
@@ -105,14 +129,12 @@ def ascii_str(u):
     return u.encode('ascii', 'replace')
 
 
-def load(zfn, fn, minpop=1000000, region=None):
+def get_file(zfn, fn):
+
     geonames_dir = config.config().geonames_dir
     filepath = op.join(geonames_dir, zfn or fn)
     if not os.path.exists(filepath):
         download_file(zfn or fn, geonames_dir)
-
-    if region:
-        w, e, s, n = positive_region(region)
 
     if zfn is not None:
         import zipfile
@@ -121,6 +143,79 @@ def load(zfn, fn, minpop=1000000, region=None):
     else:
         z = None
         f = open(filepath, 'rb')
+
+    return f, z
+
+
+def load_country_info(zfn, fn, minpop=1000000):
+
+    f, z = get_file(zfn, fn)
+
+    countries = {}
+    for line in f:
+        t = line.split(b'\t')
+        start = t[0].decode('utf8')[0]
+        if start != '#':
+            population = int(t[7])
+            if minpop <= population:
+                iso = t[0].decode('utf8')
+                name = t[4].decode('utf8')
+                capital = t[5].decode('utf8')
+                area = t[6].decode('utf8')
+                feature_code = str(t[16].decode('ascii'))
+                countries[feature_code] = (
+                    iso, name, capital, population, area, feature_code)
+
+    f.close()
+    if z is not None:
+        z.close()
+
+    return countries
+
+
+def load_country_shapes_json(zfn, fn):
+
+    f, z = get_file(zfn, fn)
+
+    import json
+
+    data = json.load(f)
+    fts = data["features"]
+
+    mid_points_countries = {}
+    for ft in fts:
+        geonameid = ft["properties"]["geoNameId"]
+        coords = ft["geometry"]["coordinates"]
+        coords_arr = num.vstack(
+            [num.array(sco) for co in chain(coords) for sco in chain(co)])
+        latlon = od.geographic_midpoint(coords_arr[:, 0], coords_arr[:, 1])
+        mid_points_countries[geonameid] = latlon
+
+    return mid_points_countries
+
+
+def load_all_countries(minpop=1000000):
+
+    country_infos = load_country_info(
+        None, "countryInfo.txt", minpop=minpop)
+    mid_points = load_country_shapes_json(
+        "shapes_simplified_low.json.zip", "shapes_simplified_low.json")
+
+    for geonameid, infos in country_infos.items():
+        try:
+            lat, lon = mid_points[geonameid]
+        except KeyError:
+            lat = lon = None
+
+        yield Country(*infos, lat, lon)
+
+
+def load_cities(zfn, fn, minpop=1000000, region=None):
+
+    f, z = get_file(zfn, fn)
+
+    if region:
+        w, e, s, n = positive_region(region)
 
     for line in f:
         t = line.split(b'\t')
@@ -149,7 +244,7 @@ g_cities = {}
 
 def load_all_keep(zfn, fn, minpop=1000000, region=None, exclude=()):
     if (zfn, fn) not in g_cities:
-        g_cities[zfn, fn] = list(load(zfn, fn, minpop=0))
+        g_cities[zfn, fn] = list(load_cities(zfn, fn, minpop=0))
 
     if region:
         w, e, s, n = positive_region(region)
--- test/base/test_geonames.py
+++ test/base/test_geonames.py
@@ -13,6 +13,12 @@ class GeonamesTestCase(unittest.TestCase):
             sorted(c.asciiname for c in cities),
             ['Bremen', 'Hamburg', 'Kiel', 'Luebeck'])
 
+    def test_geonames_countries(self):
+
+        countries = geonames.load_all_countries()
+        for country in countries:
+            print(country)
+
 
 if __name__ == '__main__':
     util.setup_logging('test_geonames', 'warning')
