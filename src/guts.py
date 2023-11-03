pyrocko/pyrocko

commit 72602a208ce0033b339237a7709922ba49ad6e32

squirrel fdsn: save metadata as pickle for speedup

src/guts.py
@@ -11,8 +11,10 @@ import datetime
 import calendar
 import re
 import sys
+import os
 import types
 import copy
+import logging
 import os.path as op
 from collections import defaultdict
 from base64 import b64decode, b64encode
@@ -33,6 +35,8 @@ except ImportError:
 from .util import time_to_str, str_to_time, TimeStrError, hpfloat, \
     get_time_float
 
+
+logger = logging.getLogger('pyrocko.guts')
 
 ALLOW_INCLUDE = False
 
@@ -2176,6 +2180,7 @@ def _iload_all_xml(
         ns_ignore=False):
 
     from xml.parsers.expat import ParserCreate
+    from pyrocko.progress import progress
 
     parser = ParserCreate('UTF-8', namespace_separator=' ')
 
@@ -2191,14 +2196,31 @@ def _iload_all_xml(
     parser.StartNamespaceDeclHandler = handler.start_namespace
     parser.EndNamespaceDeclHandler = handler.end_namespace
 
-    while True:
-        data = stream.read(bufsize)
-        parser.Parse(data, bool(not data))
-        for element in handler.get_queued_elements():
-            yield element
+    try:
+        nbytes = os.fstat(stream.fileno()).st_size - stream.tell()
+    except Exception:
+        nbytes = None
 
-        if not data:
-            break
+    ibytes = 0
+    task = progress.task('Parsing XML', nbytes, logger=logger)
+    try:
+        while True:
+            data = stream.read(bufsize)
+            ibytes += len(data)
+            parser.Parse(data, bool(not data))
+            for element in handler.get_queued_elements():
+                yield element
+
+            task.update(ibytes)
+
+            if not data:
+                break
+
+    except Exception:
+        task.fail()
+
+    finally:
+        task.done()
 
 
 def _load_all_xml(*args, **kwargs):
@@ -2688,6 +2710,60 @@ def load_all_xml(stream, **kwargs):
 def iload_all_xml(stream, **kwargs):
     kwargs.pop('filename', None)
     return _iload_all_xml(stream, **kwargs)
+
+
+def _dump_all_spickle(objects, stream):
+    import pickle
+    header = b'SPICKLE'.ljust(512)
+    stream.write(header, )
+    for obj in objects:
+        pickle.dump(obj, stream)
+
+
+def _iload_all_spickle(stream):
+    for obj, _ in _iload_all_spickle_internal(stream):
+        yield obj
+
+
+def _load_one_spickle_internal(stream):
+    import pickle
+    fpos = stream.tell()
+    return pickle.load(stream), fpos
+
+
+def _iload_all_spickle_internal(stream, offset=None):
+    if offset is not None:
+        stream.seek(offset, 0)
+    else:
+        header = stream.read(512)
+        if not header.startswith(b'SPICKLE'):
+            raise ValueError('Not a SPICKLE file.')
+
+    while True:
+        try:
+            yield _load_one_spickle_internal(stream)
+        except EOFError:
+            break
+
+
+def _load_all_spickle(stream):
+    return list(_iload_all_spickle(stream))
+
+
+@expand_stream_args('w')
+def dump_all_spickle(objects, stream, **kwargs):
+    return _dump_all_spickle(objects, stream)
+
+
+@expand_stream_args('r')
+def iload_all_spickle(stream, **kwargs):
+    _iload_all_spickle(stream)
+
+
+@expand_stream_args('r')
+def load_all_spickle(stream, **kwargs):
+    kwargs.pop('filename', None)
+    return _load_all_spickle(stream, **kwargs)
 
 
 __all__ = guts_types + [

src/io/stationxml.py
@@ -1903,7 +1903,8 @@ class FDSNStationXML(Object):
 
     def get_pyrocko_stations(self, nslcs=None, nsls=None,
                              time=None, timespan=None,
-                             inconsistencies='warn'):
+                             inconsistencies='warn',
+                             sloppy=False):
 
         assert inconsistencies in ('raise', 'warn')
 
@@ -1957,8 +1958,10 @@ class FDSNStationXML(Object):
                             continue
 
                         for channel in channels:
-                            k = nsl, channel.code[:-1], \
-                                channel.start_date, channel.end_date
+                            k = (nsl, channel.code[:-1])
+                            if not sloppy:
+                                k += (channel.start_date, channel.end_date)
+
                             sensor_to_channels[k].append(channel)
 
                 else:
@@ -1982,7 +1985,8 @@ class FDSNStationXML(Object):
                         name=station.description or ''))
 
         sensor_have = set()
-        for (nsl, bi, _, _), channels in sensor_to_channels.items():
+        for k, channels in sensor_to_channels.items():
+            (nsl, bi) = k[:2]
             if (nsl, bi) in sensor_have:
                 message = 'Duplicate station ' \
                     '(multiple epochs match): %s.%s.%s' % nsl

src/squirrel/client/fdsn.py
@@ -34,7 +34,7 @@ from pyrocko.progress import progress
 from pyrocko import has_paths
 
 from pyrocko.guts import Object, String, Timestamp, List, Tuple, Int, Dict, \
-    Duration, Bool, clone
+    Duration, Bool, clone, dump_all_spickle
 
 guts_prefix = 'squirrel'
 
@@ -460,7 +460,9 @@ class FDSNSource(Source, has_paths.HasPaths):
                 fn = self._get_channels_path()
                 util.ensuredirs(fn)
                 fn_temp = fn + '.%i.temp' % os.getpid()
-                channel_sx.dump_xml(filename=fn_temp)
+
+                dump_all_spickle([channel_sx], filename=fn_temp)
+                # channel_sx.dump_xml(filename=fn_temp)
 
                 status = move_or_keep(fn_temp, fn)
 
@@ -853,7 +855,9 @@ class FDSNSource(Source, has_paths.HasPaths):
                     fn_temp = fn + '.%i.temp' % os.getpid()
 
                     util.ensuredirs(fn_temp)
-                    response_sx.dump_xml(filename=fn_temp)
+
+                    dump_all_spickle([response_sx], filename=fn_temp)
+                    # response_sx.dump_xml(filename=fn_temp)
 
                     status_this = move_or_keep(fn_temp, fn)
 

src/squirrel/io/backends/spickle.py
@@ -1,0 +1,41 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+'''
+Squirrel IO adaptor to read YAML files.
+'''
+
+import logging
+
+from pyrocko.io.io_common import get_stats, touch  # noqa
+from pyrocko import guts
+from pyrocko.io.io_common import FileLoadError
+
+logger = logging.getLogger('psq.io.spickle')
+
+
+def provided_formats():
+    return ['spickle']
+
+
+def detect(first512):
+    return 'spickle' if first512.startswith(b'SPICKLE') else None
+
+
+def iload(format, file_path, segment, content):
+    from pyrocko.io import stationxml
+
+    with open(file_path, 'rb') as f:
+        for obj, segment in guts._iload_all_spickle_internal(
+                f, offset=segment):
+
+            if isinstance(obj, stationxml.FDSNStationXML):
+                from .stationxml import iload_stationxml
+                yield from iload_stationxml(obj, segment, content)
+            else:
+                raise FileLoadError(
+                    'Stored object of type %s is not supported by Squirrel '
+                    'framework. Found at offset %s in spickle file: %s'
+                    % (str(type(obj)), segment, file_path))

src/squirrel/io/backends/stationxml.py
@@ -33,15 +33,18 @@ def detect(first512):
 
 def iload(format, file_path, segment, content):
     assert format == 'stationxml'
+    from pyrocko.io import stationxml
+    sx = stationxml.load_xml(filename=file_path)
+    yield from iload_stationxml(sx, 0, content)
+
+
+def iload_stationxml(sx, segment, content):
+    inut = 0
 
     far_future = time.time() + 20*Y
 
     from pyrocko.io import stationxml
     value_or_none = stationxml.value_or_none
-
-    sx = stationxml.load_xml(filename=file_path)
-
-    inut = 0
 
     for network in sx.network_list:
         for station in network.station_list:
@@ -54,7 +57,7 @@ def iload(format, file_path, segment, content):
                 tmax = None
 
             station_nut = model.make_station_nut(
-                file_segment=0,
+                file_segment=segment,
                 file_element=inut,
                 codes=model.CodesNSL(net, sta, '*'),
                 tmin=tmin,
@@ -96,7 +99,7 @@ def iload(format, file_path, segment, content):
                         deltat = 1.0 / out_rate_resp
 
                 nut = model.make_channel_nut(
-                    file_segment=0,
+                    file_segment=segment,
                     file_element=inut,
                     codes=model.CodesNSLCE(net, sta, loc, cha, ''),
                     tmin=tmin,
@@ -125,7 +128,7 @@ def iload(format, file_path, segment, content):
                 if channel.response:
 
                     nut = model.make_response_nut(
-                        file_segment=0,
+                        file_segment=segment,
                         file_element=inut,
                         codes=model.CodesNSLCE(net, sta, loc, cha, ''),
                         tmin=tmin,

src/squirrel/io/base.py
@@ -15,12 +15,14 @@ from pyrocko.io.io_common import FileLoadError
 from pyrocko.progress import progress
 
 from .backends import \
-    mseed, sac, datacube, stationxml, textfiles, virtual, yaml, tdms_idas
+    mseed, sac, datacube, stationxml, textfiles, virtual, yaml, tdms_idas, \
+    spickle
 
 from ..model import to_kind_ids
 
 backend_modules = [
-    mseed, sac, datacube, stationxml, textfiles, virtual, yaml, tdms_idas]
+    mseed, sac, datacube, stationxml, textfiles, virtual, yaml, tdms_idas,
+    spickle]
 
 
 logger = logging.getLogger('psq.io')

commit 20e8f0aa9e3c7f9ef509e1f686a5241077045adc

guts: fix error introduced in 72602a208ce0033b339237a7709922ba49ad6e32

src/guts.py
@@ -2218,6 +2218,7 @@ def _iload_all_xml(
 
     except Exception:
         task.fail()
+        raise
 
     finally:
         task.done()