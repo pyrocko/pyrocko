142337d5ad05375b58e86fc7356b4e4887598d78
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2023-11-03 12:08:45 +0100

    location: include elevation in coords5 property

--- src/model/location.py
+++ src/model/location.py
@@ -215,7 +215,8 @@ class Location(Object):
     @property
     def coords5(self):
         return num.array([
-            self.lat, self.lon, self.north_shift, self.east_shift, self.depth])
+            self.lat, self.lon, self.north_shift, self.east_shift,
+            -self.elevation + self.depth])
 
 
 def get_offset(obj):
