c66471fa26f853de360815e02d9495fabaa2946f
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2024-01-03 10:20:21 +0100

    conda packaging: support Python 3.12

--- maintenance/conda/build_packages_osx_arm64.sh
+++ maintenance/conda/build_packages_osx_arm64.sh
@@ -44,6 +44,7 @@ conda-build --python 3.8 --numpy 1.21 build
 conda-build --python 3.9 --numpy 1.21 build
 conda-build --python 3.10 --numpy 1.21 build
 conda-build --python 3.11 --numpy 1.23 build
+conda-build --python 3.12 --numpy 1.23 build
 
 if [ "$ACTION" == "upload" ] ; then
     trap - EXIT
