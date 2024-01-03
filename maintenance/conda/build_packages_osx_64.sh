233e1543acf02d159898cd199bb33dcc4d48cadb
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2024-01-03 22:23:27 +0100

    conda packaging: wip

---
+++ maintenance/conda/build_packages_osx_64.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f "build_packages_osx_arm64.sh" ] ; then
+    echo 'must be run from inside maintenance/conda'
+    exit 1
+fi
+
+if [ ! -z "$(git status --untracked-files=no --porcelain)" ]; then
+    echo "repos not clean"
+    exit 1
+fi
+
+
+ACTION="$1"
+
+if [ -z "$ACTION" ] ; then
+    echo "usage: build_packages.sh (dryrun|upload)"
+    exit 1
+fi
+
+if [ "$ACTION" == "upload" ] ; then
+    if [ -z "$CONDA_USERNAME" -o -z "$CONDA_PASSWORD" ] ; then
+        echo "need anaconda credentials as env variables"
+        exit 1
+    fi
+fi
+
+conda install -y conda-build conda-verify anaconda-client numpy
+
+if [ "$ACTION" == "upload" ] ; then
+    anaconda login --username "$CONDA_USERNAME" --password "$CONDA_PASSWORD" --hostname conda-builder-`uname`
+    conda config --set anaconda_upload yes
+    function anaconda_logout {
+        anaconda logout
+    }
+    trap anaconda_logout EXIT
+else
+    conda config --set anaconda_upload no
+fi
+
+conda-build --python 3.7 --numpy 1.18 build
+conda-build --python 3.8 --numpy 1.18 build
+conda-build --python 3.9 --numpy 1.21 build
+conda-build --python 3.10 --numpy 1.21 build
+conda-build --python 3.11 --numpy 1.23 build
+conda-build --python 3.12 --numpy 1.26 build
+
+if [ "$ACTION" == "upload" ] ; then
+    trap - EXIT
+    anaconda_logout
+fi
