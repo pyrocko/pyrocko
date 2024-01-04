75e833356c778c3eef9e5b4545ba6c80b9b446b8
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2024-01-04 14:00:26 +0100

    wip

--- maintenance/conda/prepare_miniconda_osx.sh
+++ maintenance/conda/prepare_miniconda_osx.sh
@@ -4,12 +4,12 @@ set -e
 
 cd $HOME
 
-sudo echo 'preparing miniconda for arm64 and x68_64 for conda build on osx'
+sudo echo 'preparing miniconda for arm64 and x86_64 for conda build on osx'
 
 if [ ! -d prepare_miniconda_packages ] ; then
     mkdir prepare_miniconda_packages
     curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o prepare_miniconda_packages/miniconda3_arm64.sh
-    curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o prepare_miniconda_packages/miniconda3_x68_64.sh
+    curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o prepare_miniconda_packages/miniconda3_x86_64.sh
     curl -L https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.9.sdk.tar.xz -o prepare_miniconda_packages/MacOSX10.9.sdk.tar.xz
     curl -L https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.0.sdk.tar.xz -o prepare_miniconda_packages/MacOSX11.0.sdk.tar.xz
 fi
@@ -21,10 +21,10 @@ source miniconda3_arm64/bin/activate
 conda update -y -n base -c defaults conda
 conda deactivate
 
-rm -rf miniconda3_x68_64
-mkdir -p miniconda3_x68_64
-bash prepare_miniconda_packages/miniconda3_x68_64.sh -b -u -p ~/miniconda3_x68_64
-source miniconda3_x68_64/bin/activate
+rm -rf miniconda3_x86_64
+mkdir -p miniconda3_x86_64
+bash prepare_miniconda_packages/miniconda3_x86_64.sh -b -u -p ~/miniconda3_x86_64
+source miniconda3_x86_64/bin/activate
 conda update -y -n base -c defaults conda
 conda deactivate
 
