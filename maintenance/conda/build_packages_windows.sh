ce2205d308a33c80e6fb74ef0a90a81598067236
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2024-01-08 09:55:32 +0100

    conda builds on windows: workaround for bug in git on windows

--- maintenance/conda/build_packages_windows.sh
+++ maintenance/conda/build_packages_windows.sh
@@ -40,12 +40,12 @@ else
     conda config --set anaconda_upload no
 fi
 
-conda-build --python 3.7 --numpy 1.18 build
-conda-build --python 3.8 --numpy 1.18 build
-conda-build --python 3.9 --numpy 1.21 build
-conda-build --python 3.10 --numpy 1.21 build
-conda-build --python 3.11 --numpy 1.23 build
-conda-build --python 3.12 --numpy 1.26 build
+conda-build --python 3.7 --numpy 1.18 build_windows
+conda-build --python 3.8 --numpy 1.18 build_windows
+conda-build --python 3.9 --numpy 1.21 build_windows
+conda-build --python 3.10 --numpy 1.21 build_windows
+conda-build --python 3.11 --numpy 1.23 build_windows
+conda-build --python 3.12 --numpy 1.26 build_windows
 
 if [ "$ACTION" == "upload" ] ; then
     trap - EXIT
