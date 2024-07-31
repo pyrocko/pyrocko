53cef6b14d3e891401f8467017b4fa68fe2f456a
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2024-07-31 13:40:46 +0200

    wip: squirrel: service

--- src/squirrel/service/page/js/squirrel-connection.js
+++ src/squirrel/service/page/js/squirrel-connection.js
@@ -1,99 +1,95 @@
-import { ref, computed } from "./vue.esm-browser.js";
+import { ref, computed } from './vue.esm-browser.js'
 
 const now = () => {
-    return new Date().getTime();
-};
+    return new Date().getTime()
+}
 
 const fmtDuration = (d) => {
-    const h = Math.floor(d / 3600);
-    const m = Math.floor((d % 3600) / 60);
-    const s = d % 60;
+    const h = Math.floor(d / 3600)
+    const m = Math.floor((d % 3600) / 60)
+    const s = d % 60
     return (
-        (h > 0 ? h.toFixed(0) + "h " : "") +
-        (h > 0 || m > 0 ? m.toFixed(0) + "m " : "") +
-        (s.toFixed(0) + "s")
-    );
-};
+        (h > 0 ? h.toFixed(0) + 'h ' : '') +
+        (h > 0 || m > 0 ? m.toFixed(0) + 'm ' : '') +
+        (s.toFixed(0) + 's')
+    )
+}
 
 export const squirrelRequest = async (method, args) => {
-    const response = await fetch("/squirrel/" + method, {
-        method: "POST",
+    const response = await fetch('/squirrel/' + method, {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(args),
-    });
-    return response.json();
-};
+    })
+    return response.json()
+}
 
 const setupConnection = () => {
-    let serverInfo = ref(null);
-    let heartbeats = [];
+    let serverInfo = ref(null)
+    let heartbeats = []
 
     const getServerInfo = async () => {
-        serverInfo.value = await squirrelRequest("server_info");
-    };
+        serverInfo.value = await squirrelRequest('server_info')
+    }
 
-    let abortHeartbeat = null;
+    let abortHeartbeat = null
 
     const receiveHeartbeat = async () => {
-        abortHeartbeat = new AbortController();
-        const response = await fetch("/squirrel/heartbeat", {
+        abortHeartbeat = new AbortController()
+        const response = await fetch('/squirrel/heartbeat', {
             signal: abortHeartbeat.signal,
-        });
+        })
 
-        const decoder = new TextDecoder("utf-8");
+        const decoder = new TextDecoder('utf-8')
         try {
             for await (const chunk of response.body) {
-                const heartbeat = JSON.parse(decoder.decode(chunk));
-                heartbeat["time_now_client"] = now();
-                heartbeats.push(heartbeat);
+                const heartbeat = JSON.parse(decoder.decode(chunk))
+                heartbeat['time_now_client'] = now()
+                heartbeats.push(heartbeat)
                 if (heartbeats.length > 10) {
-                    heartbeats.splice(
-                        0,
-                        heartbeats.length - 10
-                    );
+                    heartbeats.splice(0, heartbeats.length - 10)
                 }
-                console.log('beat');
+                console.log('beat')
             }
         } catch (e) {
-            heartbeats = [];
-            abortHeartbeat = null;
+            heartbeats = []
+            abortHeartbeat = null
         }
-    };
+    }
 
-    let tick = ref(0);
+    let tick = ref(0)
     setInterval(() => {
-        tick.value++;
-    }, 1000);
+        tick.value++
+    }, 1000)
 
     const connected = computed(() => {
-        tick.value;
+        tick.value
         if (heartbeats.length == 0) {
-            return null;
+            return null
         } else {
-            let latest =
-                heartbeats[heartbeats.length - 1];
-            let delay = now() - latest["time_now_client"];
+            let latest = heartbeats[heartbeats.length - 1]
+            let delay = now() - latest['time_now_client']
             if (delay < 3000.0) {
                 return {
                     duration: fmtDuration(
-                        latest["time_now"] - latest["time_start"]
+                        latest['time_now'] - latest['time_start']
                     ),
-                };
+                }
             } else {
-                return null;
+                return null
             }
         }
-    });
+    })
 
-    getServerInfo();
-    receiveHeartbeat();
+    getServerInfo()
+    receiveHeartbeat()
 
     return { connected, serverInfo, request: squirrelRequest }
 }
 
-let connection = null;
+let connection = null
 
 export const squirrelConnection = () => {
     if (connection === null) {
@@ -103,4 +99,3 @@ export const squirrelConnection = () => {
     console.log('get')
     return connection
 }
-
--- src/squirrel/service/page/js/squirrel-map.js
+++ src/squirrel/service/page/js/squirrel-map.js
@@ -1,93 +1,93 @@
-import { squirrelConnection } from "./squirrel-connection.js"
+import { squirrelConnection } from './squirrel-connection.js'
 
 export const squirrelMap = async () => {
     const containerPosition = () => {
-        return document.getElementById("mapcontainer").getBoundingClientRect();
-    };
+        return document.getElementById('mapcontainer').getBoundingClientRect()
+    }
 
     const getWidth = () => {
-        return containerPosition().width;
-    };
+        return containerPosition().width
+    }
 
     const getHeight = () => {
-        let height = containerPosition().height;
-        return height;
-    };
+        let height = containerPosition().height
+        return height
+    }
 
     const getScale = () => {
-        return 1;
-    };
+        return 1
+    }
 
     const resizeProjection = (projection) => {
         projection
             .scale((getScale() * getHeight()) / 1.5 / 1.3 / Math.PI)
-            .translate([getWidth() / 2, getHeight() / 2]);
-    };
+            .translate([getWidth() / 2, getHeight() / 2])
+    }
 
     const projections = {
         ed: d3.geoAzimuthalEquidistant().clipAngle(180.0 - 1e-3),
         ea: d3.geoAzimuthalEqualArea().clipAngle(180.0 - 1),
-    };
+    }
 
-    const projection = projections.ed;
-    resizeProjection(projection);
+    const projection = projections.ed
+    resizeProjection(projection)
 
-    projection.rotate([-50, -10]);
+    projection.rotate([-50, -10])
 
     const svg = d3
-        .select("svg")
-        .attr("width", getWidth())
-        .attr("height", getHeight());
+        .select('svg')
+        .attr('width', getWidth())
+        .attr('height', getHeight())
 
     // Load external data and boot
     const data = await d3.json(
-        "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-    );
+        'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson'
+    )
 
     // Draw the map
-    svg.append("g")
-        .selectAll("path")
+    svg.append('g')
+        .selectAll('path')
         .data(data.features)
         .enter()
-        .append("path")
-        .attr("fill", "#dddddd")
-        .attr("d", d3.geoPath().projection(projection))
-        .style("stroke", "#aaa");
+        .append('path')
+        .attr('fill', '#dddddd')
+        .attr('d', d3.geoPath().projection(projection))
+        .style('stroke', '#aaa')
 
-    let graticule = d3.geoGraticule();
+    let graticule = d3.geoGraticule()
 
-    svg.append("g")
-        .append("path")
+    svg.append('g')
+        .append('path')
         .datum(graticule)
-        .attr("d", d3.geoPath().projection(projection))
-        .attr("fill", "none")
-        .attr("stroke", "#aaa");
+        .attr('d', d3.geoPath().projection(projection))
+        .attr('fill', 'none')
+        .attr('stroke', '#aaa')
 
     const connection = squirrelConnection()
-    const locations = await connection.request("get_sensors");
+    const locations = await connection.request('get_sensors')
 
-    svg.selectAll("circle")
+    svg.selectAll('circle')
         .data(locations)
         .enter()
-        .append("circle")
-        .attr("r", 2)
-        .attr("fill", "#ff555577")
-        .attr("transform", function (loc) {
-            return "translate(" + projection([loc.lon, loc.lat]) + ")";
-        });
+        .append('circle')
+        .attr('r', 2)
+        .attr('fill', '#ff555577')
+        .attr('transform', function (loc) {
+            return 'translate(' + projection([loc.lon, loc.lat]) + ')'
+        })
 
     const resize = () => {
-        resizeProjection(projection);
-        svg.attr("width", getWidth()).attr("height", getHeight());
+        resizeProjection(projection)
+        svg.attr('width', getWidth()).attr('height', getHeight())
 
-        svg.selectAll("g")
-            .selectAll("path")
-            .attr("d", d3.geoPath().projection(projection));
+        svg.selectAll('g')
+            .selectAll('path')
+            .attr('d', d3.geoPath().projection(projection))
 
-        svg.selectAll("circle").attr("transform", function (ev) {
-            return "translate(" + projection([ev.lon, ev.lat]) + ")";
-        });
-    };
+        svg.selectAll('circle').attr('transform', function (ev) {
+            return 'translate(' + projection([ev.lon, ev.lat]) + ')'
+        })
+    }
 
-    window.onresize = resize;
-};
+    window.onresize = resize
+}
