39dfebc4011792ffc8fb06c46812cbea5991f1e1
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2024-08-08 09:09:08 +0200

    wip: squirrel: service

--- src/squirrel/service/page/js/squirrel-map.js
+++ src/squirrel/service/page/js/squirrel-map.js
@@ -1,35 +1,5 @@
-import { createIfNeeded } from './squirrel-common.js'
+import { createIfNeeded, colors } from './squirrel-common.js'
 import { squirrelConnection } from './squirrel-connection.js'
-
-const colors = {
-    aluminium1: '#eeeeec',
-    aluminium2: '#d3d7cf',
-    aluminium3: '#babdb6',
-    aluminium4: '#888a85',
-    aluminium5: '#555753',
-    aluminium6: '#2e3436',
-    butter1: '#fce94f',
-    butter2: '#edd400',
-    butter3: '#c4a000',
-    chameleon1: '#8ae234',
-    chameleon2: '#73d216',
-    chameleon3: '#4e9a06',
-    chocolate1: '#e9b96e',
-    chocolate2: '#c17d11',
-    chocolate3: '#8f5902',
-    orange1: '#fcaf3e',
-    orange2: '#f57900',
-    orange3: '#ce5c00',
-    plum1: '#ad7fa8',
-    plum2: '#75507b',
-    plum3: '#5c3566',
-    scarletred1: '#ef2929',
-    scarletred2: '#cc0000',
-    scarletred3: '#a40000',
-    skyblue1: '#729fcf',
-    skyblue2: '#3465a4',
-    skyblue3: '#204a87',
-}
 
 export const squirrelMap = () => {
     let map
--- src/squirrel/service/page/js/squirrel-timeline.js
+++ src/squirrel/service/page/js/squirrel-timeline.js
@@ -1,11 +1,31 @@
-import { createIfNeeded, strToTime } from './squirrel-common.js'
+import {
+    createIfNeeded,
+    strToTime,
+    colors,
+    niceTimeTickInc,
+    timeTickLabels,
+} from './squirrel-common.js'
 
 export const squirrelTimeline = () => {
-    let container
-    let timeline
-    let x = d3.scaleLinear()
+    // parameters
     let timeMin = strToTime('1900-01-01 00:00:00')
     let timeMax = strToTime('2030-01-01 00:00:00')
+    let trackMin = 0
+    let trackMax = 3
+    let marginTop = 150 
+    let marginBottom = 150
+    let padding = 10
+    let tickLength = padding
+    let fontSize = 12
+
+    // private
+    let container
+    let timeline
+    let boxesGroup
+    let axesGroup
+    let x = d3.scaleLinear()
+    let y = d3.scaleLinear()
+    let trackStart = null
 
     const bounds = () => {
         return container.node().getBoundingClientRect()
@@ -19,62 +39,242 @@ export const squirrelTimeline = () => {
         return bounds().height
     }
 
-    const setupProjection = () => {
-        x.domain(timeMin, timeMax).range(0, getWidth())
+    const updateProjection = () => {
+        x.domain([timeMin, timeMax]).range([0, getWidth()])
+        y.domain([trackMin, trackMax]).range([
+            marginTop,
+            getHeight() - (marginTop + marginBottom),
+        ])
     }
 
-    const reProject = () => {
-        setupProjection()
+    const update = () => {
+        updateProjection()
+        updateBoxes()
+        updateAxes()
     }
 
     const resize = () => {
         timeline.attr('width', getWidth()).attr('height', getHeight())
-        reProject()
+        update()
     }
+
+    const pointerDownHandler = (ev) => {
+        container.node().setPointerCapture(ev.pointerId)
+        trackStart = {
+            position: [ev.clientX, ev.clientY],
+            domain: [...x.domain()],
+            mode:
+                ev.clientY > getHeight() - marginBottom
+                    ? 'global_fixed'
+                    : 'global',
+        }
+    }
+
+    const pointerMoveHandler = (ev) => {
+        if (trackStart) {
+            let x1 = ev.clientX
+            let y1 = ev.clientY
+            let x0 = trackStart.position[0]
+            let y0 = trackStart.position[1]
+            let w = getWidth()
+            let h = getHeight()
+            let tmin0 = trackStart.domain[0]
+            let tmax0 = trackStart.domain[1]
+            let mode = trackStart.mode
+            let scale, xfrac, dx, dy, dt, dtr
+
+            dx = (x1 - x0) / w
+            dy = (y1 - y0) / h
+            xfrac = x0 / w
+            scale = mode == 'global' ? (scale = Math.exp(-dy * 5)) : 1.0
+            dtr = (tmax0 - tmin0) * (scale - 1.0)
+            dt = dx * (tmax0 - tmin0) * scale
+            timeMin = tmin0 - dt - dtr * xfrac
+            timeMax = tmax0 - dt + dtr * (1 - xfrac)
+
+            update()
+        }
+    }
+
+    const pointerUpHandler = (ev) => {
+        trackStart = null
+    }
+
+    let boxes = [
+        {
+            id: 0,
+            tmin: strToTime('1977-02-20 00:00:00'),
+            tmax: strToTime('2023-08-05 00:00:00'),
+            ymin: 0.0,
+            ymax: 1.0,
+        },
+        {
+            id: 1,
+            tmin: strToTime('1939-01-01 00:00:00'),
+            tmax: strToTime('1945-08-05 00:00:00'),
+            ymin: 1.0,
+            ymax: 2.0,
+        },
+    ]
+
+    const boxWidth = (box) => {
+        return x(box.tmax) - x(box.tmin)
+    }
+    const boxHeight = (box) => {
+        return y(box.ymax) - y(box.ymin) - padding * 2
+    }
+    const boxLeft = (box) => {
+        return x(box.tmin)
+    }
+    const boxTop = (box) => {
+        return y(box.ymin) + padding
+    }
+
+    const updateBoxes = () => {
+        boxesGroup
+            .selectAll('rect')
+            .data(boxes, (box) => box.id)
+            .join('rect')
+            .attr('width', boxWidth)
+            .attr('height', boxHeight)
+            .attr('x', boxLeft)
+            .attr('y', boxTop)
+            .attr('fill', '#cde')
+            .attr('stroke', '#abc')
+    }
+
+    const updateAxes = () => {
+        const axisY = (i) => {
+            return [getHeight() - marginBottom + padding, marginTop - padding][
+                i
+            ]
+        }
+
+        let napprox = 5
+        let [tinc, tinc_units] = niceTimeTickInc((timeMax - timeMin) / napprox)
+        let [times, labels] = timeTickLabels(timeMin, timeMax, tinc, tinc_units)
+        let ticks = times.map((t, i) => ({
+            t: t,
+            labels: ('' + labels[i]).split('\n').reverse(),
+        }))
+
+        let axisGroups = axesGroup
+            .selectAll('.axis-group')
+            .data([0, 1], (axisId) => axisId)
+            .join((enter) => enter.append('g').attr('class', 'axis-group'))
+
+        axisGroups
+            .selectAll('.axis-line')
+            .data((axisId) => [axisId])
+            .join((enter) =>
+                enter
+                    .append('line')
+                    .attr('class', 'axis-line')
+                    .attr('stroke', colors['aluminium4'])
+                    .attr('stroke-width', 3.0)
+            )
+            .attr('x1', 0.0)
+            .attr('x2', getWidth)
+            .attr('y1', axisY)
+            .attr('y2', axisY)
+
+        axisGroups
+            .selectAll('.axis-tick')
+            .data(
+                (axisId) =>
+                    ticks
+                        .filter((tick) => tick.t > timeMin)
+                        .map((tick) => ({ axisId, tick })),
+                (d) => d.tick.t
+            )
+            .join('line')
+            .attr('class', 'axis-tick')
+            .attr('stroke', colors['aluminium4'])
+            .attr('stroke-width', 3.0)
+            .attr('y1', (d) => axisY(d.axisId))
+            .attr(
+                'y2',
+                (d) =>
+                    axisY(d.axisId) + (d.axisId == 0 ? tickLength : -tickLength)
+            )
+            .attr('x1', (d) => x(d.tick.t))
+            .attr('x2', (d) => x(d.tick.t))
+
+        axisGroups
+            .filter((d) => d == 0)
+            .selectAll('.axis-tick-label-group')
+            .data(
+                (axisId) => ticks.map((tick) => ({ axisId, tick })),
+                (d) => d.tick.t
+            )
+            .join('g')
+            .attr('class', 'axis-tick-label-group')
+            .selectAll('.axis-tick-label')
+            .data(
+                (etick) =>
+                    etick.tick.labels.map((label, ilabel) => ({
+                        ...etick,
+                        label,
+                        ilabel,
+                    })),
+                (d) => d.ilabel
+            )
+            .join('text')
+            .attr('class', 'axis-tick-label')
+            .attr(
+                'y',
+                (d) => axisY(d.axisId) + (d.axisId == 0 ? 1 : -1) * tickLength
+            )
+            .attr(
+                'dy',
+                (d) => (d.axisId == 0 ? 1 : -1) * (0.5 + 1.5 * d.ilabel) + 'em'
+            )
+            .attr('x', (d) => x(d.tick.t))
+            .attr('dx', (d) => '0.5em')
+            .attr('dx', (d) => d.tick.t == timeMin ? '0.5em' : '0')
+            .style('font-size', fontSize + 'pt')
+            .style('text-anchor', (d) => d.tick.t == timeMin ? 'left' : 'middle')
+            .style('dominant-baseline', (d) =>
+                d.axisId == 0 ? 'text-before-edge' : 'no-change'
+            )
+            .text((d) => d.label)
+
+        axisGroups
+    }
+
     let my = (selection) => {
         container = selection
         timeline = createIfNeeded(container, 'svg')
+
+        //let group = timeline.append('g')
+        //group
+        //    .selectAll('circle')
+        //    .data([10, 20, 30], (d) => d)
+        //    .join(
+        //        enter => {console.log('enter 1 '); console.log(enter); return enter.append('circle').attr('fill', 'red')},
+        //        update => {console.log('update 1'); return update.attr('fill', 'black')})
+        //    .attr('r', 5)
+        //    .attr('cx', 20)
+        //    .attr('cy', (d) => d)
+        //group
+        //    .selectAll('circle')
+        //    .data([10, 20, 30, 40], (d) => d)
+        //    .join(
+        //        enter => {console.log('enter 2 '); console.log(enter); return enter.append('circle').attr('fill', 'red')},
+        //        update => {console.log('update 2'); return update.attr('fill', 'black')})
+        //    .attr('r', 5)
+        //    .attr('cx', 40)
+        //    .attr('cy', (d) => d)
+        //
+        boxesGroup = timeline.append('g')
+        axesGroup = timeline.append('g')
+
+        container.on('pointerdown', pointerDownHandler)
+        container.on('pointerup', pointerUpHandler)
+        container.on('pointermove', pointerMoveHandler)
+
         window.onresize = resize
         resize()
-        let boxes = [
-            {
-                tmin: strToTime('1977-02-20 00:00:00'),
-                tmax: strToTime('2023-08-05 00:00:00'),
-                ymin: 0.0,
-                ymax: 1.0,
-            },
-        ]
-
-        let boxWidth = (box) => {
-            x(box.tmax) - x(box.tmin)
-        }
-        let boxHeight = (box) => {
-            box.ymax - box.ymin
-        }
-        let boxLeft = (box) => {
-            x(box.tmin)
-        }
-        let boxTop = (box) => {
-            box.ymin
-        }
-
-        timeline.append('g').append('circle').attr('r', 5).attr('fill', '#000').attr('cx', 10).attr('cy', 20)
-        console.log('xx')
-        console.log(boxes)
-
-        let xx = timeline
-            .append('g')
-            .data(boxes)
-            .enter()
-            .append('rect')
-            //.attr('width', boxWidth)
-            //.attr('height', boxHeight)
-            //.attr('x', boxLeft)
-            //.attr('y', boxTop)
-            //.attr('fill', '#cde')
-            //.attr('stroke', '#abc')
-
-        console.log(xx)
     }
 
     my.timeMin = function (_) {
