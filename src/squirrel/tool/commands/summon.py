e09b0394407d9b7986282b619ed745256a08985b
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2024-12-13 14:22:05 +0100

    squirrel summon: add --get-events option and related

--- src/squirrel/tool/commands/summon.py
+++ src/squirrel/tool/commands/summon.py
@@ -11,7 +11,7 @@ import math
 import logging
 
 from pyrocko.squirrel.error import SquirrelError
-from pyrocko import progress
+from pyrocko import progress, guts, util
 
 logger = logging.getLogger('psq.cli.summon')
 
@@ -54,13 +54,53 @@ returned. Use in combination with ``--sample-rate-min`` and
         type=float,
         help='Maximum sample rate [Hz] to consider.')
 
+    parser.add_argument(
+        '--get-events',
+        dest='get_events',
+        action='store_true',
+        help='Get only time spans with events.')
+
+    parser.add_argument(
+        '--event-time-span',
+        dest='event_time_span',
+        metavar='DURATION,DURATION',
+        help='Set time span to be completed for events, e.g. `-30m,2h`.')
+
 
 def run(parser, args):
     d = args.squirrel_query
     squirrel = args.make_squirrel()
 
-    if 'tmin' not in d or 'tmax' not in d:
-        raise SquirrelError('Time span required.')
+    if args.get_events:
+        events = squirrel.get_events(
+            tmin=d.get('tmin', None),
+            tmax=d.get('tmax', None))
+
+        if not args.event_time_span:
+            raise SquirrelError(
+                'Time span setting need. Currently --event-time-span')
+
+        try:
+            span_tmin, span_tmax = [
+                guts.parse_duration(s)
+                for s in args.event_time_span.split(',')]
+
+        except Exception:
+            raise SquirrelError(
+                'Invalid argument to --event-time-span: %s'
+                % args.event_time_span)
+
+        groups = [
+            (event.time + span_tmin, event.time + span_tmax, event.name)
+            for event in events]
+    else:
+        if 'tmin' not in d or 'tmax' not in d:
+            raise SquirrelError('Time span required.')
+
+        groups = [(d['tmin'], d['tmax'], '')]
+
+    d.pop('tmin', None)
+    d.pop('tmax', None)
 
     tinc = 3600.
 
@@ -70,21 +110,36 @@ def run(parser, args):
             cha.strip() for cha in args.channel_priorities.split(',')]
 
     with progress.view():
-        nwindows = int(math.ceil((d['tmax'] - d['tmin']) / tinc))
-        task = progress.task('Summoning', nwindows)
-        iwindow = 0
-        for trs in squirrel.chopper_waveforms(
-                tinc=tinc,
-                load_data=False,
-                channel_priorities=channel_priorities,
-                sample_rate_min=args.sample_rate_min,
-                sample_rate_max=args.sample_rate_max,
-                **d):
+        task_group = progress.task('Group', len(groups))
 
-            iwindow += 1
-            task.update(iwindow)
+        for group_tmin, group_tmax, group_name in task_group(groups):
+            if group_name:
+                logger.info('Summoning group: %s (%s - %s)' % (
+                    group_name,
+                    util.time_to_str(group_tmin),
+                    util.time_to_str(group_tmax)))
 
-        task.done()
+            tmin = math.floor(group_tmin / tinc) * tinc
+            tmax = math.ceil(group_tmax / tinc) * tinc
+            nwindows = int(round((tmax - tmin) / tinc))
+            task = progress.task('Summoning', nwindows)
+            iwindow = 0
+            try:
+                for trs in squirrel.chopper_waveforms(
+                        tinc=tinc,
+                        load_data=False,
+                        channel_priorities=channel_priorities,
+                        sample_rate_min=args.sample_rate_min,
+                        sample_rate_max=args.sample_rate_max,
+                        tmin=tmin,
+                        tmax=tmax,
+                        **d):
+
+                    iwindow += 1
+                    task.update(iwindow)
+
+            finally:
+                task.done()
 
     stats = str(squirrel)
     stats = '\n'.join('  ' + s for s in stats.splitlines())
