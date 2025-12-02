888b0aad1d46399d8b14f78a4a9c16a196e01b53
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2025-12-02 11:04:45 +0100

    remove pyqt5.qtwebkit dependency

--- prerequisites/prerequisites_debian_python3.sh
+++ prerequisites/prerequisites_debian_python3.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y make git python3-dev python3-setuptools python3-pip \
     python3-wheel python3-numpy python3-numpy-dev python3-scipy \
     python3-matplotlib python3-pyqt5 python3-pyqt5.qtopengl \
-    python3-pyqt5.qtsvg python3-pyqt5.qtwebkit python3-yaml \
+    python3-pyqt5.qtsvg python3-yaml \
     python3-jinja2 python3-requests python3-coverage \
     python3-pytest python3-pyqt5.qtwebengine
 sudo apt-get install -y python3-vtk9 \
