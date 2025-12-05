1c02fd85303c44fb6efd22ca7f02c3fa1e78a49d
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2025-12-05 14:58:08 +0100

    pip packaging: python 3.14 support and improvements

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -5,11 +5,21 @@ set -e
 rm -rf wheels_temp wheels
 
 mkdir wheels_temp
-for x in `ls /opt/python` ; do
-    [ "$x" == "cp313-cp313t" ] && continue
-    "/opt/python/$x/bin/python" -c 'import sys ; sys.exit(not ((3, 10, 0) <= sys.version_info < (3, 14, 0)) or sys.implementation.name != "cpython")' || continue
-    "/opt/python/$x/bin/pip" install --upgrade pip
-    "/opt/python/$x/bin/pip" wheel -v . -w wheels_temp --only-binary=:all:
+
+pvs="
+cp310-cp310
+cp311-cp311
+cp312-cp312
+cp313-cp313
+cp314-cp314
+"
+
+for pv in $pvs ; do
+    echo "===================================================================="
+    echo "Building for ${pv}"
+    echo "===================================================================="
+    "/opt/python/$pv/bin/pip" install --upgrade pip
+    "/opt/python/$pv/bin/pip" wheel -v . -w wheels_temp --only-binary=:all:
 done
 
 mkdir wheels
@@ -17,3 +27,8 @@ for wheel in wheels_temp/pyrocko-*.whl ; do
     auditwheel repair "$wheel" --plat $PLAT -w dist
     rm "$wheel"
 done
+
+echo "===================================================================="
+echo "Wheels in ./dist:"
+ls dist
+echo "===================================================================="
