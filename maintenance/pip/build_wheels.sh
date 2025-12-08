9f118c221065849e64eaad638c74777552dc3788
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2025-12-08 09:41:10 +0100

    wip: pip packaging

--- maintenance/pip/build_wheels.sh
+++ maintenance/pip/build_wheels.sh
@@ -6,7 +6,7 @@ rm -rf wheels_temp wheels
 
 mkdir wheels_temp
 for x in `ls /opt/python` ; do
-    [ "$x" == "cp313-cp313t" ] && continue
+    [ "$x" == "cp313-cp313t" || "$x" == "cp314-cp314t" ] && continue
     "/opt/python/$x/bin/python" -c 'import sys ; sys.exit(not ((3, 14, 0) <= sys.version_info < (3, 15, 0)) or sys.implementation.name != "cpython")' || continue
     "/opt/python/$x/bin/pip" install --upgrade pip
     "/opt/python/$x/bin/pip" wheel -v . -w wheels_temp --only-binary=:all:
