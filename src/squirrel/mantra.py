459a3dbe5a800a126d31cf51f48ad8bbd45944c3
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2026-04-10 08:59:18 +0200

    squirrel: add module `mantra`

---
+++ src/squirrel/mantra.py
@@ -0,0 +1,38 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+
+from pyrocko.guts import Object, String, List
+
+from .operators.base import BaseOperator
+
+guts_prefix = 'squirrel'
+
+
+class Mantra(Object):
+    name = String.T(default='untitled')
+    operators = List.T(BaseOperator.T())
+
+    def setup(self, sq, arrays=None):
+        if not self.operators:
+            self.outlet = sq
+        else:
+            operators = list(self.operators)
+            previous = sq
+            while operators:
+                operator = operators.pop(0)
+                operator.set_input(previous)
+                if arrays is not None:
+                    if hasattr(operator, 'set_arrays'):
+                        operator.set_arrays(arrays)
+
+                previous = operator
+
+            self.outlet = operator
+
+
+__all__ = [
+    'Mantra',
+]
