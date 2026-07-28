158cacff9854201abb9729dd7daa007f6cab0fc1
Sebastian Heimann <sebastian.heimann@uni-potsdam.de>
2026-07-28 08:48:26 +0200

    version for building pip wheels

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ is_windows = sys.platform.startswith('win')
 have_pep621_support = pv(setuptools_version) >= pv('61.0.0')
 
 packname = 'pyrocko'
-version = '2026.06.02'
+version = '2026.06.02-dev1'
 
 
 def get_numpy_include():
